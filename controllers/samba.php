b20ee9f43065098d65f4495a5f92d0ce7a2869cf
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-01-30 16:59:35 +0000

    - Added accounts initialization

--- controllers/samba.php
+++ controllers/samba.php
@@ -55,6 +55,16 @@ class Samba extends ClearOS_Controller
 
     function index()
     {
+        // Show account status widget if we're not in a happy state
+        //---------------------------------------------------------
+
+        $this->load->module('accounts/status');
+
+        if ($this->status->unhappy('openldap_directory')) {
+            $this->status->widget('password_policies', 'openldap_directory');
+            return;
+        }
+
         // Load libraries
         //---------------
 
