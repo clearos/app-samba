b8013c6f27f026aab360a0f68e0febd5d18f4efc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:26:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Administrator Дансны';
+$lang['samba_app_description'] = 'Windows Networking (Samba) Windows систем нь хоорондоо хамтарч ажилладаг шаардлагатай цавуу олгодог. апп Windows домэйн удирдлага хамт нэвтрэлт танилт үйлчилгээ, Файл болон Хэвлэх олгодог.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Компьютер';
+$lang['samba_driver_has_not_been_initialized'] = 'Жолооч эхэлж байсан байна.';
+$lang['samba_file_share_tooltip'] = 'та сүлжээний файл хувьцааг бий болгохыг хүсэж байгаа бол, Flexshare програмыг ашиглана уу.';
+$lang['samba_initializing_core_system'] = 'үндсэн системийг эхлүүлэх тэвчээртэй байх уу.';
+$lang['samba_initializing_warning'] = 'Эхлүүлэлт нь буюу хоёр минут авч болно, тэвчээртэй байх уу.';
+$lang['samba_master_node_needs_initialization'] = 'Мастер цэг эхлүүлэхээсээ өмнө эхэлж байх хэрэгтэй.';
+$lang['samba_mode'] = 'Горим';
+$lang['samba_samba_directory_installed_warning'] = 'Хэрэв та ямар ч цаашид энэ апп л шаардлагатай Samba лавлах програм хангамж, суулгасан байна.';
