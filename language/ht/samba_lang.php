58336a29bc839d92d76234d1cb43b7245c43697c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:22:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'administratè Kont';
+$lang['samba_app_description'] = 'Windows Rezo (Samba) bay lakòl ki nesesè yo enterajis ki gen yon sistèm fenèt yo. App a bay sèvis otantifikasyon, dosye ak ekri an lèt detache, ansanm ak jesyon domèn fenèt yo.';
+$lang['samba_app_name'] = 'Windows Rezo (Samba)';
+$lang['samba_computers'] = 'Odinatè';
+$lang['samba_driver_has_not_been_initialized'] = 'Chofè pa te inisyalizèd.';
+$lang['samba_file_share_tooltip'] = 'Si ou ta renmen yo kreye aksyon dosye rezo, tanpri itilize app a Flexshare.';
+$lang['samba_initializing_core_system'] = 'Inisyalizin sistèm nwayo, tanpri pran pasyans.';
+$lang['samba_initializing_warning'] = 'Inisyalizasyon ka pran yon minit oubyen de, tanpri pran pasyans.';
+$lang['samba_master_node_needs_initialization'] = 'Mèt ne bezwen yo dwe inisyalizèd anvan yo.';
+$lang['samba_mode'] = 'Mode';
+$lang['samba_samba_directory_installed_warning'] = 'Se lojisyèl an Samba Anyè enstale, kidonk, ou pa bezwen app sa a.';
