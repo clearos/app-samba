493c393b926da40e79772bb9afa0fb21b6db00dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:25:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'ನಿರ್ವಾಹಕ ಖಾತೆ';
+$lang['samba_app_description'] = 'ವಿಂಡೋಸ್ ನೆಟ್ವರ್ಕಿಂಗ್ (ಸಾಂಬಾ) ವಿಂಡೋಸ್ ವ್ಯವಸ್ಥೆಗಳು ಪರಸ್ಪರ ಅಗತ್ಯ ಅಂಟು ಒದಗಿಸುತ್ತದೆ. ಅಪ್ಲಿಕೇಶನ್ ವಿಂಡೋಸ್ ಡೊಮೈನ್ ನಿರ್ವಹಣೆ ಜೊತೆಗೆ, ದೃಢೀಕರಣ ಸೇವೆಗಳು, ಫೈಲ್ ಮತ್ತು ಮುದ್ರಣ ಒದಗಿಸುತ್ತದೆ.';
+$lang['samba_app_name'] = 'ವಿಂಡೋಸ್ ನೆಟ್ವರ್ಕಿಂಗ್ (ಸಾಂಬಾ)';
+$lang['samba_computers'] = 'ಕಂಪ್ಯೂಟರ್';
+$lang['samba_driver_has_not_been_initialized'] = 'ಚಾಲಕ ಆರಂಭಿಸಲಾಗಿಲ್ಲ ಮಾಡಿಲ್ಲ.';
+$lang['samba_file_share_tooltip'] = 'ನೀವು ಜಾಲಬಂಧ ಕಡತ ಷೇರುಗಳನ್ನು ರಚಿಸಲು ಬಯಸಿದರೆ, ದಯವಿಟ್ಟು Flexshare ಅಪ್ಲಿಕೇಶನ್ ಬಳಸಿ.';
+$lang['samba_initializing_core_system'] = 'ಕೋರ್ ವ್ಯವಸ್ಥೆಯ ಆರಂಭಿಸುವಿಕೆ, ದಯವಿಟ್ಟು ತಾಳ್ಮೆಯಿಂದಿರಿ.';
+$lang['samba_initializing_warning'] = 'ಪ್ರಾರಂಭಿಕ ಒಂದು ನಿಮಿಷ ಅಥವಾ ಎರಡು ತೆಗೆದುಕೊಳ್ಳಬಹುದು, ದಯವಿಟ್ಟು ತಾಳ್ಮೆಯಿಂದಿರಿ.';
+$lang['samba_master_node_needs_initialization'] = 'ಮಾಸ್ಟರ್ ನೋಡ್ ಮುನ್ನ ಆರಂಭಿಸಲಾಗಿಲ್ಲ ಅಗತ್ಯವಿದೆ.';
+$lang['samba_mode'] = 'ಮೋಡ್';
+$lang['samba_samba_directory_installed_warning'] = 'ಆದ್ದರಿಂದ ನೀವು ಇನ್ನು ಮುಂದೆ ಈ ಅಪ್ಲಿಕೇಶನ್, ಸಾಂಬಾ ಡೈರೆಕ್ಟರಿ ಸಾಫ್ಟ್ವೇರ್ ಅನ್ನು ಸ್ಥಾಪಿಸಿದ.';
