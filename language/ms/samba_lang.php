0c6a905d450cf1136da56bb29adef100b3341585
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:26:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Akaun pentadbir';
+$lang['samba_app_description'] = 'Windows Rangkaian (Samba) menyediakan gam yang diperlukan untuk saling beroperasi dengan sistem Windows. Aplikasi ini menyediakan perkhidmatan pengesahan, fail dan cetak, bersama-sama dengan Windows pengurusan domain.';
+$lang['samba_app_name'] = 'Windows Rangkaian (Samba)';
+$lang['samba_computers'] = 'Komputer';
+$lang['samba_driver_has_not_been_initialized'] = 'Pemandu belum dimulakan.';
+$lang['samba_file_share_tooltip'] = 'Jika anda ingin membuat perkongsian fail rangkaian, sila gunakan aplikasi Flexshare itu.';
+$lang['samba_initializing_core_system'] = 'Memulakan sistem teras, sila bersabar.';
+$lang['samba_initializing_warning'] = 'Pengawalan boleh mengambil satu atau dua minit, sila bersabar.';
+$lang['samba_master_node_needs_initialization'] = 'nod induk perlu dimulakan sebelum meneruskan.';
+$lang['samba_mode'] = 'mod';
+$lang['samba_samba_directory_installed_warning'] = 'Perisian Direktori Samba dipasang, supaya anda tidak lagi perlu app ini.';
