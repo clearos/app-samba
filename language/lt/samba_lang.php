7674b5356cab5b68ddfc826c65ac50e5c80da0e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:25:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'administratorius paskyra';
+$lang['samba_app_description'] = '"Windows tinklai (Samba) užtikrina reikiamą klijų suderinamumo su Windows sistemomis. App suteikia autentiškumo paslaugas, rinkmenų ir spausdinimo, kartu su Windows domeno valdymą.';
+$lang['samba_app_name'] = '"Windows tinklai (Samba)';
+$lang['samba_computers'] = 'Kompiuteriai';
+$lang['samba_driver_has_not_been_initialized'] = 'Vairuotojo nebuvo inicijuotas.';
+$lang['samba_file_share_tooltip'] = 'Jei norite sukurti Network File akcijas, prašome naudoti Flexshare programą.';
+$lang['samba_initializing_core_system'] = 'Inicijuojama pagrindinei sistemai, būkite kantrūs.';
+$lang['samba_initializing_warning'] = 'Pradės gali užtrukti minutę ar dvi, būkite kantrūs.';
+$lang['samba_master_node_needs_initialization'] = 'Magistro mazgas turi būti inicializuoti prieš tęsiant.';
+$lang['samba_mode'] = 'režimas';
+$lang['samba_samba_directory_installed_warning'] = 'Samba katalogas programinė įranga yra įdiegta, todėl jums nebereikia šią programą.';
