3f31b54db50c6ab7a7a4fd22ee063d6ce397366f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:25:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = '관리자 계정';
+$lang['samba_app_description'] = 'Windows 네트워킹 (삼바)는 Windows 시스템과 상호 운용하는 데 필요한 접착제를 제공합니다. 응용 프로그램은 Windows 도메인 관리와 함께, 인증 서비스, 파일 및 인쇄를 제공합니다.';
+$lang['samba_app_name'] = 'Windows 네트워킹 (삼바)';
+$lang['samba_computers'] = '컴퓨터';
+$lang['samba_driver_has_not_been_initialized'] = '드라이버가 초기화되지 않았습니다.';
+$lang['samba_file_share_tooltip'] = '당신은 네트워크 파일 공유를 생성하고 싶은 경우, FlexShare를 응용 프로그램을 사용하십시오.';
+$lang['samba_initializing_core_system'] = '코어 시스템 초기화, 기다려주십시오.';
+$lang['samba_initializing_warning'] = '초기화가 2 분 정도 걸릴 수 기다려주십시오.';
+$lang['samba_master_node_needs_initialization'] = '마스터 노드는 진행하기 전에 초기화해야합니다.';
+$lang['samba_mode'] = '방법';
+$lang['samba_samba_directory_installed_warning'] = '더 이상이 응용 프로그램을 필요로하지 않도록 삼바 디렉토리 소프트웨어가 설치됩니다.';
