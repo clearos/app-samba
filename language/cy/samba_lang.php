6f2f669e46bdc3db15bc0fd605eba7ed54cbb5fe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:32:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Cyfrif Administrator';
+$lang['samba_app_description'] = 'Windows Rhwydweithio (Samba) yn darparu y glud sy&#39;n angenrheidiol i hwyluso rhyngweithio â systemau Windows. Mae&#39;r app yn darparu gwasanaethau dilysu, ffeiliau ac argraffu, ynghyd â rheoli parth Windows.';
+$lang['samba_app_name'] = 'Windows Rhwydweithio (Samba)';
+$lang['samba_computers'] = 'cyfrifiaduron';
+$lang['samba_driver_has_not_been_initialized'] = 'Nid yw gyrrwr wedi&#39;i ymgychwyn.';
+$lang['samba_file_share_tooltip'] = 'Os hoffech chi i greu cyfranddaliadau ffeil rhwydwaith, defnyddiwch y app Flexshare.';
+$lang['samba_initializing_core_system'] = 'Ymgychwyn system graidd, byddwch yn amyneddgar.';
+$lang['samba_initializing_warning'] = 'Gall initialization gymryd munud neu ddau, byddwch yn amyneddgar.';
+$lang['samba_master_node_needs_initialization'] = 'Mae angen ymgychwyn cyn symud ymlaen Meistr nôd.';
+$lang['samba_mode'] = 'modd';
+$lang['samba_samba_directory_installed_warning'] = 'Mae&#39;r meddalwedd Cyfeiriadur Samba ei osod, felly mae angen app hwn mwyach.';
