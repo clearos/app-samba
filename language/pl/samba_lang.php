aca648ee5f01d89b9db0785821e214e9c6bcc3bd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:28:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'konto administratora';
+$lang['samba_app_description'] = 'Sieć Windows (Samba) zapewnia niezbędną klej do współdziałania z systemami Windows. Aplikacja zapewnia usługi uwierzytelniania, plików i drukarek, a także zarządzania domeny Windows.';
+$lang['samba_app_name'] = 'Sieć Windows (Samba)';
+$lang['samba_computers'] = 'Komputery';
+$lang['samba_driver_has_not_been_initialized'] = 'Sterownik nie został zainicjowany.';
+$lang['samba_file_share_tooltip'] = 'Jeśli chciałbyś stworzyć plików udostępnionych w sieci, należy użyć aplikacji FlexShare.';
+$lang['samba_initializing_core_system'] = 'Inicjowanie system podstawowy, prosimy o cierpliwość.';
+$lang['samba_initializing_warning'] = 'Inicjalizacja może trwać minutę lub dwie, prosimy o cierpliwość.';
+$lang['samba_master_node_needs_initialization'] = 'Węzeł główny musi zostać zainicjowane przed przejściem.';
+$lang['samba_mode'] = 'Tryb';
+$lang['samba_samba_directory_installed_warning'] = 'Oprogramowanie Samba katalog jest zainstalowany, więc nie trzeba już tę aplikację.';
