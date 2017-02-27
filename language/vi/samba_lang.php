35a43e18e3d8dc696c042748ec5467b29ad394ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:31:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Tài khoản quản trị';
+$lang['samba_app_description'] = 'Kết nối mạng Windows (Samba) cung cấp keo cần thiết để tương thích với các hệ thống Windows. Ứng dụng cung cấp dịch vụ chứng thực, tập tin và in, cùng với quản lý miền Windows.';
+$lang['samba_app_name'] = 'Kết nối mạng Windows (Samba)';
+$lang['samba_computers'] = 'Máy tính';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver đã không được khởi tạo.';
+$lang['samba_file_share_tooltip'] = 'Nếu bạn muốn tạo ra các file chia sẻ mạng, hãy sử dụng các ứng dụng Flexshare.';
+$lang['samba_initializing_core_system'] = 'Khởi tạo hệ thống cốt lõi, hãy kiên nhẫn.';
+$lang['samba_initializing_warning'] = 'Khởi tạo có thể mất một hoặc hai phút, hãy kiên nhẫn.';
+$lang['samba_master_node_needs_initialization'] = 'Thạc sĩ node cần phải được khởi tạo trước khi tiếp tục.';
+$lang['samba_mode'] = 'Chế độ';
+$lang['samba_samba_directory_installed_warning'] = 'Các phần mềm Samba Directory được cài đặt, do đó bạn không còn cần ứng dụng này.';
