aae044be854d1215b371738d13f1da6bee4128dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:26:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Pūkete kaiwhakahaere';
+$lang['samba_app_description'] = 'Windows Networking (Samba) whakarato te kāpia e tika ana ki te interoperate ki ngā pūnaha Windows. Te taupānga whakarato ratonga motuhēhēnga, kōnae, me te tā, me te whakahaere rohe Windows.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'rorohiko';
+$lang['samba_driver_has_not_been_initialized'] = 'e kore kua Driver i arawhiti.';
+$lang['samba_file_share_tooltip'] = 'Ki te hiahia koe ki te waihanga hea kōnae whatunga, tēnā whakamahi i te taupānga Flexshare.';
+$lang['samba_initializing_core_system'] = 'Arawhiti ana pūnaha matua, tēnā kia manawanui.';
+$lang['samba_initializing_warning'] = 'kia tango arawhiti he rua ranei meneti, tēnā kia manawanui.';
+$lang['samba_master_node_needs_initialization'] = 'Me arawhiti ki te aroaro o puta Kaiwhakaako kōpuku.';
+$lang['samba_mode'] = 'aratau';
+$lang['samba_samba_directory_installed_warning'] = 'Kei te tāuta i te pūmanawa Samba Directory, kia hiahia kore koutou i tēnei taupānga.';
