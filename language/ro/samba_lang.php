ae16bb7e7c8e2da2863e3057c7a4dcee127657ee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:28:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Contul de administrator';
+$lang['samba_app_description'] = 'Ferestre din rețea (Samba) oferă adezivul necesar pentru interoperare cu sistemele de operare Windows. Aplicația furnizează servicii de autentificare, fișiere și imprimare, împreună cu managementul de domeniu Windows.';
+$lang['samba_app_name'] = 'Ferestre din rețea (Samba)';
+$lang['samba_computers'] = 'computere';
+$lang['samba_driver_has_not_been_initialized'] = 'Conducător auto nu a fost inițializat.';
+$lang['samba_file_share_tooltip'] = 'În cazul în care doriți să creați acțiuni de fișiere de rețea, vă rugăm să folosiți aplicația Flexshare.';
+$lang['samba_initializing_core_system'] = 'core system inițializarea, vă rugăm să aveți răbdare.';
+$lang['samba_initializing_warning'] = 'Inițializarea poate dura un minut sau două, vă rugăm să aveți răbdare.';
+$lang['samba_master_node_needs_initialization'] = 'nod principal trebuie să fie inițializate înainte de a continua.';
+$lang['samba_mode'] = 'mod';
+$lang['samba_samba_directory_installed_warning'] = 'Software-ul Samba Director este instalat, astfel încât nu mai este nevoie de această aplicație.';
