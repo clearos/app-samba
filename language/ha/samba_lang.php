d698fe3ef798c25987bb93a87a9de8bb161d787f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:23:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Administrator Account';
+$lang['samba_app_description'] = 'Windows Networking (Samba) na samar da zama dole manne wa interoperate da Windows tsarin. The app bayar Tantance kalmar sirri ayyuka, fayil kuma buga, tare da Windows yankin management.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Computers';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver ba a initialized.';
+$lang['samba_file_share_tooltip'] = 'Idan ka so ka ƙirƙiri cibiyar sadarwa fayil hannun jari, don Allah a yi amfani da Flexshare app.';
+$lang['samba_initializing_core_system'] = 'Soma core tsarin, don Allah ka yi haƙuri.';
+$lang['samba_initializing_warning'] = 'Initialization iya šaukar minti ko biyu, don Allah ka yi haƙuri.';
+$lang['samba_master_node_needs_initialization'] = 'Jagora kumburi bukatar a initialized kafin a ci gaba.';
+$lang['samba_mode'] = 'Mode';
+$lang['samba_samba_directory_installed_warning'] = 'The Samba Directory software da aka shigar, sai ka daina bukatar wannan app.';
