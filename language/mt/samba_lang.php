f6bc0804f1a9ecf660cb5658297d90acbd1df7c1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:27:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Kont amministratur';
+$lang['samba_app_description'] = 'Twieqi Netwerking (Samba) jipprovdi l-kolla meħtieġa sabiex jinteroperaw mas-sistemi Windows. Il-app jipprovdi servizzi ta &#39;awtentikazzjoni, fajl u jistampa, flimkien mal-ġestjoni domain tal-Windows.';
+$lang['samba_app_name'] = 'Twieqi Netwerking (Samba)';
+$lang['samba_computers'] = 'kompjuters';
+$lang['samba_driver_has_not_been_initialized'] = 'Sewwieq ma ġietx initialized.';
+$lang['samba_file_share_tooltip'] = 'Jekk inti tixtieq li toħloq ishma fajl netwerk, jekk jogħġbok uża l-app Flexshare.';
+$lang['samba_initializing_core_system'] = 'Initializing sistema prinċipali, jekk jogħġbok kun pazjent.';
+$lang['samba_initializing_warning'] = 'Inizjalizzazzjoni jista &#39;jieħu minuta jew tnejn, jekk jogħġbok kun pazjent.';
+$lang['samba_master_node_needs_initialization'] = 'node Master jeħtieġ li jiġi initialized qabel ma tipproċedi.';
+$lang['samba_mode'] = 'modalità';
+$lang['samba_samba_directory_installed_warning'] = 'Is-softwer Samba Direttorju huwa installat, hekk m&#39;għadx għandek bżonn dan app.';
