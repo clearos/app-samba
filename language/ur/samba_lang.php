2653e117b12287fe45ca33b726bda0a9cae850fe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:31:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'ایڈمنسٹریٹر اکاؤنٹ';
+$lang['samba_app_description'] = 'ونڈوز نیٹ ورکنگ (سامبا) ونڈوز نظام کے ساتھ باہم عمل کرنے کے لئے ضروری گلو فراہم کرتا ہے. اپلی کیشن ونڈوز ڈومین کے انتظام کے ساتھ ساتھ، تصدیق خدمات، فائل اور پرنٹ فراہم کرتا ہے.';
+$lang['samba_app_name'] = 'ونڈوز نیٹ ورکنگ (سامبا)';
+$lang['samba_computers'] = 'کمپیوٹرس';
+$lang['samba_driver_has_not_been_initialized'] = 'ڈرائیور سے initialized نہیں کی گئی ہے.';
+$lang['samba_file_share_tooltip'] = 'آپ کے نیٹ ورک کی فائل حصص پیدا کرنے کے لئے چاہتے ہیں تو، Flexshare اپلی کیشن استعمال کریں، براہ مہربانی.';
+$lang['samba_initializing_core_system'] = 'بنیادی نظام شروع کیا، صبر کریں.';
+$lang['samba_initializing_warning'] = 'ابتدا ایک یا دو منٹ لگ سکتے ہیں، صبر کریں.';
+$lang['samba_master_node_needs_initialization'] = 'ماسٹر نوڈ آگے بڑھنے سے پہلے سے initialized جائے کی ضرورت ہے.';
+$lang['samba_mode'] = 'موڈ';
+$lang['samba_samba_directory_installed_warning'] = 'تم اب اس اپلی کیشن کی ضرورت ہے تاکہ سامبا ڈائرکٹری سافٹ ویئر، نصب کیا جاتا ہے.';
