96a8817028e9ce29ae848bda26bc1a84e3ac7845
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:19:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Administrator Հաշվի';
+$lang['samba_app_description'] = 'Windows Networking (սամբա) ապահովում է անհրաժեշտ սոսինձ interoperate հետ Windows համակարգերի. Հավելվածը տրամադրում իսկությունը ծառայություններ, ֆայլի եւ տպագիր, հետ Windows տիրույթի կառավարման.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Համակարգիչներ';
+$lang['samba_driver_has_not_been_initialized'] = 'Վարորդ չի եղել սկզբնավորված:';
+$lang['samba_file_share_tooltip'] = 'Եթե ​​դուք ցանկանում եք ստեղծել, ցանցային ֆայլի բաժնետոմսերը, խնդրում ենք օգտագործել Flexshare հավելվածը:';
+$lang['samba_initializing_core_system'] = 'Նախապատրաստվում հիմնական համակարգը, խնդրում ենք լինել համբերատար:';
+$lang['samba_initializing_warning'] = 'Կարգաբերումը կարող է տեւել մի րոպե, կամ երկու, խնդրում ենք լինել համբերատար:';
+$lang['samba_master_node_needs_initialization'] = 'Վարպետ հանգույցի պետք է initialized առաջ ընթանում:';
+$lang['samba_mode'] = 'ռեժիմ';
+$lang['samba_samba_directory_installed_warning'] = 'The Samba Ուղեցույց Ծրագիրը տեղադրված է, այնպես որ դուք այլեւս պետք է այս ծրագիրը.';
