e29e5f064a32ef2725368a0364b26564922c0059
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:31:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'administrator Account';
+$lang['samba_app_description'] = 'Windows Networking (Samba) ay nagbibigay ng mga kinakailangang pandikit sa interoperate sa mga sistema ng Windows. Ang app ay nagbibigay ng mga serbisyo authentication, file at i-print, kasama ang Windows pamamahala domain.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Computers';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver ay hindi pa nasimulan.';
+$lang['samba_file_share_tooltip'] = 'Kung nais mong lumikha ng namamahagi ng network file, mangyaring gamitin ang Flexshare app.';
+$lang['samba_initializing_core_system'] = 'Sinisimulan core system, mangyaring maging mahinahon.';
+$lang['samba_initializing_warning'] = 'Initialization ay maaaring tumagal ng isang minuto o dalawa, mangyaring maging mahinahon.';
+$lang['samba_master_node_needs_initialization'] = 'Master node pangangailangan upang maging initialize bago magpatuloy.';
+$lang['samba_mode'] = 'paraan';
+$lang['samba_samba_directory_installed_warning'] = 'Ang Samba Directory software ay naka-install, kaya hindi mo na kailangan ang app na ito.';
