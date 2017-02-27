1138be9a9866ee8e56a0c38d1b77dcbc20c30ca4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:18:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'administrateur rekening';
+$lang['samba_app_description'] = 'Windows Netwerk (Samba) verskaf die nodige gom om versoenbaar met Windows-stelsels. Die app bied verifikasie dienste, lêer en druk, saam met Windows domein beheer.';
+$lang['samba_app_name'] = 'Windows Netwerk (Samba)';
+$lang['samba_computers'] = 'rekenaars';
+$lang['samba_driver_has_not_been_initialized'] = 'Bestuurder is nie geïnisialiseer.';
+$lang['samba_file_share_tooltip'] = 'As jy wil graag &#39;n netwerk lêer aandele te skep, gebruik asseblief die Flexshare inligting.';
+$lang['samba_initializing_core_system'] = 'Inisialiseer kern stelsel, asseblief wees geduldig.';
+$lang['samba_initializing_warning'] = 'Inisialisering kan &#39;n minuut of twee neem, asseblief wees geduldig.';
+$lang['samba_master_node_needs_initialization'] = 'Meester knoop moet geïnisialiseer voordat jy voortgaan.';
+$lang['samba_mode'] = 'Wyse';
+$lang['samba_samba_directory_installed_warning'] = 'Die Samba Gids sagteware geïnstalleer is, sodat jy nie meer hierdie inligting nodig het.';
