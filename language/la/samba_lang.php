6d12dad3531ea9ecd6224166c3b5f08ffd14b996
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:25:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Administrator Ratio';
+$lang['samba_app_description'] = 'Networking Windows (Burana) praebet necessaria ad gluten interoperate Fenestra ratio. A app authenticas operam praebet, et Print file, cum per Fenestra dominico elit.';
+$lang['samba_app_name'] = 'Networking Windows (Burana)';
+$lang['samba_computers'] = 'Home';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver non initialized.';
+$lang['samba_file_share_tooltip'] = 'Si vis creare file participat ipsum, commodo in Flexshare App.';
+$lang['samba_initializing_core_system'] = 'Initializing Core ratio, velit esse patientem.';
+$lang['samba_initializing_warning'] = 'Accipiat Initialization minutis duobus placere patientes.';
+$lang['samba_master_node_needs_initialization'] = 'Node indiget initialized in conspectu Domini procedens.';
+$lang['samba_mode'] = 'Modus';
+$lang['samba_samba_directory_installed_warning'] = 'Directorium software installed in Burana sic ultra hoc app.';
