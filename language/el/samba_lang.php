297306f52e9906676cec0bec2251f77f258a5c9a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:22:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Ο λογαριασμός διαχειριστή';
+$lang['samba_app_description'] = 'Δικτύωση των Windows (Samba) παρέχει την απαραίτητη κόλλα για να διαλειτουργεί με τα συστήματα Windows. Η εφαρμογή παρέχει υπηρεσίες ελέγχου ταυτότητας, αρχείων και εκτυπωτών, καθώς και με τη διαχείριση του τομέα των Windows.';
+$lang['samba_app_name'] = 'Δικτύωση των Windows (Samba)';
+$lang['samba_computers'] = 'Υπολογιστές';
+$lang['samba_driver_has_not_been_initialized'] = 'Οδηγός δεν έχει προετοιμαστεί.';
+$lang['samba_file_share_tooltip'] = 'Εάν θέλετε να δημιουργήσετε κοινόχρηστα στοιχεία αρχείων δικτύου, παρακαλούμε να χρησιμοποιήσετε την εφαρμογή Flexshare.';
+$lang['samba_initializing_core_system'] = 'Προετοιμασία κεντρικό σύστημα, παρακαλώ να είστε υπομονετικοί.';
+$lang['samba_initializing_warning'] = 'Αρχικοποίηση μπορεί να διαρκέσει ένα λεπτό ή δύο, παρακαλώ να είστε υπομονετικοί.';
+$lang['samba_master_node_needs_initialization'] = 'Κύριος κόμβος πρέπει να προετοιμαστεί πριν προχωρήσετε.';
+$lang['samba_mode'] = 'Τρόπος';
+$lang['samba_samba_directory_installed_warning'] = 'Το λογισμικό Samba Directory είναι εγκατεστημένη, οπότε δεν χρειάζεστε πλέον αυτό το app.';
