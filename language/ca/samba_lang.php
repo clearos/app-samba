fe5ba89397378006ed7b8db24282caecd48bb237
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:20:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'compte d&#39;administrador';
+$lang['samba_app_description'] = 'Xarxa de Windows (Samba) proporciona la cola necessària per a interoperar amb sistemes Windows. L&#39;aplicació proporciona serveis d&#39;autenticació, arxius i impressió, juntament amb la gestió de domini de Windows.';
+$lang['samba_app_name'] = 'Xarxa de Windows (Samba)';
+$lang['samba_computers'] = 'ordinadors';
+$lang['samba_driver_has_not_been_initialized'] = 'El controlador no s&#39;ha inicialitzat.';
+$lang['samba_file_share_tooltip'] = 'Per crear recursos compartits d&#39;arxius de xarxa, si us plau feu servir l&#39;aplicació FlexShare.';
+$lang['samba_initializing_core_system'] = 'La inicialització de sistema central, si us plau sigui pacient.';
+$lang['samba_initializing_warning'] = 'La inicialització pot trigar un o dos minuts, si us plau sigui pacient.';
+$lang['samba_master_node_needs_initialization'] = 'node mestre ha de ser inicialitzat abans.';
+$lang['samba_mode'] = 'manera';
+$lang['samba_samba_directory_installed_warning'] = 'El programari de Directory Samba està instal · lat, de manera que ja no necessita aquesta aplicació.';
