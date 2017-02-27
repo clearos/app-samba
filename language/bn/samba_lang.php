61abeac52450f04a57ef89c1d40dd86f13525195
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:20:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'অ্যাডমিনিস্ট্রেটর অ্যাকাউন্ট';
+$lang['samba_app_description'] = 'উইন্ডোজ নেটওয়ার্কিং (সাম্বা) উইন্ডোজ সিস্টেমের সাথে interoperate প্রয়োজনীয় আঠা উপলব্ধ. অ্যাপ্লিকেশন উইন্ডোজ ডোমেইন পরিচালনার সঙ্গে বরাবর, অনুমোদন ব্যবস্থা, ফাইল এবং মুদ্রণ উপলব্ধ.';
+$lang['samba_app_name'] = 'উইন্ডোজ নেটওয়ার্কিং (সাম্বা)';
+$lang['samba_computers'] = 'কম্পিউটার';
+$lang['samba_driver_has_not_been_initialized'] = 'ড্রাইভার সক্রিয়া করা হয়েছে.';
+$lang['samba_file_share_tooltip'] = 'আপনি নেটওয়ার্কের ফাইল শেয়ারের তৈরি করতে চান, তাহলে Flexshare অ্যাপ্লিকেশন ব্যবহার করুন.';
+$lang['samba_initializing_core_system'] = 'কোর সিস্টেম আরম্ভ করা, দয়া করে ধৈর্য ধরুন.';
+$lang['samba_initializing_warning'] = 'আরম্ভের একটি বা দুই মিনিট সময় নিতে পারে, দয়া করে ধৈর্য ধরুন.';
+$lang['samba_master_node_needs_initialization'] = 'মাস্টার নোড অগ্রসর হওয়ার আগে সক্রিয়া করা প্রয়োজন.';
+$lang['samba_mode'] = 'মোড';
+$lang['samba_samba_directory_installed_warning'] = 'সাম্বা নির্দেশিকা সফ্টওয়্যার ইনস্টল করা হয়, তাই আপনি আর এই অ্যাপ্লিকেশন প্রয়োজন.';
