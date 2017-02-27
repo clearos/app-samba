779a5038a56f4a843db3608932ed3e352c77310f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:21:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Ylläpitäjä Tilin';
+$lang['samba_app_description'] = 'Windows Networking (Samba) tarjoaa tarvittavat liimaa yhteensopivia Windows-järjestelmissä. Sovellus tarjoaa todennuspalvelut, tiedostojen ja tulostimien sekä Windows verkkotunnuksen hallinta.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Tietokoneet';
+$lang['samba_driver_has_not_been_initialized'] = 'Kuljettaja ei ole alustettu.';
+$lang['samba_file_share_tooltip'] = 'Jos haluat luoda verkon tiedosto osakkeita, käytä Flexshare sovellusta.';
+$lang['samba_initializing_core_system'] = 'Alustetaan järjestelmän ytimen, olkaa kärsivällisiä.';
+$lang['samba_initializing_warning'] = 'Alustus voi kestää minuutin tai kaksi, olkaa kärsivällisiä.';
+$lang['samba_master_node_needs_initialization'] = 'Master solmu on alustettu ennen jatkamista.';
+$lang['samba_mode'] = 'tila';
+$lang['samba_samba_directory_installed_warning'] = 'Samba-hakemisto ohjelmisto on asennettu, joten sinun ei enää tarvitse tätä app.';
