c7615401cdc4a99607cb314350565671ebce453e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:23:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Rendszergazda fiók';
+$lang['samba_app_description'] = 'A Windows hálózati (Samba) biztosítja a szükséges ragasztót, hogy együttműködjön a Windows rendszerekben. Az alkalmazás hitelesítési szolgáltatásokat, fájl- és nyomtatási, valamint a Windows tartományi vezetés.';
+$lang['samba_app_name'] = 'A Windows hálózati (Samba)';
+$lang['samba_computers'] = 'Számítógépek';
+$lang['samba_driver_has_not_been_initialized'] = 'Illesztőprogram nincs inicializálva.';
+$lang['samba_file_share_tooltip'] = 'Ha szeretne létrehozni hálózati fájlmegosztásokon, kérjük, használja a Flexshare alkalmazást.';
+$lang['samba_initializing_core_system'] = 'Inicializálása központi rendszer, kérjük, legyen türelemmel.';
+$lang['samba_initializing_warning'] = 'Inicializálás eltarthat egy-két percig, kérjük, legyen türelemmel.';
+$lang['samba_master_node_needs_initialization'] = 'Főcsomóponton inicializálni kell, mielőtt belevágna.';
+$lang['samba_mode'] = 'Mód';
+$lang['samba_samba_directory_installed_warning'] = 'A Samba Directory szoftvert telepítve, így nem kell ezt az alkalmazást.';
