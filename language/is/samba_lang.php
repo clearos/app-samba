3be9c351cbc751c985c8707bcbc43654b58ed042
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:24:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'stjórnandi reikningur';
+$lang['samba_app_description'] = 'Windows Networking (Samba) veitir nauðsynlegar límið um samrekstur við Windows kerfi. The app veitir staðfesting þjónustu, skrá og prenta, ásamt Windows ríki stjórnun.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Tölvur';
+$lang['samba_driver_has_not_been_initialized'] = 'Ökumaður hefur ekki verið forsniðin.';
+$lang['samba_file_share_tooltip'] = 'Ef þú vildi eins og til að búa til net skrá hlutabréf, vinsamlegast notaðu Flexshare app.';
+$lang['samba_initializing_core_system'] = 'Frumstilla algerlega kerfi, vinsamlegast sýndu þolinmæði.';
+$lang['samba_initializing_warning'] = 'Frumstilling getur tekið eina mínútu eða tvær, vinsamlegast sýndu þolinmæði.';
+$lang['samba_master_node_needs_initialization'] = 'Master hnút þarf að frumstilla áður en farið er.';
+$lang['samba_mode'] = 'Mode';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directory hugbúnaður er uppsettur, þannig að þú þarft ekki lengur á þetta app.';
