039935a022b6dfe7c27f84873805b1fe96debb24
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:28:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'පරිපාලක ගිණුම';
+$lang['samba_app_description'] = 'වින්ඩෝස් ජාලකරණය (සම්බා) හි Windows සඳහා පද්ධති සමඟ interoperate සඳහා අවශ්ය මැලියම් සපයයි. මෙම යෙදුම නිපදවා ළඟා වින්ඩෝස් ඩොමේන් කළමනාකරණ සමග, සත්යාපනය සේවා, ගොනු හා මුද්රිත සපයයි.';
+$lang['samba_app_name'] = 'වින්ඩෝස් ජාලකරණය (සම්බා)';
+$lang['samba_computers'] = 'පරිගණක';
+$lang['samba_driver_has_not_been_initialized'] = 'රියදුරු ආරම්භනය කර නැත.';
+$lang['samba_file_share_tooltip'] = 'ඔබ ජාල ගොනුවක් කොටස් නිර්මාණය කිරීමට අවශ්ය නම්, එම Flexshare යෙදුම භාවිතා කරන්න.';
+$lang['samba_initializing_core_system'] = 'මූලික පද්ධතිය ආරම්භනය, ඉවසිලිවන්ත වන්න.';
+$lang['samba_initializing_warning'] = 'ආරම්භය විනාඩියක් හෝ දෙකක් ගත විය හැකිය, ඉවසිලිවන්ත වන්න.';
+$lang['samba_master_node_needs_initialization'] = 'මාස්ටර් node එකක් මතම ඊට අදාල කටයුතු කිරීමට පෙර ආරම්භ කල යුතුය.';
+$lang['samba_mode'] = 'මාදිලිය';
+$lang['samba_samba_directory_installed_warning'] = 'මෙම සම්බා නාමාවලිය මෘදුකාංග ස්ථාපනය කර, ඒ නිසා ඔබ තවදුරටත් මෙම යෙදුම අවශ්ය වේ.';
