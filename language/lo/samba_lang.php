20dddbe2b0f2804144b2fd3f0b4a8b37309c4ddd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:25:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'ບັນຊີບໍລິຫານ';
+$lang['samba_app_description'] = 'Windows ລະບົບເຄືອຂ່າຍ (Samba) ໃຫ້ກາວສິ່ງຈໍາເປັນເພື່ອ interoperate ກັບລະບົບ Windows. ການ app ໃຫ້ບໍລິການກວດສອບເອກະສານແລະພິມ, ຄຽງຄູ່ກັບການຈັດການໂດເມນ Windows.';
+$lang['samba_app_name'] = 'Windows ລະບົບເຄືອຂ່າຍ (Samba)';
+$lang['samba_computers'] = 'ຄອມພິວເຕີ';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver ບໍ່ໄດ້ຮັບການເລີ່ມຕົ້ນ.';
+$lang['samba_file_share_tooltip'] = 'ຖ້າຫາກວ່າທ່ານຕ້ອງການທີ່ຈະສ້າງຮຸ້ນໄຟລ໌ເຄືອຂ່າຍ, ກະລຸນາໃຊ້ app Flexshare.';
+$lang['samba_initializing_core_system'] = 'ການເລີ່ມຕົ້ນລະບົບ core, ກະລຸນາຄວາມອົດທົນ.';
+$lang['samba_initializing_warning'] = 'ການເລີ່ມຕົ້ນອາດໃຊ້ເວລານາທີຫຼືສອງ, ກະລຸນາຄວາມອົດທົນ.';
+$lang['samba_master_node_needs_initialization'] = 'ຂໍ້ຕົ້ນສະບັບຕ້ອງການທີ່ຈະໄດ້ຮັບການເລີ່ມຕົ້ນກ່ອນທີ່ຈະດໍາເນີນການ.';
+$lang['samba_mode'] = 'ຮູບແບບການ';
+$lang['samba_samba_directory_installed_warning'] = 'ຊອບແວ Samba Directory ໄດ້ຖືກຕິດຕັ້ງ, ສະນັ້ນທ່ານບໍ່ຈໍາເປັນຕ້ອງ app ນີ້.';
