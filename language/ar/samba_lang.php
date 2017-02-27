6f8034a3d5464df44b8e6ae3e635008c568afd4d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:19:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'حساب المسؤول';
+$lang['samba_app_description'] = 'Windows شبكة (سامبا) على الغراء الضروري للتعامل مع أنظمة ويندوز. ويقدم التطبيق خدمات التوثيق، والملفات والطباعة، جنبا إلى جنب مع إدارة مجال Windows.';
+$lang['samba_app_name'] = 'Windows شبكة (سامبا)';
+$lang['samba_computers'] = 'أجهزة الكمبيوتر';
+$lang['samba_driver_has_not_been_initialized'] = 'لم تتم تهيئة السائق.';
+$lang['samba_file_share_tooltip'] = 'إذا كنت ترغب في إنشاء مشاركات الملفات شبكة، يرجى استخدام التطبيق Flexshare.';
+$lang['samba_initializing_core_system'] = 'تهيئة النظام الأساسي، يرجى التحلي بالصبر.';
+$lang['samba_initializing_warning'] = 'التهيئة قد يستغرق دقيقة أو اثنتين، يرجى التحلي بالصبر.';
+$lang['samba_master_node_needs_initialization'] = 'تحتاج عقدة رئيسية ليتم تهيئتها قبل المتابعة.';
+$lang['samba_mode'] = 'الوضع';
+$lang['samba_samba_directory_installed_warning'] = 'تثبيت البرنامج سامبا دليل، لذلك لم تعد تحتاج هذا التطبيق.';
