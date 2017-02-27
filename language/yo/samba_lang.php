2bdd6f9b947a6f03bb4fca24b9e793130f4366c2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:32:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'IT Account';
+$lang['samba_app_description'] = 'Windows Nẹtiwọki (Samba) pese awọn pataki pọ to interoperate pẹlu Windows awọn ọna šiše. Awọn app pese ìfàṣẹsí iṣẹ, faili ki o si ta, pẹlú pẹlu Windows ašẹ isakoso.';
+$lang['samba_app_name'] = 'Windows Nẹtiwọki (Samba)';
+$lang['samba_computers'] = 'kọmputa';
+$lang['samba_driver_has_not_been_initialized'] = 'Iwakọ ti ko ti initialized.';
+$lang['samba_file_share_tooltip'] = 'Ti o ba ti o ba yoo fẹ lati ṣẹda nẹtiwọki faili mọlẹbi, jọwọ lo awọn Flexshare app.';
+$lang['samba_initializing_core_system'] = 'Initializing mojuto eto, jọwọ jẹ alaisan.';
+$lang['samba_initializing_warning'] = 'Initialization le gba a iseju kan tabi meji, jọwọ jẹ alaisan.';
+$lang['samba_master_node_needs_initialization'] = 'Titunto si ipade nilo lati wa ni initialized ṣaaju ki o to ye.';
+$lang['samba_mode'] = 'mode';
+$lang['samba_samba_directory_installed_warning'] = 'The Samba Directory software ti fi sori ẹrọ, ki o ko si ohun to nilo yi app.';
