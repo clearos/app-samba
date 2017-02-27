b1aa39114cea618e7b291ce6adbfae28592e759a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:29:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'administrator Akun';
+$lang['samba_app_description'] = 'Windows jaringan (Samba) nyadiakeun lem perlu interoperate jeung sistem Windows. app nyadiakeun ladenan auténtikasi, file jeung print, babarengan jeung manajemén domain Windows.';
+$lang['samba_app_name'] = 'Windows jaringan (Samba)';
+$lang['samba_computers'] = 'komputer';
+$lang['samba_driver_has_not_been_initialized'] = 'Supir tacan initialized.';
+$lang['samba_file_share_tooltip'] = 'Lamun hoyong nyieun biasa file jaringan, mangga nganggo aplikasi Flexshare.';
+$lang['samba_initializing_core_system'] = 'Initializing sistim inti, mangga jadi sabar.';
+$lang['samba_initializing_warning'] = 'Initialization butuh hiji atawa dua menit, mangga jadi sabar.';
+$lang['samba_master_node_needs_initialization'] = 'Master titik perlu initialized saméméh lajengkeun.';
+$lang['samba_mode'] = 'mode';
+$lang['samba_samba_directory_installed_warning'] = 'Software Samba Diréktori geus dipasang, ku kituna anjeun teu deui kudu app ieu.';
