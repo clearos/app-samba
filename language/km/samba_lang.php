679aab741cd55e3b17964091b977e65488766caa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:24:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'គណនីអ្នកគ្រប់គ្រង';
+$lang['samba_app_description'] = 'ប្រព័ន្ធប្រតិបត្តិការ Windows បណ្ដាញ (Samba) ផ្តល់នូវការកាវបិទការចាំបាច់ដើម្បី interoperate ជាមួយប្រព័ន្ធ Windows ។ កម្មវិធីនេះបានផ្តល់នូវសេវាការផ្ទៀងផ្ទាត់ភាពត្រឹមត្រូវ, ឯកសារនិងការបោះពុម្ព, ការគ្រប់គ្រងដែនរួមជាមួយនឹងប្រព័ន្ធប្រតិបត្តិការ Windows ។';
+$lang['samba_app_name'] = 'ប្រព័ន្ធប្រតិបត្តិការ Windows បណ្ដាញ (Samba)';
+$lang['samba_computers'] = 'កុំព្យូទ័រ';
+$lang['samba_driver_has_not_been_initialized'] = 'កម្មវិធីបញ្ជាមិនត្រូវបានចាប់ផ្តើមទេ។';
+$lang['samba_file_share_tooltip'] = 'ប្រសិនបើអ្នកចង់បង្កើតការចែករំលែកឯកសារបណ្តាញ, សូមប្រើកម្មវិធី Flexshare ។';
+$lang['samba_initializing_core_system'] = 'ចាប់ផ្ដើមប្រព័ន្ធស្នូលសូមអត់ធ្មត់។';
+$lang['samba_initializing_warning'] = 'ដំបូងអាចនឹងចំណាយមួយឬពីរនាទីសូមអត់ធ្មត់។';
+$lang['samba_master_node_needs_initialization'] = 'ថ្នាំងអនុបណ្ឌិតចាំបាច់ត្រូវរៀបចំមុននឹងបន្ត។';
+$lang['samba_mode'] = 'របៀប';
+$lang['samba_samba_directory_installed_warning'] = 'នេះជាកម្មវិធីថត Samba ត្រូវបានដំឡើងឡើយដូច្នេះអ្នកមិនត្រូវការកម្មវិធីនេះ។';
