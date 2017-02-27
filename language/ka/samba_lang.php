62e118dbdd62f50cc0cdf20fd45290db14079bbc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:24:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Administrator Account';
+$lang['samba_app_description'] = 'Windows ქსელში (Samba) უზრუნველყოფს საჭირო წებო შეეწყონ ერთად Windows სისტემებს. App უზრუნველყოფს ავტორიზაციის მომსახურება, ფაილი და ბეჭდვითი ერთად Windows დომენის მართვა.';
+$lang['samba_app_name'] = 'Windows ქსელში (Samba)';
+$lang['samba_computers'] = 'კომპიუტერები';
+$lang['samba_driver_has_not_been_initialized'] = 'მძღოლი არ ინიციალიზაცია.';
+$lang['samba_file_share_tooltip'] = 'თუ გსურთ, რათა შეიქმნას ქსელის ფაილი აქციების, გთხოვთ, გამოიყენოთ Flexshare app.';
+$lang['samba_initializing_core_system'] = 'ინიციალიზება ძირითადი სისტემა, გთხოვთ მოთმინება.';
+$lang['samba_initializing_warning'] = 'Initialization შეუძლია მიიღოს ერთი წუთით ან ორი, გთხოვთ მოთმინება.';
+$lang['samba_master_node_needs_initialization'] = 'ოსტატი კვანძის საჭიროებს ინიციალიზაცია გაგრძელებამდე.';
+$lang['samba_mode'] = 'რეჟიმი';
+$lang['samba_samba_directory_installed_warning'] = 'სამბა დირექტორია პროგრამული დამონტაჟებულია, ასე რომ თქვენ აღარ უნდა ამ app.';
