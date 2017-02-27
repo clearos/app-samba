d58b834578817338a50bbf28f206ded6f6276584
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:22:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Conta de administrador';
+$lang['samba_app_description'] = 'Rede Windows (Samba) ofrece a cola necesaria para interoperar con sistemas Windows. A aplicación ofrece servizos de identificación, arquivo e impresión, xunto coa xestión de dominio de Windows.';
+$lang['samba_app_name'] = 'Rede Windows (Samba)';
+$lang['samba_computers'] = 'informática';
+$lang['samba_driver_has_not_been_initialized'] = 'Controlador non foi iniciado.';
+$lang['samba_file_share_tooltip'] = 'Se desexa crear compartilhamentos de arquivos de rede, por favor, use a aplicación FlexShare.';
+$lang['samba_initializing_core_system'] = 'Arrincar núcleo do sistema, por favor, sexa paciente.';
+$lang['samba_initializing_warning'] = 'Arranque pode levar un minuto ou dous, por favor, sexa paciente.';
+$lang['samba_master_node_needs_initialization'] = 'nó mestre debe ser iniciado antes de continuar.';
+$lang['samba_mode'] = 'modo';
+$lang['samba_samba_directory_installed_warning'] = 'O software Directorio Samba se instala, así que non deste programa.';
