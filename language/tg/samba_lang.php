455753ea50c470ed44f19bb35dce15872aa3066b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:30:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Ҳисоби Administrator';
+$lang['samba_app_description'] = 'Windows Networking (Samba) ба ширеше зарур interoperate бо системаҳои Windows таъмин менамояд. Дар барнома аз хизматрасониҳои аутентификатсия, файл ва чоп, дар якҷоягӣ бо идоракунии домени Windows.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Компютерҳо';
+$lang['samba_driver_has_not_been_initialized'] = 'Ронандаи кардааст initialized нашудааст.';
+$lang['samba_file_share_tooltip'] = 'Агар шумо мехоҳед ба эҷоди файли саҳмияҳои шабака, лутфан истифода баред барнома Flexshare.';
+$lang['samba_initializing_core_system'] = 'Initializing системаи аслӣ, лутфан сабр кун.';
+$lang['samba_initializing_warning'] = 'Initialization метавонад аз як ё ду дақиқа мегирад, лутфан сабр кун.';
+$lang['samba_master_node_needs_initialization'] = 'Master гиреҳ бояд пеш аз кӯчонидани initialized шавад.';
+$lang['samba_mode'] = 'ҳолати';
+$lang['samba_samba_directory_installed_warning'] = 'Нармафзор Samba Directory гузошта шуда бошад, пас шумо дигар лозим аст, ин барнома.';
