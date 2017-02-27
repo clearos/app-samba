6debc1a20d985749aae9f994f62fe1a53b828df6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:20:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Account woyang&#39;anira';
+$lang['samba_app_description'] = 'Windows Intaneti (Samba) amatipatsa zomatira zofunika interoperate ndi kachitidwe mawindo. app limapereka chithandizo kutsimikizika, file ndi kusindikiza, pamodzi ndi kasamalidwe Windows ankalamulira.';
+$lang['samba_app_name'] = 'Windows Intaneti (Samba)';
+$lang['samba_computers'] = 'makompyuta';
+$lang['samba_driver_has_not_been_initialized'] = 'Dalaivala sichinayambe initialized.';
+$lang['samba_file_share_tooltip'] = 'Ngati mukufuna kulenga magawo Intaneti file, chonde ntchito pulogalamu Flexshare.';
+$lang['samba_initializing_core_system'] = 'Potsegula dongosolo pachimake, chonde pirira.';
+$lang['samba_initializing_warning'] = 'Initialization akhoza kutenga mphindi imodzi kapena ziwiri, chonde pirira.';
+$lang['samba_master_node_needs_initialization'] = 'Master mfundo ayenera initialized akuyendera.';
+$lang['samba_mode'] = 'akafuna';
+$lang['samba_samba_directory_installed_warning'] = 'Mapulogalamu Samba Directory waikidwa, kotero inu sipafinikanso pulogalamu.';
