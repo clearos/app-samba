eeeaa867c8b66a713b0b7d60353ab5c1d04c8ad7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:19:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Administrator hesab';
+$lang['samba_app_description'] = 'Windows Networking (Samba) Windows sistemi ilə çalışacak üçün zəruri yapışqan edir. app Windows domain rəhbərliyi ilə yanaşı, identifikasiyası xidmətləri, fayl və çap edir.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Computers';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver başlatılmadı deyil.';
+$lang['samba_file_share_tooltip'] = 'Siz şəbəkə fayl səhmlərin yaratmaq istəyirsinizsə, Flexshare app istifadə edin.';
+$lang['samba_initializing_core_system'] = 'core sistemi başlatılıyor, səbr edin.';
+$lang['samba_initializing_warning'] = 'Başlatma bir dəqiqə və ya iki bilər, səbr edin.';
+$lang['samba_master_node_needs_initialization'] = 'Master node davam əvvəl başlatılmış lazımdır.';
+$lang['samba_mode'] = 'Mode';
+$lang['samba_samba_directory_installed_warning'] = 'Siz artıq bu app lazımdır, belə ki, Samba Directory software, yüklü.';
