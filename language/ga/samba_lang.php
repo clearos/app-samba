b2b17b10367c7321198be0d3dcab2b556dbee59b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:23:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Cuntas riarthóir';
+$lang['samba_app_description'] = 'Soláthraíonn Windows Líonrú (Samba) an gliú is gá chun interoperate le córais Windows. Soláthraíonn an app seirbhísí fíordheimhnithe, comhad agus a phriontáil, mar aon le bainistíocht fearainn Windows.';
+$lang['samba_app_name'] = 'Windows Líonrú (Samba)';
+$lang['samba_computers'] = 'Ríomhairí';
+$lang['samba_driver_has_not_been_initialized'] = 'Nach bhfuil tiománaí initialized.';
+$lang['samba_file_share_tooltip'] = 'Más mian leat a chruthú scaireanna comhad líonra, bain úsáid as an app Flexshare.';
+$lang['samba_initializing_core_system'] = 'Initializing córas lárnach, le do thoil a n-othar.';
+$lang['samba_initializing_warning'] = 'D&#39;fhéadfadh initialization ghlacadh nóiméad nó dhó, le do thoil a n-othar.';
+$lang['samba_master_node_needs_initialization'] = 'Ní mór máistir nód a initialized roimh dul ar aghaidh.';
+$lang['samba_mode'] = 'mód';
+$lang['samba_samba_directory_installed_warning'] = 'Is é an bogearraí Samba Eolaire suiteáilte, mar sin leat a thuilleadh gá an app.';
