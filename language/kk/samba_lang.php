faae3ac1a21fef94faa34c1b79176bd7d5b47592
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:24:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Әкімші шот';
+$lang['samba_app_description'] = 'Windows Networking (Samba) Windows жүйелерімен өзара қажетті желім ұсынады. Қолданба Windows домен басқару бірге, аутентификация қызметтерді, файлдар мен басып шығарғышты қамтамасыз етеді.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Компьютерлер';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver бапталғаннан жоқ.';
+$lang['samba_file_share_tooltip'] = 'Егер сіз желілік файл акцияларын жасау келсе, Flexshare қолданбасын пайдаланыңыз.';
+$lang['samba_initializing_core_system'] = 'негізгі жүйесін баптандыру, өтінеміз шыдамды.';
+$lang['samba_initializing_warning'] = 'Инициализации бір минут немесе екі кетуі мүмкін, өтінеміз шыдамды.';
+$lang['samba_master_node_needs_initialization'] = 'Мастер торап жалғастыру алдында баптандырылады қажет.';
+$lang['samba_mode'] = 'Mode';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directory бағдарламалық қамтамасыз ету орнатылған, сондықтан сіз енді бұл қолданбаны қажет.';
