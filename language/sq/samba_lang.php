d338bd51978d6d7ec1b0a58c0a1b4f78f32b7f0e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:19:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Llogaria Administrator';
+$lang['samba_app_description'] = 'Windows Networking (Samba) siguron zam nevojshme për të ndërveprojnë me sistemin Windows. Aplikacioni ofron shërbime të legalizuara, fotografi dhe të shtypura, së bashku me menaxhimin domain Windows-it.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Kompjuter';
+$lang['samba_driver_has_not_been_initialized'] = 'Shoferi nuk ka nisur.';
+$lang['samba_file_share_tooltip'] = 'Nëse ju do të donte për të krijuar rrjeti fotografi aksione, ju lutem, përdorni app Flexshare.';
+$lang['samba_initializing_core_system'] = 'Fillimin e sistemit bazë, ju lutem të jetë i durueshëm.';
+$lang['samba_initializing_warning'] = 'Initialization mund të marrë një minutë apo dy, ju lutemi të jetë i durueshëm.';
+$lang['samba_master_node_needs_initialization'] = 'nyje Master duhet të inicializohet para se të vazhdoni.';
+$lang['samba_mode'] = 'mënyrë';
+$lang['samba_samba_directory_installed_warning'] = 'Software Samba Directory është i instaluar, kështu që ju nuk ka më nevojë këtë app.';
