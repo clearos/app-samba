0c30df4bf976b07711ecec41eb7e99250dc36fe5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:23:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'administrator Akun';
+$lang['samba_app_description'] = 'Jendela Networking (Samba) menyediakan lem yang diperlukan untuk beroperasi dengan sistem Windows. Aplikasi ini menyediakan layanan otentikasi, file dan cetak, bersama dengan manajemen domain Windows.';
+$lang['samba_app_name'] = 'Jendela Networking (Samba)';
+$lang['samba_computers'] = 'komputer';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver belum diinisialisasi.';
+$lang['samba_file_share_tooltip'] = 'Jika Anda ingin membuat file saham jaringan, silakan gunakan aplikasi Flexshare.';
+$lang['samba_initializing_core_system'] = 'Inisialisasi sistem inti, harap bersabar.';
+$lang['samba_initializing_warning'] = 'Inisialisasi dapat mengambil satu atau dua menit, harap bersabar.';
+$lang['samba_master_node_needs_initialization'] = 'Node master perlu diinisialisasi sebelum melanjutkan.';
+$lang['samba_mode'] = 'Mode';
+$lang['samba_samba_directory_installed_warning'] = 'Perangkat lunak Samba Directory diinstal, sehingga Anda tidak perlu lagi aplikasi ini.';
