6b812b55d4af651604a33631019c5be7d51f0c59
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:23:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'प्रशासक खाता';
+$lang['samba_app_description'] = 'विंडोज नेटवर्किंग (सांबा) विंडोज सिस्टम के साथ interoperate करने के लिए आवश्यक गोंद प्रदान करता है। एप्लिकेशन Windows डोमेन प्रबंधन के साथ-साथ, प्रमाणीकरण सेवाएं, फ़ाइल और प्रिंट प्रदान करता है।';
+$lang['samba_app_name'] = 'विंडोज नेटवर्किंग (सांबा)';
+$lang['samba_computers'] = 'कम्प्यूटर';
+$lang['samba_driver_has_not_been_initialized'] = 'ड्राइवर प्रारंभ नहीं किया गया है।';
+$lang['samba_file_share_tooltip'] = 'आप नेटवर्क फ़ाइल साझा बनाने के लिए चाहते हैं, तो Flexshare एप्लिकेशन का इस्तेमाल करें।';
+$lang['samba_initializing_core_system'] = 'कोर प्रणाली आरंभ किया जा रहा है, कृपया धैर्य रखें।';
+$lang['samba_initializing_warning'] = 'प्रारंभ एक या दो मिनट का समय लग सकता है, कृपया धैर्य रखें।';
+$lang['samba_master_node_needs_initialization'] = 'मास्टर नोड आगे बढ़ने से पहले प्रारंभ की जा करने की जरूरत है।';
+$lang['samba_mode'] = 'मोड';
+$lang['samba_samba_directory_installed_warning'] = 'सांबा निर्देशिका सॉफ्टवेयर स्थापित किया गया है, ताकि आप अब इस अनुप्रयोग की जरूरत है।';
