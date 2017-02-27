692d286aeb8dd702264a3640a09d55fa5abed264
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:32:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'ngasebenzi';
+$lang['samba_app_description'] = 'Windows Networking (Samba) inikeza glue ezidingekayo yokusebenza Windows izinhlelo. Uhlelo lokusebenza inikeza ngamasevisi ubuqiniso, ifayela futhi uprinte, kanye Windows ukuphathwa kwesizinda.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Ezamakhompiyutha';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver engakaze ukuqaliswa.';
+$lang['samba_file_share_tooltip'] = 'Uma ungathanda ukudala amasheya ifayela inethiwekhi, sicela usebenzise uhlelo lokusebenza Flexshare.';
+$lang['samba_initializing_core_system'] = 'Iqalisa core uhlelo, sicela ubekezele.';
+$lang['samba_initializing_warning'] = 'Ukuqalisa kungathatha umzuzu noma emibili, sicela ubekezele.';
+$lang['samba_master_node_needs_initialization'] = 'Master Node idinga ukuba ukuqaliswa ngaphambi kokuqhubeka.';
+$lang['samba_mode'] = 'Mode';
+$lang['samba_samba_directory_installed_warning'] = 'Isofthiwe Samba Directory efakwe, ngakho wena akusadingeki le app.';
