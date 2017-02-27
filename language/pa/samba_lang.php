a5cdbb0ca3457979c0de71ce771194ff8fd621bf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:27:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'ਪਰਬੰਧਕ ਖਾਤਾ';
+$lang['samba_app_description'] = 'Windows ਨੈੱਟਵਰਕ (Samba) ਵਿੰਡੋ ਸਿਸਟਮ ਨਾਲ ਚੱਲ ਸਕਣ ਲਈ ਜ਼ਰੂਰੀ ਗਲੂ ਪ੍ਰਦਾਨ ਕਰਦਾ ਹੈ. ਐਪਲੀਕੇਸ਼ ਨੂੰ ਡੋਮੇਨ ਪ੍ਰਬੰਧਨ ਦੇ ਨਾਲ-ਨਾਲ, ਪ੍ਰਮਾਣਿਕਤਾ ਸੇਵਾ, ਫਾਇਲ ਅਤੇ ਪ੍ਰਿੰਟ ਪ੍ਰਦਾਨ ਕਰਦਾ ਹੈ.';
+$lang['samba_app_name'] = 'Windows ਨੈੱਟਵਰਕ (Samba)';
+$lang['samba_computers'] = 'ਕੰਪਿਊਟਰ';
+$lang['samba_driver_has_not_been_initialized'] = 'ਡਰਾਈਵਰ ਸ਼ੁਰੂ ਨਾ ਕੀਤਾ ਗਿਆ ਹੈ.';
+$lang['samba_file_share_tooltip'] = 'ਤੁਹਾਨੂੰ ਨੈੱਟਵਰਕ ਫਾਇਲ ਸ਼ੇਅਰ ਬਣਾਉਣ ਲਈ ਚਾਹੁੰਦੇ ਹੋ, Flexshare ਐਪਲੀਕੇਸ਼ ਨੂੰ ਵਰਤਣ, ਕਿਰਪਾ ਕਰਕੇ.';
+$lang['samba_initializing_core_system'] = 'ਕੋਰ ਸਿਸਟਮ ਸ਼ੁਰੂ ਕੀਤਾ ਹੈ, ਕਿਰਪਾ ਕਰਕੇ ਮਰੀਜ਼ ਹੋ.';
+$lang['samba_initializing_warning'] = 'ਸ਼ੁਰੂਆਤ ਇਕ-ਦੋ ਮਿੰਟ ਲੱਗ ਸਕਦਾ ਹੈ, ਕਿਰਪਾ ਕਰਕੇ ਮਰੀਜ਼ ਹੋ.';
+$lang['samba_master_node_needs_initialization'] = 'ਮਾਸਟਰ ਨੋਡ ਜਾਰੀ ਅੱਗੇ ਸ਼ੁਰੂ ਕਰਨ ਦੀ ਲੋੜ ਹੈ.';
+$lang['samba_mode'] = 'ਮੋਡ';
+$lang['samba_samba_directory_installed_warning'] = 'Samba ਡਾਇਰੈਕਟਰੀ ਸਾਫਟਵੇਅਰ ਇੰਸਟਾਲ ਹੈ, ਇਸ ਲਈ ਤੁਹਾਨੂੰ ਕੋਈ ਵੀ ਹੁਣ ਇਸ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਲੋੜ ਹੈ.';
