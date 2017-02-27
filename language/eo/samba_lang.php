645558fa86872c164f2ec1da153e2dec651d1537
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:21:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'administranto Konto';
+$lang['samba_app_description'] = 'Fenestroj Interkonektanta (Sambo) provizas la necesan gluo por interoperar kun Windows sistemoj. La app provizas identigan servoj, arkivo kaj presaĵo, kune kun Vindozo havaĵo demarŝo.';
+$lang['samba_app_name'] = 'Fenestroj Interkonektanta (Samba)';
+$lang['samba_computers'] = 'komputiloj';
+$lang['samba_driver_has_not_been_initialized'] = 'Ŝoforo ne estis inicializado.';
+$lang['samba_file_share_tooltip'] = 'Se vi ŝatus krei reton dosiero akcioj, bonvolu uzi la Flexshare app.';
+$lang['samba_initializing_core_system'] = 'Inicializar kerna sistemo, bonvolu esti pacienca.';
+$lang['samba_initializing_warning'] = 'Inicialización povas preni momenton aŭ du, bonvolu esti pacienca.';
+$lang['samba_master_node_needs_initialization'] = 'Mastro nodo devas esti inicializado antaŭ ol vi antaŭeniru.';
+$lang['samba_mode'] = 'mode';
+$lang['samba_samba_directory_installed_warning'] = 'La Samba Gvidlibro programaro estas instalita, do vi ne bezonas tiun programon.';
