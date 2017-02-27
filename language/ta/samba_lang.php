c9161bc610facdfd63f6d3dcff1ad6c7b7b50fbf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:30:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'நிர்வாகி கணக்கு';
+$lang['samba_app_description'] = 'விண்டோஸ் வலையமைப்பு (சம்பா) விண்டோஸ் அமைப்புகள் இரண்டையும் ஒன்றுடன் ஒன்று அவசியம் பசை வழங்குகிறது. பயன்பாட்டை விண்டோஸ் டொமைன் மேலாண்மை இணைந்து, அங்கீகார சேவைகள், கோப்பு மற்றும் அச்சு வழங்குகிறது.';
+$lang['samba_app_name'] = 'விண்டோஸ் வலையமைப்பு (சம்பா)';
+$lang['samba_computers'] = 'கணனிகள்';
+$lang['samba_driver_has_not_been_initialized'] = 'டிரைவர் துவக்கப்படும் இல்லை.';
+$lang['samba_file_share_tooltip'] = 'நீங்கள் பிணைய கோப்பு பங்குகளை உருவாக்க விரும்பினால், Flexshare பயன்பாட்டை பயன்படுத்த தயவு செய்து.';
+$lang['samba_initializing_core_system'] = 'மைய அமைப்பு துவக்குகிறது, பொறுமை, தயவு செய்து.';
+$lang['samba_initializing_warning'] = 'துவக்கும் ஒரு நிமிடம் அல்லது இரண்டு ஆகலாம், பொறுமை, தயவு செய்து.';
+$lang['samba_master_node_needs_initialization'] = 'மாஸ்டர் முனை தொடர்வதற்கு முன் துவக்க வேண்டும்.';
+$lang['samba_mode'] = 'முறை';
+$lang['samba_samba_directory_installed_warning'] = 'எனவே இனி நீங்கள் இந்த பயன்பாட்டை வேண்டும் சம்பா டைரக்டரி மென்பொருளால், நிறுவப்படும்.';
