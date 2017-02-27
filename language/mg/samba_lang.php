5f0b0dd84fc734d79febdf17e5e6001fcbedabe3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:25:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Administrator Account';
+$lang['samba_app_description'] = 'Windows Fifaneraserana (Samba) dia manome ny ilaina lakaoly mba interoperate amin&#39;ny Windows rafitra. Ny fampiharana manome asa fanamarinana, rakitra sy pirinty, miaraka amin&#39;ny Windows sehatra fitantanana.';
+$lang['samba_app_name'] = 'Windows Fifaneraserana (Samba)';
+$lang['samba_computers'] = 'Computers';
+$lang['samba_driver_has_not_been_initialized'] = 'Mpamily no tsy initialized.';
+$lang['samba_file_share_tooltip'] = 'Raha te-hamorona tambajotra rakitra mizara, azafady mampiasa ny Flexshare fampiharana.';
+$lang['samba_initializing_core_system'] = 'Nitranga teo am-rafitra fototra, azafady hanam-paharetana.';
+$lang['samba_initializing_warning'] = 'Initialization Mety ho iray minitra na roa, azafady hanam-paharetana.';
+$lang['samba_master_node_needs_initialization'] = 'Master node tokony initialized alohan&#39;ny.';
+$lang['samba_mode'] = 'Mode';
+$lang['samba_samba_directory_installed_warning'] = 'Ny Samba Directory rindrambaiko no nametraka, mba tsy mila fampiharana izany.';
