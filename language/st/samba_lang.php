de78b0ea2e6305d5a51a5fa4a34143e2f21fd628
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:29:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Account mohlokomeli oa';
+$lang['samba_app_description'] = 'Windows Inthaneteng (Samba) fana ka sekhomaretsi ho hlokahala hore ho interoperate le litsamaiso Windows. app e e fana ka ditshebeletso tsa netefatso, faele le ngotsoeng, hammoho le taolo ya Windows domain name.';
+$lang['samba_app_name'] = 'Windows Inthaneteng (Samba)';
+$lang['samba_computers'] = 'lik&#39;homphieutha';
+$lang['samba_driver_has_not_been_initialized'] = 'Mokhanni ha e so initialized.';
+$lang['samba_file_share_tooltip'] = 'Haeba u ka rata ho bōpa diabo tsa netweke ya faele, ka kōpo sebelisa app e sa Flexshare.';
+$lang['samba_initializing_core_system'] = 'Initializing tsamaiso ea mantlha e, ka kōpo ba le mamello.';
+$lang['samba_initializing_warning'] = 'Initialization ka nka motsotso kapa e &#39;meli, ka kōpo ba le mamello.';
+$lang['samba_master_node_needs_initialization'] = 'Master noute e lokela ho initialized pele ba tsoela pele.';
+$lang['samba_mode'] = 'Mode';
+$lang['samba_samba_directory_installed_warning'] = 'Software Samba Directory e hlomamisa, kahoo u ba hlola ba hloka app e lena.';
