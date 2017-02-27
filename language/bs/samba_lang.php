34e2fc541ba653c3609dda0d2159a730ea3df14d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:20:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Administrator Account';
+$lang['samba_app_description'] = 'Windows Networking (Samba) osigurava potrebnu ljepilo za interoperabilnost s Windows sistemima. Aplikacija pruža autentifikaciju usluge, datoteke i ispis, uz upravljanje Windows domenom.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Računala';
+$lang['samba_driver_has_not_been_initialized'] = 'Vozač nije inicijaliziran.';
+$lang['samba_file_share_tooltip'] = 'Ako želite stvoriti akcija mrežu datoteka, molimo vas da koristite Flexshare aplikaciju.';
+$lang['samba_initializing_core_system'] = 'Inicijalizacija core sistem, molimo vas da budete strpljivi.';
+$lang['samba_initializing_warning'] = 'Inicijalizacije može uzeti minutu ili dvije, molim vas da budete strpljivi.';
+$lang['samba_master_node_needs_initialization'] = 'Master čvor se mora inicijalizirati prije nastavka.';
+$lang['samba_mode'] = 'način';
+$lang['samba_samba_directory_installed_warning'] = 'Softver Samba Directory je instaliran, tako da više ne treba ovu aplikaciju.';
