25cdd47ff0729ab753cea9cc96733c1c21695362
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:25:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Administrator konts';
+$lang['samba_app_description'] = 'Windows Networking (Samba) nodrošina nepieciešamo līmi ir savietojami ar Windows sistēmām. App sniedz autentificēšanas pakalpojumus, failu un drukāt, kopā ar Windows domēna pārvaldību.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Datori';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver nav inicializēts.';
+$lang['samba_file_share_tooltip'] = 'Ja jūs vēlētos, lai radītu tīkla failu akcijas, lūdzu, izmantojiet Flexshare lietotni.';
+$lang['samba_initializing_core_system'] = 'Inicializēšana pamatsistēmu, lūdzu, esiet pacietīgi.';
+$lang['samba_initializing_warning'] = 'Inicializācijas var aizņemt minūti vai divas, lūdzu, esiet pacietīgi.';
+$lang['samba_master_node_needs_initialization'] = 'Master mezglā jābūt inicializēts pirms turpināt.';
+$lang['samba_mode'] = 'mode';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directory programmatūra ir instalēta, tāpēc jums vairs nav nepieciešams šo app.';
