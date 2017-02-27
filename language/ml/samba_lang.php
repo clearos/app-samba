493f33613e63d9495e35f79bdbe5c005d83faf65
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:26:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'അഡ്മിനിസ്ട്രേറ്റർ അക്കൌണ്ട്';
+$lang['samba_app_description'] = 'Windows നെറ്റ്വർക്കിങ് (സാംബാ) വിൻഡോസ് സിസ്റ്റങ്ങളിൽ പരസ്പരം ആവശ്യമായ പശ നൽകുന്നു. അപ്ലിക്കേഷൻ വിൻഡോസ് ഡൊമെയിൻ മാനേജ്മെന്റ് ചേർന്ന് ആധികാരികത, ഫയൽ പ്രിന്റ് നൽകുന്നു.';
+$lang['samba_app_name'] = 'Windows നെറ്റ്വർക്കിങ് (സാംബാ)';
+$lang['samba_computers'] = 'കംപ്യൂട്ടർ';
+$lang['samba_driver_has_not_been_initialized'] = 'ഡ്രൈവർ ആരംഭിച്ചു ചെയ്തിട്ടില്ല.';
+$lang['samba_file_share_tooltip'] = 'നിങ്ങൾ നെറ്റ്വർക്ക് ഫയൽ ഓഹരികൾ സൃഷ്ടിക്കാൻ താൽപ്പര്യമുണ്ടെങ്കിൽ, Flexshare അപ്ലിക്കേഷൻ ഉപയോഗിക്കുക.';
+$lang['samba_initializing_core_system'] = 'കോർ സിസ്റ്റം ആരംഭിക്കുന്നു, ദയവായി ക്ഷമയോടെ.';
+$lang['samba_initializing_warning'] = 'ഇനീഷ്യലൈസേഷന് ഒന്നോ രണ്ടോ മിനിട്ട് എടുത്തേക്കാം, ദയവായി ക്ഷമയോടെ.';
+$lang['samba_master_node_needs_initialization'] = 'മാസ്റ്റർ നോഡ് മുന്നോട്ടുപോകുന്നതിന് മുമ്പ് ഇനിഷ്യലൈസ് ആവശ്യമുണ്ട്.';
+$lang['samba_mode'] = 'മോഡ്';
+$lang['samba_samba_directory_installed_warning'] = 'സാംബാ ഡയറക്ടറി സോഫ്റ്റ്വെയർ ഇൻസ്റ്റാൾ, അങ്ങനെ നീ ഇനി ഈ അപ്ലിക്കേഷൻ ആവശ്യമാണ്.';
