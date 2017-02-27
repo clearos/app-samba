31c62a7e11648953b2e4abafbaf6921ac9bf305f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:29:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'účet správcu';
+$lang['samba_app_description'] = 'Windows siete (Samba) zabezpečuje potrebnú lepidlo spolupracovať so systémami Windows. Aplikácia poskytuje služby overovania, zdieľanie súborov a tlačiarní, spolu s vedením domény systému Windows.';
+$lang['samba_app_name'] = 'Windows siete (Samba)';
+$lang['samba_computers'] = 'Počítače';
+$lang['samba_driver_has_not_been_initialized'] = 'Vodič nebol inicializovaný.';
+$lang['samba_file_share_tooltip'] = 'Ak by ste chceli vytvoriť zdieľanie súborov siete, použite aplikáciu FlexShare.';
+$lang['samba_initializing_core_system'] = 'Inicializácia hlavného systému, prosím, buďte trpezliví.';
+$lang['samba_initializing_warning'] = 'Inicializácia môže trvať minútu alebo dve, prosím, buďte trpezliví.';
+$lang['samba_master_node_needs_initialization'] = 'Hlavný uzol musí byť inicializovaný pred pokračovaním.';
+$lang['samba_mode'] = 'režim';
+$lang['samba_samba_directory_installed_warning'] = 'Tento softvér Samba Adresár je nainštalovaný, takže už túto aplikáciu je potrebné.';
