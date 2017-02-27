d85cdd95d97f56422fcfcad08ec0fb17a75d4d14
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:23:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Administrator Account';
+$lang['samba_app_description'] = 'Windows Networking (Samba) na-enye ndị dị mkpa mama ka interoperate na Windows usoro. The ngwa na-enye Nyocha ọrụ, faịlụ na-ebipụta, tinyere Windows domain management.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Computers';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver bụghị e initialized.';
+$lang['samba_file_share_tooltip'] = 'Ọ bụrụ na ị ga-achọ ka ịmepụta netwọk file mbak, biko jiri Flexshare ngwa.';
+$lang['samba_initializing_core_system'] = 'Initializing isi usoro, biko na-enwe ndidi.';
+$lang['samba_initializing_warning'] = 'Initialization nwere ike iwe nkeji ma ọ bụ abụọ, biko na-enwe ndidi.';
+$lang['samba_master_node_needs_initialization'] = 'Master ọnụ kwesịrị ka a initialized tupu ya etinyewe.';
+$lang['samba_mode'] = 'mode';
+$lang['samba_samba_directory_installed_warning'] = 'The Samba Directory software arụnyere, otú ị na-adịkwaghị mkpa a na ngwa.';
