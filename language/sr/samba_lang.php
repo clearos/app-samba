9fb4316abb27139e450bd2063ef22ff2087bbf70
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:28:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'администратор рачуна';
+$lang['samba_app_description'] = 'Виндовс умрежавање (Самба) обезбеђује потребну лепак за сарадњу са Виндовс системима. Апликација пружа услуге Аутхентицатион, и одштампа, заједно са Виндовс Манагемент домена.';
+$lang['samba_app_name'] = 'Виндовс умрежавање (Самба)';
+$lang['samba_computers'] = 'Компјутери';
+$lang['samba_driver_has_not_been_initialized'] = 'Возач није покренут.';
+$lang['samba_file_share_tooltip'] = 'Ако желите да направите акција Нетворк Филе, користите Флексхаре апликацију.';
+$lang['samba_initializing_core_system'] = 'Инитиализинг главни систем, будите стрпљиви.';
+$lang['samba_initializing_warning'] = 'Инитиализатион може да потраје минут или два, будите стрпљиви.';
+$lang['samba_master_node_needs_initialization'] = 'Мастер чвор треба да буде покренут пре него што наставите.';
+$lang['samba_mode'] = 'начин';
+$lang['samba_samba_directory_installed_warning'] = 'Самба Директоријум софтвер инсталиран, тако да више не треба ову апликацију.';
