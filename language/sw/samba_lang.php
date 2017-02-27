5ac0e93249a11e8c93fbc433028a4fc5d03e54c9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:30:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Akaunti msimamizi';
+$lang['samba_app_description'] = 'Windows Networking (Samba) hutoa gundi muhimu interoperate na mifumo Windows. programu hutoa huduma ya kujitambulisha, faili na magazeti, pamoja na usimamizi wa Windows domain.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Kompyuta';
+$lang['samba_driver_has_not_been_initialized'] = 'Dereva haijawahi initialized.';
+$lang['samba_file_share_tooltip'] = 'Kama ungependa kujenga hisa mtandao file, tafadhali kutumia programu Flexshare.';
+$lang['samba_initializing_core_system'] = 'Initializing mfumo wa msingi, tafadhali kuwa na subira.';
+$lang['samba_initializing_warning'] = 'Initialization inaweza kuchukua dakika moja au mbili, tafadhali kuwa na subira.';
+$lang['samba_master_node_needs_initialization'] = 'Mwalimu node inahitaji kuanzishwa kabla ya kuendelea.';
+$lang['samba_mode'] = 'mode';
+$lang['samba_samba_directory_installed_warning'] = 'programu Samba Saraka imewekwa, hivyo hakuna tena haja ya programu hii.';
