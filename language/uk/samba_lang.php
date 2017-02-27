04569bdbc9b223289afd3dc06265436a713bff37
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:31:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Обліковий запис адміністратора';
+$lang['samba_app_description'] = 'Мереж Windows (Samba) забезпечує необхідний клей для взаємодії з системами Windows. Додаток надає послуги аутентифікації, файлів і друку, поряд з управлінням домену Windows.';
+$lang['samba_app_name'] = 'Мереж Windows (Samba)';
+$lang['samba_computers'] = 'комп&#39;ютери';
+$lang['samba_driver_has_not_been_initialized'] = 'Водій не був инициализирован.';
+$lang['samba_file_share_tooltip'] = 'Якщо ви хотіли б створити акції мережі файлів, необхідно використовувати додаток FlexShare.';
+$lang['samba_initializing_core_system'] = 'Ініціалізація ядра системи, будь ласка, будьте терплячі.';
+$lang['samba_initializing_warning'] = 'Ініціалізація може зайняти хвилину або дві, будь ласка, будьте терплячі.';
+$lang['samba_master_node_needs_initialization'] = 'Майстер вузол слід буде почати, перш ніж продовжити.';
+$lang['samba_mode'] = 'режим';
+$lang['samba_samba_directory_installed_warning'] = 'Програмне забезпечення Samba каталог встановлюється, так що вам більше не потрібно це додаток.';
