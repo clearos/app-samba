3cec898a909fbc12d1ea5661bf23281a9f55a44e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:19:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/samba_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['samba_administrator_account'] = 'Administratzailearen kontua';
+$lang['samba_app_description'] = 'Windows Networking (Samba) beharrezko kola to Windows sistema batzuekin elkar eskaintzen. Aplikazioa autentifikazio zerbitzuak, fitxategi eta inprimatu eskaintzen, Windows domeinu kudeaketa batera.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Ordenagailuak';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver ez du hasieratu egin dira.';
+$lang['samba_file_share_tooltip'] = 'duzu sare fitxategi dira partekatu nahi izanez gero, mesedez, erabili Flexshare aplikazioa.';
+$lang['samba_initializing_core_system'] = 'core sistema hasieratzen, mesedez izan pazientzia.';
+$lang['samba_initializing_warning'] = 'Hasieratzeko minutu bat edo bi hartu ahal izango dute, pazientzia izan.';
+$lang['samba_master_node_needs_initialization'] = 'Master nodo jarraitu aurretik hasieratu behar.';
+$lang['samba_mode'] = 'Modu';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directory Software instalatu, beraz, behar ez dituzun aplikazio hau.';
