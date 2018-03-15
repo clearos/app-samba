113e1d44a03b3fe5779018f89884984452a3e12d
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:21:21 +0000

    Add new files and directories, commit changes, push changes

--- language/my/samba_lang.php
+++ language/my/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'အုပ်ချုပ်ရေးမှူးအကောင့်';
-$lang['samba_app_description'] = 'Windows ကိုကွန်ယက်အဖွဲ့ (Samba) က Windows စနစ်များနှင့်အတူ interoperate ရန်လိုအပ်သောကော်ပေးစွမ်းသည်။ ဒီ app ကို Windows ဒိုမိန်းစီမံခန့်ခွဲမှုနှင့်အတူစစ်မှန်ကြောင်းအထောက်အထားပြသခြင်းန်ဆောင်မှု, ဖိုင်နဲ့ပုံနှိပ်ပေးစွမ်းသည်။';
-$lang['samba_app_name'] = 'Windows ကိုကွန်ယက်အဖွဲ့ (Samba)';
-$lang['samba_computers'] = 'ကွန်ပျူတာများ';
-$lang['samba_driver_has_not_been_initialized'] = 'မောင်းသူစတင်ဖို့မခံတော်မူပြီ။';
-$lang['samba_file_share_tooltip'] = 'သငျသညျကှနျယဖိုင်ရှယ်ယာဖန်တီးချင်ပါတယ်လျှင်, Flexshare app ကိုသုံးပါ။';
-$lang['samba_initializing_core_system'] = 'core ကို system ကိုစတင်လုပ်ကိုင်, လူနာဖြစ်ပါ။';
-$lang['samba_initializing_warning'] = 'မိတ်ဆက်တစ်မိနစ်သို့မဟုတ်နှစ်ဦးကိုယူစေခြင်းငှါ, လူနာဖြစ်ပါ။';
-$lang['samba_master_node_needs_initialization'] = 'မဟာ node ကိုဆက်လက်မလုပ်ဆောင်မီစတင်ဖို့ခံရဖို့လိုအပ်ပါတယ်။';
+$lang['samba_app_description'] = 'Windows ကိုကွန်ယက်အဖွဲ့ (ဆမ်ဘာ) ကို Windows စနစ်များနှင့်အတူ interoperate ရန်လိုအပ်သောကော်ပေးပါသည်။ ဒီ app ကို Windows ဒိုမိန်းစီမံခန့်ခွဲမှုနှင့်အတူစစ်မှန်ကြောင်းအထောက်အထားပြသခြင်းန်ဆောင်မှုများ, ဖိုင်နဲ့ပုံနှိပ်ပေးပါသည်။';
+$lang['samba_app_name'] = 'Windows ကိုကွန်ယက်အဖွဲ့ (ဆမ်ဘာ)';
+$lang['samba_computers'] = 'ကွန်ပြူတာများ';
+$lang['samba_driver_has_not_been_initialized'] = 'ကားမောင်းသူနန့်မထားပါဘူး။';
+$lang['samba_file_share_tooltip'] = 'သငျသညျကှနျယကျဖိုင်ရှယ်ယာဖန်တီးချင်ပါတယ်လျှင်, Flexshare app ကိုသုံးပါ။';
+$lang['samba_initializing_core_system'] = 'အဓိက system ကိုစတင်လုပ်ကိုင်, စိတ်ရှည်ပါ။';
+$lang['samba_initializing_warning'] = 'မိတ်ဆက်တစ်မိနစ်သို့မဟုတ်နှစ်ဦးကိုယူစေခြင်းငှါ, စိတ်ရှည်ပါ။';
+$lang['samba_master_node_needs_initialization'] = 'မာစတာ node ကိုဆက်လက်မလုပ်ဆောင်မီစတင်ဖို့လိုအပ်တယ်။';
 $lang['samba_mode'] = 'mode ကို';
-$lang['samba_samba_directory_installed_warning'] = 'အဆိုပါ Samba Directory ကို software ကို install လုပ်, ဒါကြောင့်သင်တို့သည်နောက်တဖန်မဒီ app ကိုလိုအပ်လျက်ရှိသည်။';
+$lang['samba_samba_directory_installed_warning'] = 'အဆိုပါ Samba Directory ကို software ကို installed, ဒါကြောင့်သင်မရှိတော့ဒီ app ကိုလိုအပ်နေပါသည်။';
