69a14d8440d9a4cdb93de70a011a440b7b93efe9
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:10:04 +0000

    Add new files and directories, commit changes, push changes

--- language/yo/samba_lang.php
+++ language/yo/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'IT Account';
-$lang['samba_app_description'] = 'Windows Nẹtiwọki (Samba) pese awọn pataki pọ to interoperate pẹlu Windows awọn ọna šiše. Awọn app pese ìfàṣẹsí iṣẹ, faili ki o si ta, pẹlú pẹlu Windows ašẹ isakoso.';
-$lang['samba_app_name'] = 'Windows Nẹtiwọki (Samba)';
-$lang['samba_computers'] = 'kọmputa';
-$lang['samba_driver_has_not_been_initialized'] = 'Iwakọ ti ko ti initialized.';
-$lang['samba_file_share_tooltip'] = 'Ti o ba ti o ba yoo fẹ lati ṣẹda nẹtiwọki faili mọlẹbi, jọwọ lo awọn Flexshare app.';
-$lang['samba_initializing_core_system'] = 'Initializing mojuto eto, jọwọ jẹ alaisan.';
-$lang['samba_initializing_warning'] = 'Initialization le gba a iseju kan tabi meji, jọwọ jẹ alaisan.';
-$lang['samba_master_node_needs_initialization'] = 'Titunto si ipade nilo lati wa ni initialized ṣaaju ki o to ye.';
-$lang['samba_mode'] = 'mode';
-$lang['samba_samba_directory_installed_warning'] = 'The Samba Directory software ti fi sori ẹrọ, ki o ko si ohun to nilo yi app.';
+$lang['samba_administrator_account'] = 'Oluṣakoso IT';
+$lang['samba_app_description'] = 'Nẹtiwọki Nẹtiwọki (Samba) n pese folda ti o yẹ lati ṣepọ pẹlu awọn ọna Windows. Ifilọlẹ naa pese awọn iṣẹ imudaniloju, faili ati tẹjade, pẹlu pẹlu iṣakoso ašẹ Windows.';
+$lang['samba_app_name'] = 'Nẹtiwọki Nẹtiwọki (Samba)';
+$lang['samba_computers'] = 'Awọn kọmputa';
+$lang['samba_driver_has_not_been_initialized'] = 'Iwakọ ko ti ni ibẹrẹ.';
+$lang['samba_file_share_tooltip'] = 'Ti o ba fẹ lati ṣẹda awọn pinpin faili nẹtiwọki, jọwọ lo Flexshare app.';
+$lang['samba_initializing_core_system'] = 'Ni iṣeto akọkọ eto, jọwọ jẹ alaisan.';
+$lang['samba_initializing_warning'] = 'Ibẹrẹ le gba iṣẹju kan tabi meji, jọwọ jẹ alaisan.';
+$lang['samba_master_node_needs_initialization'] = 'Ipele adaṣe nilo lati wa ni akọkọ ṣaaju ki o to bẹrẹ.';
+$lang['samba_mode'] = 'Ipo';
+$lang['samba_samba_directory_installed_warning'] = 'Awọn software ti Samba Directory ti fi sori ẹrọ, nitorina o ko nilo ohun elo yii.';
