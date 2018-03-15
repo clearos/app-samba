951728d7c5871302c6b844c059f01eed9280df04
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:56:07 +0000

    Add new files and directories, commit changes, push changes

--- language/sk/samba_lang.php
+++ language/sk/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'účet správcu';
-$lang['samba_app_description'] = 'Windows siete (Samba) zabezpečuje potrebnú lepidlo spolupracovať so systémami Windows. Aplikácia poskytuje služby overovania, zdieľanie súborov a tlačiarní, spolu s vedením domény systému Windows.';
-$lang['samba_app_name'] = 'Windows siete (Samba)';
+$lang['samba_administrator_account'] = 'Účet správcu';
+$lang['samba_app_description'] = 'Windows Networking (Samba) poskytuje potrebné lepidlo na spoluprácu so systémami Windows. Aplikácia poskytuje autentifikačné služby, súbor a tlač, spolu s riadením domén Windows.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
 $lang['samba_computers'] = 'Počítače';
-$lang['samba_driver_has_not_been_initialized'] = 'Vodič nebol inicializovaný.';
-$lang['samba_file_share_tooltip'] = 'Ak by ste chceli vytvoriť zdieľanie súborov siete, použite aplikáciu FlexShare.';
-$lang['samba_initializing_core_system'] = 'Inicializácia hlavného systému, prosím, buďte trpezliví.';
-$lang['samba_initializing_warning'] = 'Inicializácia môže trvať minútu alebo dve, prosím, buďte trpezliví.';
-$lang['samba_master_node_needs_initialization'] = 'Hlavný uzol musí byť inicializovaný pred pokračovaním.';
+$lang['samba_driver_has_not_been_initialized'] = 'Ovládač nebol inicializovaný.';
+$lang['samba_file_share_tooltip'] = 'Ak chcete vytvoriť zdieľané súbory v sieti, použite prosím aplikáciu Flexshare.';
+$lang['samba_initializing_core_system'] = 'Inicializácia základného systému, prosím, buďte trpezliví.';
+$lang['samba_initializing_warning'] = 'Inicializácia môže trvať minútu alebo dva, prosím, buďte trpezliví.';
+$lang['samba_master_node_needs_initialization'] = 'Pred pokračovaním musí byť hlavný uzol inicializovaný.';
 $lang['samba_mode'] = 'režim';
-$lang['samba_samba_directory_installed_warning'] = 'Tento softvér Samba Adresár je nainštalovaný, takže už túto aplikáciu je potrebné.';
+$lang['samba_samba_directory_installed_warning'] = 'Softvér Samba Directory je nainštalovaný, takže už túto aplikáciu nepotrebujete.';
