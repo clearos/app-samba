89a2364ff68e9def587ccecf596174756fd976be
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:23:32 +0000

    Add new files and directories, commit changes, push changes

--- language/zh/samba_lang.php
+++ language/zh/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = '管理员帐户';
-$lang['samba_app_description'] = 'Windows网络（桑巴）提供了必要的胶水与Windows系统进行互操作。 该应用程序提供认证服务，文件和打印，与Windows域管理一起。';
-$lang['samba_app_name'] = 'Windows网络（桑巴）';
+$lang['samba_app_description'] = 'Windows网络（Samba）为与Windows系统进行互操作提供了必要的粘合剂。 该应用程序提供身份验证服务，文件和打印以及Windows域管理。';
+$lang['samba_app_name'] = 'Windows网络（Samba）';
 $lang['samba_computers'] = '电脑';
 $lang['samba_driver_has_not_been_initialized'] = '驱动程序尚未初始化。';
-$lang['samba_file_share_tooltip'] = '如果你想创建网络文件共享，请使用了FlexShare应用。';
-$lang['samba_initializing_core_system'] = '初始化内核系统，请耐心等待。';
-$lang['samba_initializing_warning'] = '初始化可能需要一到两分钟，请耐心等待。';
-$lang['samba_master_node_needs_initialization'] = '主节点需要之后才能继续进行初始化。';
+$lang['samba_file_share_tooltip'] = '如果您想创建网络文件共享，请使用Flexshare应用程序。';
+$lang['samba_initializing_core_system'] = '初始化核心系统，请耐心等待。';
+$lang['samba_initializing_warning'] = '初始化可能需要一两分钟，请耐心等待。';
+$lang['samba_master_node_needs_initialization'] = '在继续之前，主节点需要被初始化。';
 $lang['samba_mode'] = '模式';
-$lang['samba_samba_directory_installed_warning'] = 'Samba的目录软件安装，让您不再需要这个应用程序。';
+$lang['samba_samba_directory_installed_warning'] = 'Samba目录软件已安装，因此您不再需要此应用程序。';
