a41da98b23be13cacbba7ca3ed79e6bbdea13c6d
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:04:17 +0000

    Add new files and directories, commit changes, push changes

--- language/th/samba_lang.php
+++ language/th/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'บัญชีผู้ดูแลระบบ';
-$lang['samba_app_description'] = 'เครือข่าย Windows (Samba) ให้กาวที่จำเป็นในการทำงานร่วมกับระบบที่ใช้ Windows app ที่ให้บริการการตรวจสอบไฟล์และพิมพ์พร้อมกับการจัดการโดเมน Windows';
-$lang['samba_app_name'] = 'เครือข่าย Windows (Samba)';
+$lang['samba_app_description'] = 'Windows Networking (Samba) มีกาวที่จำเป็นในการทำงานร่วมกับระบบ Windows แอปจะให้บริการพิสูจน์ตัวตนไฟล์และพิมพ์ตลอดจนการจัดการโดเมน Windows';
+$lang['samba_app_name'] = 'ระบบเครือข่ายของ Windows (Samba)';
 $lang['samba_computers'] = 'คอมพิวเตอร์';
-$lang['samba_driver_has_not_been_initialized'] = 'คนขับไม่ได้รับการเริ่มต้น';
-$lang['samba_file_share_tooltip'] = 'หากคุณต้องการที่จะสร้างการแชร์ไฟล์เครือข่ายโปรดใช้แอป Flexshare';
-$lang['samba_initializing_core_system'] = 'การเริ่มต้นระบบหลักโปรดอดทน';
-$lang['samba_initializing_warning'] = 'เริ่มต้นอาจใช้เวลาหนึ่งหรือสองนาทีโปรดอดทน';
-$lang['samba_master_node_needs_initialization'] = 'โหนดหลักจะต้องมีการเริ่มต้นก่อนที่จะดำเนิน';
+$lang['samba_driver_has_not_been_initialized'] = 'ยังไม่ได้เริ่มต้นโปรแกรมควบคุม';
+$lang['samba_file_share_tooltip'] = 'หากคุณต้องการสร้างการแชร์ไฟล์เครือข่ายโปรดใช้แอปพลิเคชัน Flexshare';
+$lang['samba_initializing_core_system'] = 'เริ่มต้นระบบหลักโปรดอดใจรอ';
+$lang['samba_initializing_warning'] = 'การเริ่มต้นอาจใช้เวลาสักครู่หรือสองนาทีโปรดอดทนรอ';
+$lang['samba_master_node_needs_initialization'] = 'โหนดต้นแบบต้องได้รับการเตรียมใช้งานก่อนดำเนินการต่อ';
 $lang['samba_mode'] = 'โหมด';
-$lang['samba_samba_directory_installed_warning'] = 'ซอฟแวร์แซมบ้าไดเรกทอรีที่มีการติดตั้งเพื่อให้คุณไม่จำเป็นต้องตรวจสอบนี้';
+$lang['samba_samba_directory_installed_warning'] = 'ซอฟต์แวร์ติดตั้ง Samba Directory ทำให้คุณไม่ต้องใช้แอพนี้อีกต่อไป';
