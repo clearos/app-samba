dc4abcfe82146b71bfb17a741db73c5523691091
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:31:42 +0000

    Add new files and directories, commit changes, push changes

--- language/ha/samba_lang.php
+++ language/ha/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Administrator Account';
-$lang['samba_app_description'] = 'Windows Networking (Samba) na samar da zama dole manne wa interoperate da Windows tsarin. The app bayar Tantance kalmar sirri ayyuka, fayil kuma buga, tare da Windows yankin management.';
-$lang['samba_app_name'] = 'Windows Networking (Samba)';
-$lang['samba_computers'] = 'Computers';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver ba a initialized.';
-$lang['samba_file_share_tooltip'] = 'Idan ka so ka ƙirƙiri cibiyar sadarwa fayil hannun jari, don Allah a yi amfani da Flexshare app.';
-$lang['samba_initializing_core_system'] = 'Soma core tsarin, don Allah ka yi haƙuri.';
-$lang['samba_initializing_warning'] = 'Initialization iya šaukar minti ko biyu, don Allah ka yi haƙuri.';
-$lang['samba_master_node_needs_initialization'] = 'Jagora kumburi bukatar a initialized kafin a ci gaba.';
-$lang['samba_mode'] = 'Mode';
-$lang['samba_samba_directory_installed_warning'] = 'The Samba Directory software da aka shigar, sai ka daina bukatar wannan app.';
+$lang['samba_administrator_account'] = 'Mai sarrafa Adireshin';
+$lang['samba_app_description'] = 'Sabis na Windows (Samba) yana ba da mahimmanci don daidaitawa tare da tsarin Windows. Aikace-aikacen yana samar da ƙwarewar sabis, fayil da bugawa, tare da gudanarwa na yankin Windows.';
+$lang['samba_app_name'] = 'Sabis na Windows (Samba)';
+$lang['samba_computers'] = 'Kwamfuta';
+$lang['samba_driver_has_not_been_initialized'] = 'Ba a ƙaddamar da direba ba.';
+$lang['samba_file_share_tooltip'] = 'Idan kuna son ƙirƙirar hannun jari na hanyar sadarwa, don Allah a yi amfani da aikace-aikacen Flexshare.';
+$lang['samba_initializing_core_system'] = 'Daftarin ainihin tsarin, don Allah ka yi hakuri.';
+$lang['samba_initializing_warning'] = 'Haddatarwa na iya ɗaukar minti daya ko biyu, don Allah a yi hakuri.';
+$lang['samba_master_node_needs_initialization'] = 'Dole ne a fara gyara kusoshi mai kula kafin a ci gaba.';
+$lang['samba_mode'] = 'Yanayin';
+$lang['samba_samba_directory_installed_warning'] = 'An shigar da software na Samba ɗin, don haka ba za ku bukaci wannan app ba.';
