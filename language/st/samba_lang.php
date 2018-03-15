07fd04dbe02a69de66316f4f2b1c991e65926871
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:57:50 +0000

    Add new files and directories, commit changes, push changes

--- language/st/samba_lang.php
+++ language/st/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Account mohlokomeli oa';
-$lang['samba_app_description'] = 'Windows Inthaneteng (Samba) fana ka sekhomaretsi ho hlokahala hore ho interoperate le litsamaiso Windows. app e e fana ka ditshebeletso tsa netefatso, faele le ngotsoeng, hammoho le taolo ya Windows domain name.';
-$lang['samba_app_name'] = 'Windows Inthaneteng (Samba)';
-$lang['samba_computers'] = 'lik&#39;homphieutha';
-$lang['samba_driver_has_not_been_initialized'] = 'Mokhanni ha e so initialized.';
-$lang['samba_file_share_tooltip'] = 'Haeba u ka rata ho bōpa diabo tsa netweke ya faele, ka kōpo sebelisa app e sa Flexshare.';
-$lang['samba_initializing_core_system'] = 'Initializing tsamaiso ea mantlha e, ka kōpo ba le mamello.';
-$lang['samba_initializing_warning'] = 'Initialization ka nka motsotso kapa e &#39;meli, ka kōpo ba le mamello.';
-$lang['samba_master_node_needs_initialization'] = 'Master noute e lokela ho initialized pele ba tsoela pele.';
-$lang['samba_mode'] = 'Mode';
-$lang['samba_samba_directory_installed_warning'] = 'Software Samba Directory e hlomamisa, kahoo u ba hlola ba hloka app e lena.';
+$lang['samba_administrator_account'] = 'Accounting Account';
+$lang['samba_app_description'] = 'Windows Networking (Samba) e fana ka sekhomaretsi se hlokahalang ho sebelisana le mechine ea Windows. Lenaneo le fana ka litšebeletso tsa bopaki, lifaele le ho hatisa, hammoho le tsamaiso ea domain Windows.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Lik&#39;homphieutha';
+$lang['samba_driver_has_not_been_initialized'] = 'Mokhanni ha a sa qalisoa.';
+$lang['samba_file_share_tooltip'] = 'Haeba o ka rata ho etsa lihlahisoa tsa faele tsa marang-rang, ka kopo sebelisa lenaneo la Flexshare.';
+$lang['samba_initializing_core_system'] = 'Qetella mokhoa oa motheo, ka kōpo e-ba le mamello.';
+$lang['samba_initializing_warning'] = 'Ho qala pele ho ka nka metsotso kapa tse peli, ka kōpo e-ba le mamello.';
+$lang['samba_master_node_needs_initialization'] = 'Node ea thuto e hloka ho qalisoa pele e tsoela pele.';
+$lang['samba_mode'] = 'Boemo';
+$lang['samba_samba_directory_installed_warning'] = 'Lenaneo la Tsamaiso la Samba le kenngoa, kahoo ha u sa hloka lenaneo lena.';
