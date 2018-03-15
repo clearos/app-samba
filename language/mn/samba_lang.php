cdd26202c3ff03d2db8a4913016457d5b3e8c19b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:46:10 +0000

    Add new files and directories, commit changes, push changes

--- language/mn/samba_lang.php
+++ language/mn/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Administrator Дансны';
-$lang['samba_app_description'] = 'Windows Networking (Samba) Windows систем нь хоорондоо хамтарч ажилладаг шаардлагатай цавуу олгодог. апп Windows домэйн удирдлага хамт нэвтрэлт танилт үйлчилгээ, Файл болон Хэвлэх олгодог.';
-$lang['samba_app_name'] = 'Windows Networking (Samba)';
-$lang['samba_computers'] = 'Компьютер';
-$lang['samba_driver_has_not_been_initialized'] = 'Жолооч эхэлж байсан байна.';
-$lang['samba_file_share_tooltip'] = 'та сүлжээний файл хувьцааг бий болгохыг хүсэж байгаа бол, Flexshare програмыг ашиглана уу.';
-$lang['samba_initializing_core_system'] = 'үндсэн системийг эхлүүлэх тэвчээртэй байх уу.';
-$lang['samba_initializing_warning'] = 'Эхлүүлэлт нь буюу хоёр минут авч болно, тэвчээртэй байх уу.';
-$lang['samba_master_node_needs_initialization'] = 'Мастер цэг эхлүүлэхээсээ өмнө эхэлж байх хэрэгтэй.';
+$lang['samba_administrator_account'] = 'Админы бүртгэл';
+$lang['samba_app_description'] = 'Windows Сүлжээ (Samba) нь Windows системтэй ажиллахад шаардлагатай цавуугаар хангадаг. Апп нь нэвтрэлт танилтын үйлчилгээ, файл, хэвлэх, Windows домэйн удирдлагын хамт өгдөг.';
+$lang['samba_app_name'] = 'Windows Сүлжээний (Samba)';
+$lang['samba_computers'] = 'Компьютерууд';
+$lang['samba_driver_has_not_been_initialized'] = 'Жолооч эхлээгүй байна.';
+$lang['samba_file_share_tooltip'] = 'Хэрэв та сүлжээний файлын хувьцааг үүсгэхийг хүсвэл Flexshare програмыг ашиглана уу.';
+$lang['samba_initializing_core_system'] = 'Гол системийг эхлүүлэх нь тэвчээртэй байгаарай.';
+$lang['samba_initializing_warning'] = 'Эхлэх нь нэг минут, хоёр хоцрогдож магадгүй тул тэвчээртэй байгаарай.';
+$lang['samba_master_node_needs_initialization'] = 'Мастер зангилааг үргэлжлүүлэхээс өмнө эхлүүлэх шаардлагатай.';
 $lang['samba_mode'] = 'Горим';
-$lang['samba_samba_directory_installed_warning'] = 'Хэрэв та ямар ч цаашид энэ апп л шаардлагатай Samba лавлах програм хангамж, суулгасан байна.';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directory програм хангамжийг суулгасан тул та энэ аппликейшныг ашиглахаа больсон.';
