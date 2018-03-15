c2028f244fbc09debc4fa9bd945b9ceace07dea3
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:18:49 +0000

    Add new files and directories, commit changes, push changes

--- language/be/samba_lang.php
+++ language/be/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'Уліковы запіс адміністратара';
-$lang['samba_app_description'] = 'Сетак Windows (Samba) забяспечвае неабходны клей для ўзаемадзеяння з сістэмамі Windows. Дадатак прадастаўляе паслугі аўтэнтыфікацыі, файлаў і друку, нароўні з кіраваннем дамена Windows.';
+$lang['samba_app_description'] = 'Сетак Windows (Samba) забяспечвае неабходны клей для ўзаемадзеяння з сістэмамі Windows. Дадатак прадастаўляе паслугі аўтэнтыфікацыі, файл і друк, нароўні з кіраваннем даменам Windows.';
 $lang['samba_app_name'] = 'Сетак Windows (Samba)';
 $lang['samba_computers'] = 'кампутары';
 $lang['samba_driver_has_not_been_initialized'] = 'Вадзіцель не быў ініцыялізаваны.';
-$lang['samba_file_share_tooltip'] = 'Калі вы хацелі б стварыць акцыі сеткі файлаў, неабходна выкарыстоўваць прыкладанне FlexShare.';
+$lang['samba_file_share_tooltip'] = 'Калі вы хочаце стварыць акцыі сеткі файлаў, неабходна выкарыстоўваць прыкладанне FlexShare.';
 $lang['samba_initializing_core_system'] = 'Ініцыялізацыя ядра сістэмы, калі ласка, будзьце цярплівыя.';
 $lang['samba_initializing_warning'] = 'Ініцыялізацыя можа заняць хвіліну ці дзве, калі ласка, будзьце цярплівыя.';
 $lang['samba_master_node_needs_initialization'] = 'Майстар вузел павінен быць ініцыялізаваны, перш чым працягнуць.';
 $lang['samba_mode'] = 'рэжым';
-$lang['samba_samba_directory_installed_warning'] = 'Праграмнае забеспячэнне Samba каталог усталёўваецца, так што вам больш не трэба гэта дадатак.';
+$lang['samba_samba_directory_installed_warning'] = 'Праграмнае забеспячэнне Samba Каталог усталёўваецца, так што вам больш не трэба гэта дадатак.';
