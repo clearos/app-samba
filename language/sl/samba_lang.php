20b2471f7890c6bad05959f8539a31e413b210e4
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:56:32 +0000

    Add new files and directories, commit changes, push changes

--- language/sl/samba_lang.php
+++ language/sl/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'administrator računa';
-$lang['samba_app_description'] = 'Windows Networking (Samba) zagotavlja potrebno lepilo interoperabilni s sistemi Windows. Aplikacija nudi storitve za preverjanje pristnosti, datoteke in tiskanje, skupaj z upravljanjem Windows domene.';
+$lang['samba_administrator_account'] = 'Administratorski račun';
+$lang['samba_app_description'] = 'Windows Networking (Samba) zagotavlja potrebno lepilo za interoperabilnost s sistemi Windows. Aplikacija ponuja storitve overjanja, datoteke in tiskanja, skupaj z upravljanjem domene Windows.';
 $lang['samba_app_name'] = 'Windows Networking (Samba)';
-$lang['samba_computers'] = 'računalniki';
-$lang['samba_driver_has_not_been_initialized'] = 'Voznik ni bil inicializiran.';
-$lang['samba_file_share_tooltip'] = 'Če želite ustvariti omrežje datotek delnic, uporabite aplikacijo Flexshare.';
-$lang['samba_initializing_core_system'] = 'Inicializacijo jedro sistema, zato bodite potrpežljivi.';
-$lang['samba_initializing_warning'] = 'Inicializacija lahko traja minuto ali dve, zato bodite potrpežljivi.';
-$lang['samba_master_node_needs_initialization'] = 'mora nadrejeno vozlišče inicializirati, preden nadaljujete.';
-$lang['samba_mode'] = 'način';
-$lang['samba_samba_directory_installed_warning'] = 'Programska oprema Samba imenik je nameščen, tako da vam ni treba več to aplikacijo.';
+$lang['samba_computers'] = 'Računalniki';
+$lang['samba_driver_has_not_been_initialized'] = 'Gonilnik ni inicializirano.';
+$lang['samba_file_share_tooltip'] = 'Če želite ustvariti del omrežne datoteke, uporabite aplikacijo Flexshare.';
+$lang['samba_initializing_core_system'] = 'Inicializiranje osnovnega sistema, bodite potrpežljivi.';
+$lang['samba_initializing_warning'] = 'Inicializacija lahko traja minuto ali dve, prosimo, bodite potrpežljivi.';
+$lang['samba_master_node_needs_initialization'] = 'Glavno vozlišče je treba inicializirati, preden nadaljujete.';
+$lang['samba_mode'] = 'Način';
+$lang['samba_samba_directory_installed_warning'] = 'Programska oprema Samba Directory je nameščena, zato te aplikacije ne potrebujete več.';
