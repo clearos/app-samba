28bfd34acfb3ce43a4c2e6a0a98d37c3ee70b3c2
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:37:53 +0000

    Add new files and directories, commit changes, push changes

--- language/ka/samba_lang.php
+++ language/ka/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Administrator Account';
-$lang['samba_app_description'] = 'Windows ქსელში (Samba) უზრუნველყოფს საჭირო წებო შეეწყონ ერთად Windows სისტემებს. App უზრუნველყოფს ავტორიზაციის მომსახურება, ფაილი და ბეჭდვითი ერთად Windows დომენის მართვა.';
-$lang['samba_app_name'] = 'Windows ქსელში (Samba)';
+$lang['samba_administrator_account'] = 'ადმინისტრატორის ანგარიში';
+$lang['samba_app_description'] = 'Windows Networking (Samba) უზრუნველყოფს Windows- ის სისტემების ინტეგრაციას. აპლიკაცია უზრუნველყოფს ავტორიზაციის მომსახურებას, ფაილსა და ბეჭდვას, Windows დომენების მენეჯმენტთან ერთად.';
+$lang['samba_app_name'] = 'Windows ქსელის (Samba)';
 $lang['samba_computers'] = 'კომპიუტერები';
-$lang['samba_driver_has_not_been_initialized'] = 'მძღოლი არ ინიციალიზაცია.';
-$lang['samba_file_share_tooltip'] = 'თუ გსურთ, რათა შეიქმნას ქსელის ფაილი აქციების, გთხოვთ, გამოიყენოთ Flexshare app.';
-$lang['samba_initializing_core_system'] = 'ინიციალიზება ძირითადი სისტემა, გთხოვთ მოთმინება.';
-$lang['samba_initializing_warning'] = 'Initialization შეუძლია მიიღოს ერთი წუთით ან ორი, გთხოვთ მოთმინება.';
-$lang['samba_master_node_needs_initialization'] = 'ოსტატი კვანძის საჭიროებს ინიციალიზაცია გაგრძელებამდე.';
+$lang['samba_driver_has_not_been_initialized'] = 'მძღოლი არ არის ინიციალიზებული.';
+$lang['samba_file_share_tooltip'] = 'თუ გსურთ შექმნათ ქსელის ფაილების აქციები, გთხოვთ გამოიყენოთ Flexshare აპლიკაცია.';
+$lang['samba_initializing_core_system'] = 'ძირითადი სისტემის ჩამოყალიბება, გთხოვთ, მოთმინოთ.';
+$lang['samba_initializing_warning'] = 'ინიციალიზაციისას შეიძლება წუთი ან ორი, მოთმინება.';
+$lang['samba_master_node_needs_initialization'] = 'სამაგისტრო კვანძის დაწყებამდე უნდა იყოს ინიცირებული.';
 $lang['samba_mode'] = 'რეჟიმი';
-$lang['samba_samba_directory_installed_warning'] = 'სამბა დირექტორია პროგრამული დამონტაჟებულია, ასე რომ თქვენ აღარ უნდა ამ app.';
+$lang['samba_samba_directory_installed_warning'] = 'Samba დირექტორიაში დამონტაჟებული პროგრამული უზრუნველყოფა დამონტაჟებულია, ამიტომ ამ აპს აღარ გჭირდებათ.';
