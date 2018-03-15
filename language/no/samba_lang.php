6fc8d8ff9f010bf29ab33facb608d00c7842cbb8
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:50:04 +0000

    Add new files and directories, commit changes, push changes

--- language/no/samba_lang.php
+++ language/no/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'administrator~~POS=TRUNC konto~~POS=HEADCOMP';
-$lang['samba_app_description'] = 'Windows Networking (Samba) gir nødvendig lim for å fungere sammen med Windows-systemer. Applikasjonen gir autentiseringstjenester, fil og print, sammen med Windows domeneadministrasjon.';
+$lang['samba_administrator_account'] = 'Administratorkonto';
+$lang['samba_app_description'] = 'Windows Networking (Samba) gir det nødvendige limet for å samvirke med Windows-systemer. Appen tilbyr autentiseringstjenester, fil og utskrift, sammen med Windows-domeneadministrasjon.';
 $lang['samba_app_name'] = 'Windows Networking (Samba)';
 $lang['samba_computers'] = 'datamaskiner';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver har ikke blitt initialisert.';
-$lang['samba_file_share_tooltip'] = 'Hvis du ønsker å skape nettverk fildelinger, vennligst bruk Flexshare app.';
-$lang['samba_initializing_core_system'] = 'Initialkjernesystem, vær tålmodig.';
-$lang['samba_initializing_warning'] = 'Initialisering kan ta et minutt eller to, vær tålmodig.';
-$lang['samba_master_node_needs_initialization'] = 'Master node må klargjøres før du fortsetter.';
+$lang['samba_driver_has_not_been_initialized'] = 'Driveren er ikke initialisert.';
+$lang['samba_file_share_tooltip'] = 'Hvis du vil opprette nettverksfilaktier, vennligst bruk Flexshare-appen.';
+$lang['samba_initializing_core_system'] = 'Initialiserende kjernesystem, vær så tålmodig.';
+$lang['samba_initializing_warning'] = 'Initialisering kan ta et minutt eller to, vær så tålmodig.';
+$lang['samba_master_node_needs_initialization'] = 'Master node må initialiseres før du fortsetter.';
 $lang['samba_mode'] = 'Modus';
-$lang['samba_samba_directory_installed_warning'] = 'Samba Directory programvaren er installert, slik at du ikke lenger trenger dette programmet.';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directory-programvaren er installert, slik at du ikke lenger trenger denne appen.';
