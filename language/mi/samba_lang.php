863fb93fc98c909a03e682f637a0a4486e9cedc5
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:44:27 +0000

    Add new files and directories, commit changes, push changes

--- language/mi/samba_lang.php
+++ language/mi/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Pūkete kaiwhakahaere';
-$lang['samba_app_description'] = 'Windows Networking (Samba) whakarato te kāpia e tika ana ki te interoperate ki ngā pūnaha Windows. Te taupānga whakarato ratonga motuhēhēnga, kōnae, me te tā, me te whakahaere rohe Windows.';
-$lang['samba_app_name'] = 'Windows Networking (Samba)';
-$lang['samba_computers'] = 'rorohiko';
-$lang['samba_driver_has_not_been_initialized'] = 'e kore kua Driver i arawhiti.';
-$lang['samba_file_share_tooltip'] = 'Ki te hiahia koe ki te waihanga hea kōnae whatunga, tēnā whakamahi i te taupānga Flexshare.';
-$lang['samba_initializing_core_system'] = 'Arawhiti ana pūnaha matua, tēnā kia manawanui.';
-$lang['samba_initializing_warning'] = 'kia tango arawhiti he rua ranei meneti, tēnā kia manawanui.';
-$lang['samba_master_node_needs_initialization'] = 'Me arawhiti ki te aroaro o puta Kaiwhakaako kōpuku.';
-$lang['samba_mode'] = 'aratau';
-$lang['samba_samba_directory_installed_warning'] = 'Kei te tāuta i te pūmanawa Samba Directory, kia hiahia kore koutou i tēnei taupānga.';
+$lang['samba_administrator_account'] = 'Pūkete Kaiwhakahaere';
+$lang['samba_app_description'] = 'Ko te Whakaaetanga Windows (Samba) e whakarato ana i te kapau e tika ana hei whakahoahonohono me nga pūnaha Windows. Ko te taupānga e whakarato ana i nga ratonga whakamotuhēhēnga, i te kōnae me te tā, me te whakahaeretanga o te rohe Windows.';
+$lang['samba_app_name'] = 'Whatunga Windows (Samba)';
+$lang['samba_computers'] = 'Ngā rorohiko';
+$lang['samba_driver_has_not_been_initialized'] = 'Kaore i taatatia te Kaikoi.';
+$lang['samba_file_share_tooltip'] = 'Mena e hiahia ana koe ki te hanga i nga roopu roopu whatunga, me whakamahi i te taupānga Flexshare.';
+$lang['samba_initializing_core_system'] = 'Ko te whakapuaki i te pünaha matua, kia manawanui.';
+$lang['samba_initializing_warning'] = 'Ka taea e te whakawhitinga tuatahi te tango i te meneti ka rua ranei, na kia manawanui.';
+$lang['samba_master_node_needs_initialization'] = 'Me timata te rae matua ki mua i te whakahaere.';
+$lang['samba_mode'] = 'Aratau';
+$lang['samba_samba_directory_installed_warning'] = 'Kua whakauruhia te rorohiko Whaiaronga Samba, no reira kaore koe e hiahia ana i tenei taupānga.';
