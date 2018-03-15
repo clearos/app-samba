8dcaf79914766e14bfd802b713f1f372f701387d
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:42:18 +0000

    Add new files and directories, commit changes, push changes

--- language/lt/samba_lang.php
+++ language/lt/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'administratorius paskyra';
-$lang['samba_app_description'] = '"Windows tinklai (Samba) užtikrina reikiamą klijų suderinamumo su Windows sistemomis. App suteikia autentiškumo paslaugas, rinkmenų ir spausdinimo, kartu su Windows domeno valdymą.';
-$lang['samba_app_name'] = '"Windows tinklai (Samba)';
+$lang['samba_administrator_account'] = 'Administratoriaus sąskaita';
+$lang['samba_app_description'] = '"Windows Networking" ("Samba") suteikia būtiną tarpusavio sąveiką su "Windows" sistemomis. Programa teikia autentifikavimo paslaugas, rinkmenas ir spausdina kartu su "Windows" domeno valdymu.';
+$lang['samba_app_name'] = '"Windows Networking" ("Samba")';
 $lang['samba_computers'] = 'Kompiuteriai';
-$lang['samba_driver_has_not_been_initialized'] = 'Vairuotojo nebuvo inicijuotas.';
-$lang['samba_file_share_tooltip'] = 'Jei norite sukurti Network File akcijas, prašome naudoti Flexshare programą.';
-$lang['samba_initializing_core_system'] = 'Inicijuojama pagrindinei sistemai, būkite kantrūs.';
-$lang['samba_initializing_warning'] = 'Pradės gali užtrukti minutę ar dvi, būkite kantrūs.';
-$lang['samba_master_node_needs_initialization'] = 'Magistro mazgas turi būti inicializuoti prieš tęsiant.';
-$lang['samba_mode'] = 'režimas';
-$lang['samba_samba_directory_installed_warning'] = 'Samba katalogas programinė įranga yra įdiegta, todėl jums nebereikia šią programą.';
+$lang['samba_driver_has_not_been_initialized'] = 'Vairuotojas nebuvo inicijuotas.';
+$lang['samba_file_share_tooltip'] = 'Jei norite sukurti tinklo dalijimosi failus, naudokite "Flexshare" programą.';
+$lang['samba_initializing_core_system'] = 'Inicijuojanti pagrindinę sistemą, prašau būti kantrūs.';
+$lang['samba_initializing_warning'] = 'Inicijavimas gali užtrukti minutę ar du, būkite kantrūs.';
+$lang['samba_master_node_needs_initialization'] = 'Prieš tęsiant, pagrindinis mazgas turi būti inicijuotas.';
+$lang['samba_mode'] = 'Režimas';
+$lang['samba_samba_directory_installed_warning'] = '"Samba Directory" programinė įranga yra įdiegta, todėl jums nebereikia šios programos.';
