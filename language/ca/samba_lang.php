621dad717bab4866cbd3c0f1414563928a4ed5f6
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:22:13 +0000

    Add new files and directories, commit changes, push changes

--- language/ca/samba_lang.php
+++ language/ca/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'compte d&#39;administrador';
-$lang['samba_app_description'] = 'Xarxa de Windows (Samba) proporciona la cola necessària per a interoperar amb sistemes Windows. L&#39;aplicació proporciona serveis d&#39;autenticació, arxius i impressió, juntament amb la gestió de domini de Windows.';
+$lang['samba_administrator_account'] = 'Compte d&#39;administrador';
+$lang['samba_app_description'] = 'La xarxa de Windows (Samba) proporciona la cua necessària per interoperar amb els sistemes Windows. L&#39;aplicació proporciona serveis d&#39;autenticació, fitxers i impressió, juntament amb la gestió del domini de Windows.';
 $lang['samba_app_name'] = 'Xarxa de Windows (Samba)';
-$lang['samba_computers'] = 'ordinadors';
+$lang['samba_computers'] = 'Informàtica';
 $lang['samba_driver_has_not_been_initialized'] = 'El controlador no s&#39;ha inicialitzat.';
-$lang['samba_file_share_tooltip'] = 'Per crear recursos compartits d&#39;arxius de xarxa, si us plau feu servir l&#39;aplicació FlexShare.';
-$lang['samba_initializing_core_system'] = 'La inicialització de sistema central, si us plau sigui pacient.';
-$lang['samba_initializing_warning'] = 'La inicialització pot trigar un o dos minuts, si us plau sigui pacient.';
-$lang['samba_master_node_needs_initialization'] = 'node mestre ha de ser inicialitzat abans.';
-$lang['samba_mode'] = 'manera';
-$lang['samba_samba_directory_installed_warning'] = 'El programari de Directory Samba està instal · lat, de manera que ja no necessita aquesta aplicació.';
+$lang['samba_file_share_tooltip'] = 'Si voleu crear fitxers de fitxers de xarxa, utilitzeu l&#39;aplicació FlexShare.';
+$lang['samba_initializing_core_system'] = 'Inicialitzant el nucli del sistema, tingueu paciència.';
+$lang['samba_initializing_warning'] = 'La inicialització pot trigar un minut o dos, si us plau, tingueu paciència.';
+$lang['samba_master_node_needs_initialization'] = 'El node mestre s&#39;ha d&#39;inicialitzar abans de continuar.';
+$lang['samba_mode'] = 'Mode';
+$lang['samba_samba_directory_installed_warning'] = 'El programari del directori Samba està instal·lat, de manera que ja no necessiteu aquesta aplicació.';
