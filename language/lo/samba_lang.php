265a4565d00ede5510a8a223cf740ba925d0e597
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:41:52 +0000

    Add new files and directories, commit changes, push changes

--- language/lo/samba_lang.php
+++ language/lo/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'ບັນຊີບໍລິຫານ';
-$lang['samba_app_description'] = 'Windows ລະບົບເຄືອຂ່າຍ (Samba) ໃຫ້ກາວສິ່ງຈໍາເປັນເພື່ອ interoperate ກັບລະບົບ Windows. ການ app ໃຫ້ບໍລິການກວດສອບເອກະສານແລະພິມ, ຄຽງຄູ່ກັບການຈັດການໂດເມນ Windows.';
-$lang['samba_app_name'] = 'Windows ລະບົບເຄືອຂ່າຍ (Samba)';
-$lang['samba_computers'] = 'ຄອມພິວເຕີ';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver ບໍ່ໄດ້ຮັບການເລີ່ມຕົ້ນ.';
-$lang['samba_file_share_tooltip'] = 'ຖ້າຫາກວ່າທ່ານຕ້ອງການທີ່ຈະສ້າງຮຸ້ນໄຟລ໌ເຄືອຂ່າຍ, ກະລຸນາໃຊ້ app Flexshare.';
-$lang['samba_initializing_core_system'] = 'ການເລີ່ມຕົ້ນລະບົບ core, ກະລຸນາຄວາມອົດທົນ.';
-$lang['samba_initializing_warning'] = 'ການເລີ່ມຕົ້ນອາດໃຊ້ເວລານາທີຫຼືສອງ, ກະລຸນາຄວາມອົດທົນ.';
-$lang['samba_master_node_needs_initialization'] = 'ຂໍ້ຕົ້ນສະບັບຕ້ອງການທີ່ຈະໄດ້ຮັບການເລີ່ມຕົ້ນກ່ອນທີ່ຈະດໍາເນີນການ.';
-$lang['samba_mode'] = 'ຮູບແບບການ';
-$lang['samba_samba_directory_installed_warning'] = 'ຊອບແວ Samba Directory ໄດ້ຖືກຕິດຕັ້ງ, ສະນັ້ນທ່ານບໍ່ຈໍາເປັນຕ້ອງ app ນີ້.';
+$lang['samba_administrator_account'] = 'Administrator Account';
+$lang['samba_app_description'] = 'Windows Networking (Samba) ໃຫ້ກາວທີ່ຈໍາເປັນເພື່ອເຮັດວຽກຮ່ວມກັບລະບົບ Windows. app ສະຫນອງການບໍລິການກວດສອບ, ໄຟລ໌ແລະພິມ, ພ້ອມກັບການຄຸ້ມຄອງໂດເມນ Windows.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'ຄອມພິວເຕີ້';
+$lang['samba_driver_has_not_been_initialized'] = 'ຄົນຂັບບໍ່ໄດ້ເລີ່ມຕົ້ນ.';
+$lang['samba_file_share_tooltip'] = 'ຖ້າທ່ານຕ້ອງການສ້າງຮຸ້ນໄຟລ໌ເຄືອຂ່າຍ, ກະລຸນາໃຊ້ແອັບຯ Flexshare.';
+$lang['samba_initializing_core_system'] = 'ການເລີ່ມຕົ້ນລະບົບຫຼັກ, ກະລຸນາປ່ວຍ.';
+$lang['samba_initializing_warning'] = 'ການເລີ່ມຕົ້ນອາດໃຊ້ເວລາຫນຶ່ງນາທີຫຼືສອງ, ກະລຸນາປ່ວຍ.';
+$lang['samba_master_node_needs_initialization'] = 'node Master ຕ້ອງການເລີ່ມຕົ້ນກ່ອນທີ່ຈະດໍາເນີນການ.';
+$lang['samba_mode'] = 'ໂຫມດ';
+$lang['samba_samba_directory_installed_warning'] = 'ຊອບແວ Directory Samba ຖືກຕິດຕັ້ງ, ສະນັ້ນທ່ານບໍ່ຈໍາເປັນຕ້ອງ app ນີ້.';
