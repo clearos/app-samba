0e91896e1895a8305bd410a03a7e16ddf3b1eece
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:57:24 +0000

    Add new files and directories, commit changes, push changes

--- language/so/samba_lang.php
+++ language/so/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Account Administrator';
-$lang['samba_app_description'] = 'Windows Networking (Samba) waxay bixisaa xabagta lagama maarmaanka u ah interoperate nidaamka Windows. app wuxuu bixiyaa adeegyo sugida, file iyo print, oo ay weheliyaan maamulka domain Windows.';
-$lang['samba_app_name'] = 'Windows Networking (Samba)';
-$lang['samba_computers'] = 'Computers';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver aan la initialized.';
-$lang['samba_file_share_tooltip'] = 'Haddii aad jeclaan lahayd si ay u abuuraan saamiyada file shabakadda, fadlan isticmaal app Flexshare ah.';
-$lang['samba_initializing_core_system'] = 'Talaalmayaa nidaamka asaasiga ah, fadlan samir.';
-$lang['samba_initializing_warning'] = 'Initialization qaadan kartaa daqiiqo ama laba, fadlan samir.';
-$lang['samba_master_node_needs_initialization'] = 'Master Guntin baahan yahay in la initialized hor Socota.';
-$lang['samba_mode'] = 'Mode';
-$lang['samba_samba_directory_installed_warning'] = 'The software Samba Directory la geliyo, oo sidaas daraaddeed idinku mar dambe ma u baahan tahay app this.';
+$lang['samba_administrator_account'] = 'Koontada Maamulaha';
+$lang['samba_app_description'] = 'Shabakadda Windows-ka (Samba) waxay ku siinaysaa jumlad lagama maarmaan ah si laysugu xiro nidaamyada Windows. App wuxuu bixiyaa adeegyada aqoonsiga, feyl iyo daabac, iyo sidoo kale maamulka domain Windows.';
+$lang['samba_app_name'] = 'Shabakadaha Windows-ka (Samba)';
+$lang['samba_computers'] = 'Kumbuyuutarada';
+$lang['samba_driver_has_not_been_initialized'] = 'Darawal aan la bilaabin.';
+$lang['samba_file_share_tooltip'] = 'Haddii aad jeclaan laheyd inaad abuurto saamiga faylka shabakadda, fadlan isticmaal faxsheegga Flexshare.';
+$lang['samba_initializing_core_system'] = 'Hordhaca nidaamka asaasiga ah, fadlan dulqaado.';
+$lang['samba_initializing_warning'] = 'Initialization waxay qaadan kartaa daqiiqo ama laba, fadlan dulqaado.';
+$lang['samba_master_node_needs_initialization'] = 'Master node wuxuu u baahan yahay in la bilaabo ka hor inta aan la bilaabin.';
+$lang['samba_mode'] = 'Qaabka';
+$lang['samba_samba_directory_installed_warning'] = 'Nidaamka Samba Directory waa la rakibay, sidaa daraadeed uma baahnid barnaamijkaan.';
