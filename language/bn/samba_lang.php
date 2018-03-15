6d3090a50e7f7cae0cae57b38d037653eceea64b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:19:38 +0000

    Add new files and directories, commit changes, push changes

--- language/bn/samba_lang.php
+++ language/bn/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'অ্যাডমিনিস্ট্রেটর অ্যাকাউন্ট';
-$lang['samba_app_description'] = 'উইন্ডোজ নেটওয়ার্কিং (সাম্বা) উইন্ডোজ সিস্টেমের সাথে interoperate প্রয়োজনীয় আঠা উপলব্ধ. অ্যাপ্লিকেশন উইন্ডোজ ডোমেইন পরিচালনার সঙ্গে বরাবর, অনুমোদন ব্যবস্থা, ফাইল এবং মুদ্রণ উপলব্ধ.';
+$lang['samba_administrator_account'] = 'প্রশাসক অ্যাকাউন্ট';
+$lang['samba_app_description'] = 'উইন্ডোজ নেটওয়ার্কিং (সাম্বা) উইন্ডোজ সিস্টেমে আন্তঃঅবহারের জন্য প্রয়োজনীয় আঠা সরবরাহ করে। অ্যাপটি উইন্ডোজ ডোমেন ব্যবস্থাপনা সহ প্রমাণীকরণ পরিষেবা, ফাইল এবং প্রিন্ট প্রদান করে।';
 $lang['samba_app_name'] = 'উইন্ডোজ নেটওয়ার্কিং (সাম্বা)';
 $lang['samba_computers'] = 'কম্পিউটার';
-$lang['samba_driver_has_not_been_initialized'] = 'ড্রাইভার সক্রিয়া করা হয়েছে.';
-$lang['samba_file_share_tooltip'] = 'আপনি নেটওয়ার্কের ফাইল শেয়ারের তৈরি করতে চান, তাহলে Flexshare অ্যাপ্লিকেশন ব্যবহার করুন.';
-$lang['samba_initializing_core_system'] = 'কোর সিস্টেম আরম্ভ করা, দয়া করে ধৈর্য ধরুন.';
-$lang['samba_initializing_warning'] = 'আরম্ভের একটি বা দুই মিনিট সময় নিতে পারে, দয়া করে ধৈর্য ধরুন.';
-$lang['samba_master_node_needs_initialization'] = 'মাস্টার নোড অগ্রসর হওয়ার আগে সক্রিয়া করা প্রয়োজন.';
+$lang['samba_driver_has_not_been_initialized'] = 'ড্রাইভার শুরু করা হয় নি।';
+$lang['samba_file_share_tooltip'] = 'আপনি যদি নেটওয়ার্ক ফাইল শেয়ার তৈরি করতে চান, তাহলে Flexshare অ্যাপ্লিকেশনটি ব্যবহার করুন।';
+$lang['samba_initializing_core_system'] = 'মূল সিস্টেম শুরু, ধৈর্য ধরুন দয়া করে।';
+$lang['samba_initializing_warning'] = 'শুরুতে এক বা দুই মিনিট সময় লাগতে পারে, দয়া করে ধৈর্য ধরুন।';
+$lang['samba_master_node_needs_initialization'] = 'অগ্রসর হওয়ার আগে মাস্টার নোডের সূচনা করতে হবে।';
 $lang['samba_mode'] = 'মোড';
-$lang['samba_samba_directory_installed_warning'] = 'সাম্বা নির্দেশিকা সফ্টওয়্যার ইনস্টল করা হয়, তাই আপনি আর এই অ্যাপ্লিকেশন প্রয়োজন.';
+$lang['samba_samba_directory_installed_warning'] = 'সাম্বা ডিরেক্টরি সফ্টওয়্যারটি ইনস্টল করা আছে, তাই আপনার আর এই অ্যাপ্লিকেশানটির প্রয়োজন নেই';
