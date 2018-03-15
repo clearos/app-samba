74e61a844029bacbdd2dca67c851d0d742d9b65b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:41:01 +0000

    Add new files and directories, commit changes, push changes

--- language/la/samba_lang.php
+++ language/la/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'Administrator Ratio';
-$lang['samba_app_description'] = 'Networking Windows (Burana) praebet necessaria ad gluten interoperate Fenestra ratio. A app authenticas operam praebet, et Print file, cum per Fenestra dominico elit.';
-$lang['samba_app_name'] = 'Networking Windows (Burana)';
-$lang['samba_computers'] = 'Home';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver non initialized.';
-$lang['samba_file_share_tooltip'] = 'Si vis creare file participat ipsum, commodo in Flexshare App.';
-$lang['samba_initializing_core_system'] = 'Initializing Core ratio, velit esse patientem.';
-$lang['samba_initializing_warning'] = 'Accipiat Initialization minutis duobus placere patientes.';
-$lang['samba_master_node_needs_initialization'] = 'Node indiget initialized in conspectu Domini procedens.';
-$lang['samba_mode'] = 'Modus';
-$lang['samba_samba_directory_installed_warning'] = 'Directorium software installed in Burana sic ultra hoc app.';
+$lang['samba_app_description'] = 'Fenestra networking (Burana) praebet necessaria sunt ad munera gluten in Fenestra interoperate systems. In app praebet authenticas servicia, lima quod procer, una cum administratione Fenestra domain.';
+$lang['samba_app_name'] = 'Fenestra networking (Burana)';
+$lang['samba_computers'] = 'Computers';
+$lang['samba_driver_has_not_been_initialized'] = 'Exactoris non initialized.';
+$lang['samba_file_share_tooltip'] = 'Si vos would amo ut creare file network partis, placere uti Flexshare app.';
+$lang['samba_initializing_core_system'] = 'Initializing core ratio, placere patientes estote.';
+$lang['samba_initializing_warning'] = 'Ut initialization ut minutam circiter duo, placere patientes estote.';
+$lang['samba_master_node_needs_initialization'] = 'Initialized nocte antequam dominus vult.';
+$lang['samba_mode'] = 'modus';
+$lang['samba_samba_directory_installed_warning'] = 'Quod is software installed Directory Burana, et non opus est tibi haec app.';
