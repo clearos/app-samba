c7abc6e3a89e0983fb7334334d12f406c69f140b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:48:20 +0000

    Add new files and directories, commit changes, push changes

--- language/mt/samba_lang.php
+++ language/mt/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Kont amministratur';
-$lang['samba_app_description'] = 'Twieqi Netwerking (Samba) jipprovdi l-kolla meħtieġa sabiex jinteroperaw mas-sistemi Windows. Il-app jipprovdi servizzi ta &#39;awtentikazzjoni, fajl u jistampa, flimkien mal-ġestjoni domain tal-Windows.';
-$lang['samba_app_name'] = 'Twieqi Netwerking (Samba)';
-$lang['samba_computers'] = 'kompjuters';
-$lang['samba_driver_has_not_been_initialized'] = 'Sewwieq ma ġietx initialized.';
-$lang['samba_file_share_tooltip'] = 'Jekk inti tixtieq li toħloq ishma fajl netwerk, jekk jogħġbok uża l-app Flexshare.';
-$lang['samba_initializing_core_system'] = 'Initializing sistema prinċipali, jekk jogħġbok kun pazjent.';
-$lang['samba_initializing_warning'] = 'Inizjalizzazzjoni jista &#39;jieħu minuta jew tnejn, jekk jogħġbok kun pazjent.';
-$lang['samba_master_node_needs_initialization'] = 'node Master jeħtieġ li jiġi initialized qabel ma tipproċedi.';
-$lang['samba_mode'] = 'modalità';
-$lang['samba_samba_directory_installed_warning'] = 'Is-softwer Samba Direttorju huwa installat, hekk m&#39;għadx għandek bżonn dan app.';
+$lang['samba_administrator_account'] = 'Kont Amministratur';
+$lang['samba_app_description'] = 'Netwerking bil-Windows (Samba) jipprovdi l-kolla meħtieġa biex tinteropera mas-sistemi tal-Windows. L-app tipprovdi servizzi ta &#39;awtentikazzjoni, fajl u stampar, flimkien mal-ġestjoni tad-dominju tal-Windows.';
+$lang['samba_app_name'] = 'Netwerking bil-Windows (Samba)';
+$lang['samba_computers'] = 'Kompjuters';
+$lang['samba_driver_has_not_been_initialized'] = 'Is-sewwieq ma ġiex inizjalizzat.';
+$lang['samba_file_share_tooltip'] = 'Jekk tixtieq toħloq ishma ta &#39;fajls tan-netwerk, jekk jogħġbok uża l-app FlexScare.';
+$lang['samba_initializing_core_system'] = 'L-inizjazzjoni tas-sistema ewlenija, jekk jogħġbok tkun pazjent.';
+$lang['samba_initializing_warning'] = 'L-inizjazzjoni tista &#39;tieħu minuta jew tnejn, jekk jogħġbok tkun pazjent.';
+$lang['samba_master_node_needs_initialization'] = 'Il-kaptan node jeħtieġ li jiġi inizjalizzat qabel ma jipproċedi.';
+$lang['samba_mode'] = 'Modalità';
+$lang['samba_samba_directory_installed_warning'] = 'Is-softwer tad-Direttorju Samba huwa installat, u għalhekk m&#39;għandekx bżonn din l-app.';
