758d0b75945e0552881879209aa252d6d8d7f0a9
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:05:39 +0000

    Add new files and directories, commit changes, push changes

--- language/tr/samba_lang.php
+++ language/tr/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'yönetici Hesabı';
-$lang['samba_app_description'] = 'Windows ağ (Samba) Windows sistemlerinde ile birlikte çalışan için gerekli tutkal sağlar. Uygulamanın Windows etki alanı yönetimi ile birlikte, kimlik doğrulama hizmetleri, dosya ve yazıcı sağlar.';
-$lang['samba_app_name'] = 'Windows ağ (Samba)';
+$lang['samba_administrator_account'] = 'Yönetici Hesabı';
+$lang['samba_app_description'] = 'Windows Ağ (Samba), Windows sistemleri ile birlikte çalışmak için gerekli tutkal sağlar. Uygulama, Windows etki alanı yönetimi ile birlikte kimlik doğrulama hizmetleri, dosya ve yazdırma sağlar.';
+$lang['samba_app_name'] = 'Windows Ağ (Samba)';
 $lang['samba_computers'] = 'Bilgisayarlar';
-$lang['samba_driver_has_not_been_initialized'] = 'Sürücü başlatılmadı.';
-$lang['samba_file_share_tooltip'] = 'ağ dosya paylaşımları oluşturmak istiyorsanız, Flexshare uygulamasını kullanın lütfen.';
-$lang['samba_initializing_core_system'] = 'çekirdek sistem başlatılıyor, lütfen sabırlı olun.';
-$lang['samba_initializing_warning'] = 'Başlatma bir ya da iki dakika sürebilir, lütfen sabırlı olun.';
-$lang['samba_master_node_needs_initialization'] = 'Ana düğüm geçmeden önce başlatılması gerekmektedir.';
+$lang['samba_driver_has_not_been_initialized'] = 'Sürücü başlatılmamış.';
+$lang['samba_file_share_tooltip'] = 'Ağ dosya paylaşımları oluşturmak istiyorsanız, lütfen Flexshare uygulamasını kullanın.';
+$lang['samba_initializing_core_system'] = 'Çekirdek sistemi başlatılıyor, lütfen sabırlı olun.';
+$lang['samba_initializing_warning'] = 'Başlatma bir veya iki dakika sürebilir, lütfen sabırlı olun.';
+$lang['samba_master_node_needs_initialization'] = 'Devam etmeden önce ana düğümün başlatılması gerekir.';
 $lang['samba_mode'] = 'kip';
-$lang['samba_samba_directory_installed_warning'] = 'artık bu uygulamayı gereken şekilde Samba Directory yazılım yüklenir.';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directory yazılımı yüklü, bu yüzden artık bu uygulamaya ihtiyacınız yok.';
