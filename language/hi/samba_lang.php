b1bb89a59ca23c62e0aefdacf7ea3c52ccdba251
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:32:34 +0000

    Add new files and directories, commit changes, push changes

--- language/hi/samba_lang.php
+++ language/hi/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'प्रशासक खाता';
-$lang['samba_app_description'] = 'विंडोज नेटवर्किंग (सांबा) विंडोज सिस्टम के साथ interoperate करने के लिए आवश्यक गोंद प्रदान करता है। एप्लिकेशन Windows डोमेन प्रबंधन के साथ-साथ, प्रमाणीकरण सेवाएं, फ़ाइल और प्रिंट प्रदान करता है।';
+$lang['samba_app_description'] = 'विंडोज नेटवर्किंग (सांबा) विंडोज सिस्टम के साथ इंटरऑपरेट करने के लिए आवश्यक गोंद प्रदान करता है ऐप प्रमाणीकरण सेवाएं, फ़ाइल और प्रिंट, विंडोज डोमेन प्रबंधन के साथ प्रदान करता है';
 $lang['samba_app_name'] = 'विंडोज नेटवर्किंग (सांबा)';
-$lang['samba_computers'] = 'कम्प्यूटर';
-$lang['samba_driver_has_not_been_initialized'] = 'ड्राइवर प्रारंभ नहीं किया गया है।';
-$lang['samba_file_share_tooltip'] = 'आप नेटवर्क फ़ाइल साझा बनाने के लिए चाहते हैं, तो Flexshare एप्लिकेशन का इस्तेमाल करें।';
-$lang['samba_initializing_core_system'] = 'कोर प्रणाली आरंभ किया जा रहा है, कृपया धैर्य रखें।';
-$lang['samba_initializing_warning'] = 'प्रारंभ एक या दो मिनट का समय लग सकता है, कृपया धैर्य रखें।';
-$lang['samba_master_node_needs_initialization'] = 'मास्टर नोड आगे बढ़ने से पहले प्रारंभ की जा करने की जरूरत है।';
+$lang['samba_computers'] = 'कंप्यूटर';
+$lang['samba_driver_has_not_been_initialized'] = 'चालक को आरम्भ नहीं किया गया है।';
+$lang['samba_file_share_tooltip'] = 'यदि आप नेटवर्क फ़ाइल साझा करना चाहते हैं, तो कृपया Flexshare ऐप का उपयोग करें।';
+$lang['samba_initializing_core_system'] = 'कोर सिस्टम शुरू करने, कृपया धैर्य रखें।';
+$lang['samba_initializing_warning'] = 'शुरूआत में एक या दो मिनट लग सकते हैं, कृपया धैर्य रखें।';
+$lang['samba_master_node_needs_initialization'] = 'आगे बढ़ने से पहले मास्टर नोड को आरंभ करने की आवश्यकता है।';
 $lang['samba_mode'] = 'मोड';
-$lang['samba_samba_directory_installed_warning'] = 'सांबा निर्देशिका सॉफ्टवेयर स्थापित किया गया है, ताकि आप अब इस अनुप्रयोग की जरूरत है।';
+$lang['samba_samba_directory_installed_warning'] = 'साम्बा निर्देशिका सॉफ्टवेयर स्थापित है, इसलिए अब आपको इस ऐप की आवश्यकता नहीं है।';
