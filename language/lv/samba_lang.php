ba0de83f5f7de4ac07cbfd924b4670943155cf26
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:43:10 +0000

    Add new files and directories, commit changes, push changes

--- language/lv/samba_lang.php
+++ language/lv/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Administrator konts';
-$lang['samba_app_description'] = 'Windows Networking (Samba) nodrošina nepieciešamo līmi ir savietojami ar Windows sistēmām. App sniedz autentificēšanas pakalpojumus, failu un drukāt, kopā ar Windows domēna pārvaldību.';
-$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_administrator_account'] = 'Administratora konts';
+$lang['samba_app_description'] = 'Windows tīkli (Samba) nodrošina nepieciešamo līmi, lai sadarbotos ar Windows sistēmām. Lietojumprogramma nodrošina autentifikācijas pakalpojumus, failu un drukāšanu, kā arī Windows domēnu pārvaldību.';
+$lang['samba_app_name'] = 'Windows tīklošana (Samba)';
 $lang['samba_computers'] = 'Datori';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver nav inicializēts.';
-$lang['samba_file_share_tooltip'] = 'Ja jūs vēlētos, lai radītu tīkla failu akcijas, lūdzu, izmantojiet Flexshare lietotni.';
-$lang['samba_initializing_core_system'] = 'Inicializēšana pamatsistēmu, lūdzu, esiet pacietīgi.';
-$lang['samba_initializing_warning'] = 'Inicializācijas var aizņemt minūti vai divas, lūdzu, esiet pacietīgi.';
-$lang['samba_master_node_needs_initialization'] = 'Master mezglā jābūt inicializēts pirms turpināt.';
-$lang['samba_mode'] = 'mode';
-$lang['samba_samba_directory_installed_warning'] = 'Samba Directory programmatūra ir instalēta, tāpēc jums vairs nav nepieciešams šo app.';
+$lang['samba_driver_has_not_been_initialized'] = 'Vadītājs nav inicializēts.';
+$lang['samba_file_share_tooltip'] = 'Ja vēlaties izveidot tīkla failu koplietošanu, lūdzu, izmantojiet lietotni Flexshare.';
+$lang['samba_initializing_core_system'] = 'Inicializējot pamatcentru, lūdzu, esiet pacietīgi.';
+$lang['samba_initializing_warning'] = 'Inicializācija var ilgt vienu vai divas minūtes, lūdzu, esiet pacietīgi.';
+$lang['samba_master_node_needs_initialization'] = 'Pirms turpināšanas ir jāiestata galvenais mezgls.';
+$lang['samba_mode'] = 'Režīms';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directory programmatūra ir instalēta, tāpēc jums vairs nav šīs lietotnes.';
