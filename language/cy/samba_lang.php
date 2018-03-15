c4a451960649f5a222a8f15dc8de8f37d1ea1412
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:08:44 +0000

    Add new files and directories, commit changes, push changes

--- language/cy/samba_lang.php
+++ language/cy/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Cyfrif Administrator';
-$lang['samba_app_description'] = 'Windows Rhwydweithio (Samba) yn darparu y glud sy&#39;n angenrheidiol i hwyluso rhyngweithio â systemau Windows. Mae&#39;r app yn darparu gwasanaethau dilysu, ffeiliau ac argraffu, ynghyd â rheoli parth Windows.';
-$lang['samba_app_name'] = 'Windows Rhwydweithio (Samba)';
-$lang['samba_computers'] = 'cyfrifiaduron';
-$lang['samba_driver_has_not_been_initialized'] = 'Nid yw gyrrwr wedi&#39;i ymgychwyn.';
-$lang['samba_file_share_tooltip'] = 'Os hoffech chi i greu cyfranddaliadau ffeil rhwydwaith, defnyddiwch y app Flexshare.';
-$lang['samba_initializing_core_system'] = 'Ymgychwyn system graidd, byddwch yn amyneddgar.';
-$lang['samba_initializing_warning'] = 'Gall initialization gymryd munud neu ddau, byddwch yn amyneddgar.';
-$lang['samba_master_node_needs_initialization'] = 'Mae angen ymgychwyn cyn symud ymlaen Meistr nôd.';
-$lang['samba_mode'] = 'modd';
-$lang['samba_samba_directory_installed_warning'] = 'Mae&#39;r meddalwedd Cyfeiriadur Samba ei osod, felly mae angen app hwn mwyach.';
+$lang['samba_administrator_account'] = 'Cyfrif Gweinyddwr';
+$lang['samba_app_description'] = 'Mae Rhwydweithiau Windows (Samba) yn darparu&#39;r glud angenrheidiol i gydweithio â systemau Windows. Mae&#39;r app yn darparu gwasanaethau dilysu, ffeilio ac argraffu, ynghyd â rheoli parth Windows.';
+$lang['samba_app_name'] = 'Rhwydweithio Windows (Samba)';
+$lang['samba_computers'] = 'Cyfrifiaduron';
+$lang['samba_driver_has_not_been_initialized'] = 'Nid yw gyrrwr wedi ei gychwyn.';
+$lang['samba_file_share_tooltip'] = 'Os hoffech greu cyfrannau ffeiliau rhwydwaith, defnyddiwch yr app Flexshare.';
+$lang['samba_initializing_core_system'] = 'Wrth ddechrau&#39;r system graidd, byddwch yn amyneddgar.';
+$lang['samba_initializing_warning'] = 'Gall y broses gychwynnol gymryd munud neu ddau, byddwch yn amyneddgar.';
+$lang['samba_master_node_needs_initialization'] = 'Mae angen dechrau&#39;r prif feistr cyn mynd ymlaen.';
+$lang['samba_mode'] = 'Modd';
+$lang['samba_samba_directory_installed_warning'] = 'Mae meddalwedd Cyfeiriadur Samba wedi&#39;i osod, felly does dim angen yr app yma arnoch bellach.';
