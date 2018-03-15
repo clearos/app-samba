700cf6027696e89c94cd72b9cc967ef4b8816abf
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:18:23 +0000

    Add new files and directories, commit changes, push changes

--- language/eu/samba_lang.php
+++ language/eu/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Administratzailearen kontua';
-$lang['samba_app_description'] = 'Windows Networking (Samba) beharrezko kola to Windows sistema batzuekin elkar eskaintzen. Aplikazioa autentifikazio zerbitzuak, fitxategi eta inprimatu eskaintzen, Windows domeinu kudeaketa batera.';
-$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_administrator_account'] = 'Administratzaile kontua';
+$lang['samba_app_description'] = 'Windows sarean (Samba) Windows sistemarekin elkarreragiteko beharrezko kola ematen du. Aplikazioek autentifikazio zerbitzuak, fitxategiak eta inprimakiak eskaintzen dituzte, Windows domeinuaren kudeaketarekin batera.';
+$lang['samba_app_name'] = 'Windows sarean (Samba)';
 $lang['samba_computers'] = 'Ordenagailuak';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver ez du hasieratu egin dira.';
-$lang['samba_file_share_tooltip'] = 'duzu sare fitxategi dira partekatu nahi izanez gero, mesedez, erabili Flexshare aplikazioa.';
-$lang['samba_initializing_core_system'] = 'core sistema hasieratzen, mesedez izan pazientzia.';
-$lang['samba_initializing_warning'] = 'Hasieratzeko minutu bat edo bi hartu ahal izango dute, pazientzia izan.';
-$lang['samba_master_node_needs_initialization'] = 'Master nodo jarraitu aurretik hasieratu behar.';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver ez da hasieratu.';
+$lang['samba_file_share_tooltip'] = 'Sareko fitxategiak partekatzea nahi baduzu, erabili Flexshare aplikazioa.';
+$lang['samba_initializing_core_system'] = 'Core sistema hasieratzen, pazientzia izan.';
+$lang['samba_initializing_warning'] = 'Hasieraketak minutu bat edo bi eman ditzake, gaixo izan.';
+$lang['samba_master_node_needs_initialization'] = 'Master nodoa hasieratu egin behar da aurrera egin aurretik.';
 $lang['samba_mode'] = 'Modu';
-$lang['samba_samba_directory_installed_warning'] = 'Samba Directory Software instalatu, beraz, behar ez dituzun aplikazio hau.';
+$lang['samba_samba_directory_installed_warning'] = 'Samba direktorioaren softwarea instalatuta dago, beraz ez duzu jada aplikazioa behar.';
