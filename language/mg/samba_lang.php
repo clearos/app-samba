b30009eed8997c538846c739433794c5652f619f
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:43:36 +0000

    Add new files and directories, commit changes, push changes

--- language/mg/samba_lang.php
+++ language/mg/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Administrator Account';
-$lang['samba_app_description'] = 'Windows Fifaneraserana (Samba) dia manome ny ilaina lakaoly mba interoperate amin&#39;ny Windows rafitra. Ny fampiharana manome asa fanamarinana, rakitra sy pirinty, miaraka amin&#39;ny Windows sehatra fitantanana.';
-$lang['samba_app_name'] = 'Windows Fifaneraserana (Samba)';
+$lang['samba_administrator_account'] = 'Administrateur Account';
+$lang['samba_app_description'] = 'Windows Networking (Samba) no manome ny glue ilaina mba hiatrehana ny rafitra Windows. Ny fampiharana dia manome tolotra fanamarinana, rakitra ary pirinty, miaraka amin&#39;ny fitantanana Windows doméne.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
 $lang['samba_computers'] = 'Computers';
-$lang['samba_driver_has_not_been_initialized'] = 'Mpamily no tsy initialized.';
-$lang['samba_file_share_tooltip'] = 'Raha te-hamorona tambajotra rakitra mizara, azafady mampiasa ny Flexshare fampiharana.';
-$lang['samba_initializing_core_system'] = 'Nitranga teo am-rafitra fototra, azafady hanam-paharetana.';
-$lang['samba_initializing_warning'] = 'Initialization Mety ho iray minitra na roa, azafady hanam-paharetana.';
-$lang['samba_master_node_needs_initialization'] = 'Master node tokony initialized alohan&#39;ny.';
+$lang['samba_driver_has_not_been_initialized'] = 'Tsy nampidirina voalohany ny mpamily.';
+$lang['samba_file_share_tooltip'] = 'Raha te hamorona orinasan&#39;ny rakitra ianao, azafady ampiasao ny app Flexshare.';
+$lang['samba_initializing_core_system'] = 'Atomboka ny rafitra fototra, ataovy marary.';
+$lang['samba_initializing_warning'] = 'Ny fandraisana an-tanana dia afaka iray minitra na roa, azafady manàna faharetana.';
+$lang['samba_master_node_needs_initialization'] = 'Mila manomboka ny mozika Master alohan&#39;ny handehanana.';
 $lang['samba_mode'] = 'Mode';
-$lang['samba_samba_directory_installed_warning'] = 'Ny Samba Directory rindrambaiko no nametraka, mba tsy mila fampiharana izany.';
+$lang['samba_samba_directory_installed_warning'] = 'Ny rindrambaiko Directory Samba Directory dia napetraka, noho izany dia tsy mila intsony ity app ity ianao.';
