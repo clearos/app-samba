d54e48bdd9f676dcd603560a9fbff542533000fe
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:54:48 +0000

    Add new files and directories, commit changes, push changes

--- language/sr/samba_lang.php
+++ language/sr/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'администратор рачуна';
-$lang['samba_app_description'] = 'Виндовс умрежавање (Самба) обезбеђује потребну лепак за сарадњу са Виндовс системима. Апликација пружа услуге Аутхентицатион, и одштампа, заједно са Виндовс Манагемент домена.';
+$lang['samba_administrator_account'] = 'Администраторски налог';
+$lang['samba_app_description'] = 'Виндовс Нетворкинг (Самба) пружа потребан лепак за интероперабилност са Виндовс системима. Апликација пружа услуге за аутентикацију, датотеку и штампање, заједно са управљањем домена Виндовса.';
 $lang['samba_app_name'] = 'Виндовс умрежавање (Самба)';
-$lang['samba_computers'] = 'Компјутери';
-$lang['samba_driver_has_not_been_initialized'] = 'Возач није покренут.';
-$lang['samba_file_share_tooltip'] = 'Ако желите да направите акција Нетворк Филе, користите Флексхаре апликацију.';
-$lang['samba_initializing_core_system'] = 'Инитиализинг главни систем, будите стрпљиви.';
-$lang['samba_initializing_warning'] = 'Инитиализатион може да потраје минут или два, будите стрпљиви.';
-$lang['samba_master_node_needs_initialization'] = 'Мастер чвор треба да буде покренут пре него што наставите.';
-$lang['samba_mode'] = 'начин';
-$lang['samba_samba_directory_installed_warning'] = 'Самба Директоријум софтвер инсталиран, тако да више не треба ову апликацију.';
+$lang['samba_computers'] = 'Рачунари';
+$lang['samba_driver_has_not_been_initialized'] = 'Возач није иницијализован.';
+$lang['samba_file_share_tooltip'] = 'Ако желите да креирате акције у мрежи, молимо Вас да користите апликацију Флексхаре.';
+$lang['samba_initializing_core_system'] = 'Иницирање језгре система, молимо вас да будете стрпљиви.';
+$lang['samba_initializing_warning'] = 'Иницијализација може трајати минут или два, молимо вас да будете стрпљиви.';
+$lang['samba_master_node_needs_initialization'] = 'Мастер чвор треба иницијализирати пре него што наставите.';
+$lang['samba_mode'] = 'Режим';
+$lang['samba_samba_directory_installed_warning'] = 'Софтвер Самба Дирецтори је инсталиран, тако да вам више није потребна ова апликација.';
