80c795d4e38f4abbc54fda5e952c6abf546fb9cf
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:39:19 +0000

    Add new files and directories, commit changes, push changes

--- language/km/samba_lang.php
+++ language/km/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'គណនីអ្នកគ្រប់គ្រង';
-$lang['samba_app_description'] = 'ប្រព័ន្ធប្រតិបត្តិការ Windows បណ្ដាញ (Samba) ផ្តល់នូវការកាវបិទការចាំបាច់ដើម្បី interoperate ជាមួយប្រព័ន្ធ Windows ។ កម្មវិធីនេះបានផ្តល់នូវសេវាការផ្ទៀងផ្ទាត់ភាពត្រឹមត្រូវ, ឯកសារនិងការបោះពុម្ព, ការគ្រប់គ្រងដែនរួមជាមួយនឹងប្រព័ន្ធប្រតិបត្តិការ Windows ។';
-$lang['samba_app_name'] = 'ប្រព័ន្ធប្រតិបត្តិការ Windows បណ្ដាញ (Samba)';
+$lang['samba_app_description'] = 'បណ្តាញវីនដូ (Samba) ផ្តល់នូវកាវបិទចាំបាច់ដើម្បីធ្វើអន្តរកម្មជាមួយប្រព័ន្ធវីនដូ។ កម្មវិធីផ្តល់នូវសេវាកម្មផ្ទៀងផ្ទាត់ឯកសារនិងការបោះពុម្ពរួមជាមួយការគ្រប់គ្រងដែនរបស់វីនដូ។';
+$lang['samba_app_name'] = 'បណ្តាញវីនដូ (Samba)';
 $lang['samba_computers'] = 'កុំព្យូទ័រ';
-$lang['samba_driver_has_not_been_initialized'] = 'កម្មវិធីបញ្ជាមិនត្រូវបានចាប់ផ្តើមទេ។';
-$lang['samba_file_share_tooltip'] = 'ប្រសិនបើអ្នកចង់បង្កើតការចែករំលែកឯកសារបណ្តាញ, សូមប្រើកម្មវិធី Flexshare ។';
-$lang['samba_initializing_core_system'] = 'ចាប់ផ្ដើមប្រព័ន្ធស្នូលសូមអត់ធ្មត់។';
-$lang['samba_initializing_warning'] = 'ដំបូងអាចនឹងចំណាយមួយឬពីរនាទីសូមអត់ធ្មត់។';
-$lang['samba_master_node_needs_initialization'] = 'ថ្នាំងអនុបណ្ឌិតចាំបាច់ត្រូវរៀបចំមុននឹងបន្ត។';
+$lang['samba_driver_has_not_been_initialized'] = 'កម្មវិធីបញ្ជាមិនត្រូវបានចាប់ផ្ដើម។';
+$lang['samba_file_share_tooltip'] = 'ប្រសិនបើអ្នកចង់បង្កើតការចែករំលែកឯកសារបណ្ដាញសូមប្រើកម្មវិធី Flexshare ។';
+$lang['samba_initializing_core_system'] = 'កំពុងចាប់ផ្ដើមប្រព័ន្ធស្នូលសូមអត់ធ្មត់។';
+$lang['samba_initializing_warning'] = 'ការចាប់ផ្តើមអាចចំណាយពេលមួយឬពីរនាទីសូមអត់ធ្មត់។';
+$lang['samba_master_node_needs_initialization'] = 'តម្រូវឱ្យចាប់ផ្ដើមថ្នាំងមេមុនពេលដំណើរការ។';
 $lang['samba_mode'] = 'របៀប';
-$lang['samba_samba_directory_installed_warning'] = 'នេះជាកម្មវិធីថត Samba ត្រូវបានដំឡើងឡើយដូច្នេះអ្នកមិនត្រូវការកម្មវិធីនេះ។';
+$lang['samba_samba_directory_installed_warning'] = 'ផ្នែកទន់ថតរបស់ Samba ត្រូវបានតំឡើងដូច្នេះអ្នកមិនត្រូវការកម្មវិធីនេះទៀតទេ។';
