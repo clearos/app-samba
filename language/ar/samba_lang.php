74220f617eea9ec27ff09f12d761d6853b721b46
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:16:15 +0000

    Add new files and directories, commit changes, push changes

--- language/ar/samba_lang.php
+++ language/ar/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'حساب المسؤول';
-$lang['samba_app_description'] = 'Windows شبكة (سامبا) على الغراء الضروري للتعامل مع أنظمة ويندوز. ويقدم التطبيق خدمات التوثيق، والملفات والطباعة، جنبا إلى جنب مع إدارة مجال Windows.';
-$lang['samba_app_name'] = 'Windows شبكة (سامبا)';
+$lang['samba_app_description'] = 'يوفر Windows Networking (Samba) الغراء الضروري للتشغيل المتداخل مع أنظمة Windows. يوفر التطبيق خدمات المصادقة والملفات والطباعة ، جنبا إلى جنب مع إدارة مجال Windows.';
+$lang['samba_app_name'] = 'شبكات ويندوز (سامبا)';
 $lang['samba_computers'] = 'أجهزة الكمبيوتر';
-$lang['samba_driver_has_not_been_initialized'] = 'لم تتم تهيئة السائق.';
-$lang['samba_file_share_tooltip'] = 'إذا كنت ترغب في إنشاء مشاركات الملفات شبكة، يرجى استخدام التطبيق Flexshare.';
-$lang['samba_initializing_core_system'] = 'تهيئة النظام الأساسي، يرجى التحلي بالصبر.';
-$lang['samba_initializing_warning'] = 'التهيئة قد يستغرق دقيقة أو اثنتين، يرجى التحلي بالصبر.';
-$lang['samba_master_node_needs_initialization'] = 'تحتاج عقدة رئيسية ليتم تهيئتها قبل المتابعة.';
+$lang['samba_driver_has_not_been_initialized'] = 'لم يتم تهيئة برنامج التشغيل.';
+$lang['samba_file_share_tooltip'] = 'إذا كنت ترغب في إنشاء مشاركات ملفات الشبكة ، فالرجاء استخدام تطبيق Flexshare.';
+$lang['samba_initializing_core_system'] = 'تهيئة النظام الأساسي ، يرجى التحلي بالصبر.';
+$lang['samba_initializing_warning'] = 'قد تستغرق عملية التهيئة دقيقة أو دقيقتين ، يرجى التحلي بالصبر.';
+$lang['samba_master_node_needs_initialization'] = 'تحتاج العقدة الرئيسية إلى التهيئة قبل المتابعة.';
 $lang['samba_mode'] = 'الوضع';
-$lang['samba_samba_directory_installed_warning'] = 'تثبيت البرنامج سامبا دليل، لذلك لم تعد تحتاج هذا التطبيق.';
+$lang['samba_samba_directory_installed_warning'] = 'تم تثبيت برنامج Samba Directory ، لذلك لم تعد بحاجة إلى هذا التطبيق.';
