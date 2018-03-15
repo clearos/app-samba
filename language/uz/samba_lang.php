17ac41dc91114ffbed72cab549b594160f626a4c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:07:27 +0000

    Add new files and directories, commit changes, push changes

--- language/uz/samba_lang.php
+++ language/uz/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Administrator hisobi';
-$lang['samba_app_description'] = 'Windows Networking (Samba) Windows tizimlari bilan ishlashga zarur elim beradi. App Windows domen boshqarish bilan birga, autentifikatsiya xizmatlar, fayl va kiritish beradi.';
-$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_administrator_account'] = 'Administrator hisob qaydnomasi';
+$lang['samba_app_description'] = 'Windows tarmoqlari (Samba) Windows tizimlar bilan ishlash uchun zarur bo&#39;lgan yopishqoqlikni ta&#39;minlaydi. Ilova Windows domenini boshqarish bilan birgalikda autentifikatsiya xizmatlarini, fayl va chop etishni ta&#39;minlaydi.';
+$lang['samba_app_name'] = 'Windows tarmoqlari (Samba)';
 $lang['samba_computers'] = 'Kompyuterlar';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver başlatılmamış.';
-$lang['samba_file_share_tooltip'] = 'Agar tarmoq fayl aksiyalarini yaratish istasangiz, Flexshare ilovadan foydalaning.';
-$lang['samba_initializing_core_system'] = 'asosiy tizimi ishga, iltimos, sabr qiling.';
-$lang['samba_initializing_warning'] = 'Initialization bir daqiqa yoki ikki o&#39;tishi mumkin, iltimos, sabr qiling.';
-$lang['samba_master_node_needs_initialization'] = 'Master tugun davom ettirishdan oldin boshlash kerak.';
-$lang['samba_mode'] = 'usul';
-$lang['samba_samba_directory_installed_warning'] = 'Agar endi bu ilovani kerak, shuning uchun Samba Directory dasturi, o&#39;rnatilgan.';
+$lang['samba_driver_has_not_been_initialized'] = 'Drayv ishga tushirilmadi.';
+$lang['samba_file_share_tooltip'] = 'Tarmoq fayllari aktsiyalarini yaratmoqchi bo&#39;lsangiz, iltimos, Flexshare ilovasidan foydalaning.';
+$lang['samba_initializing_core_system'] = 'Asosiy tizimni ishga tushirish uchun iltimos, sabr-toqatli bo&#39;ling.';
+$lang['samba_initializing_warning'] = 'Initializatsiya bir daqiqa yoki ikki daqiqa o&#39;tishi mumkin, iltimos, sabr qiling.';
+$lang['samba_master_node_needs_initialization'] = 'Davom etishdan oldin master tugunni ishga tushirish kerak.';
+$lang['samba_mode'] = 'Tartibni';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directory dasturi o&#39;rnatildi, shuning uchun endi ushbu ilovaga ehtiyoj sezmaysiz.';
