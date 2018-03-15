45224085c90b2ef060c97a06f9abf1fcf197a4d4
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:04:43 +0000

    Add new files and directories, commit changes, push changes

--- language/tl/samba_lang.php
+++ language/tl/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'administrator Account';
-$lang['samba_app_description'] = 'Windows Networking (Samba) ay nagbibigay ng mga kinakailangang pandikit sa interoperate sa mga sistema ng Windows. Ang app ay nagbibigay ng mga serbisyo authentication, file at i-print, kasama ang Windows pamamahala domain.';
+$lang['samba_administrator_account'] = 'Account ng Administrator';
+$lang['samba_app_description'] = 'Ang Windows Networking (Samba) ay nagbibigay ng kinakailangang pangkola upang makipag-ugnayan sa mga system ng Windows. Ang app ay nagbibigay ng mga serbisyo ng pagpapatunay, file at pag-print, kasama ang pamamahala ng domain ng Windows.';
 $lang['samba_app_name'] = 'Windows Networking (Samba)';
-$lang['samba_computers'] = 'Computers';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver ay hindi pa nasimulan.';
-$lang['samba_file_share_tooltip'] = 'Kung nais mong lumikha ng namamahagi ng network file, mangyaring gamitin ang Flexshare app.';
-$lang['samba_initializing_core_system'] = 'Sinisimulan core system, mangyaring maging mahinahon.';
-$lang['samba_initializing_warning'] = 'Initialization ay maaaring tumagal ng isang minuto o dalawa, mangyaring maging mahinahon.';
-$lang['samba_master_node_needs_initialization'] = 'Master node pangangailangan upang maging initialize bago magpatuloy.';
-$lang['samba_mode'] = 'paraan';
-$lang['samba_samba_directory_installed_warning'] = 'Ang Samba Directory software ay naka-install, kaya hindi mo na kailangan ang app na ito.';
+$lang['samba_computers'] = 'Mga Computer';
+$lang['samba_driver_has_not_been_initialized'] = 'Ang driver ay hindi pa nasimulan.';
+$lang['samba_file_share_tooltip'] = 'Kung nais mong lumikha ng pagbabahagi ng file ng network, mangyaring gamitin ang Flexshare app.';
+$lang['samba_initializing_core_system'] = 'Pinasimulan ang core system, mangyaring maging matiyaga.';
+$lang['samba_initializing_warning'] = 'Maaaring tumagal ng isang minuto o dalawa ang pagsisimula, mangyaring maging matiyaga.';
+$lang['samba_master_node_needs_initialization'] = 'Kailangan ng node ng Master na magpasimula bago magpatuloy.';
+$lang['samba_mode'] = 'Mode';
+$lang['samba_samba_directory_installed_warning'] = 'Ang software ng Samba Directory ay na-install, kaya hindi mo na kailangan ang app na ito.';
