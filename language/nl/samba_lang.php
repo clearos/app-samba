446dac9b0c62e46a3d52372f5710e95d3eada8ee
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:49:38 +0000

    Add new files and directories, commit changes, push changes

--- language/nl/samba_lang.php
+++ language/nl/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'Administrator-account';
-$lang['samba_app_description'] = 'Windows Networking (Samba) zorgt voor de nodige lijm om samen te werken met Windows-systemen. De app biedt authenticatie diensten, bestanden en afdrukken, samen met Windows domeinbeheer.';
+$lang['samba_app_description'] = 'Windows Networking (Samba) biedt de benodigde lijm om te kunnen samenwerken met Windows-systemen. De app biedt verificatieservices, bestanden en afdrukken, samen met Windows-domeinbeheer.';
 $lang['samba_app_name'] = 'Windows Networking (Samba)';
 $lang['samba_computers'] = 'computers';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver is niet geïnitialiseerd.';
-$lang['samba_file_share_tooltip'] = 'Als u wilt netwerk file shares te maken, gebruik dan de Flexshare app.';
-$lang['samba_initializing_core_system'] = 'Initialiseren kern van het systeem, wees geduldig.';
+$lang['samba_driver_has_not_been_initialized'] = 'Stuurprogramma is niet geïnitialiseerd.';
+$lang['samba_file_share_tooltip'] = 'Als u gedeelde netwerkbestanden wilt maken, gebruikt u de Flexshare-app.';
+$lang['samba_initializing_core_system'] = 'Het kernsysteem initialiseren, wees geduldig.';
 $lang['samba_initializing_warning'] = 'Initialisatie kan een minuut of twee duren, wees geduldig.';
-$lang['samba_master_node_needs_initialization'] = 'Master node moet worden geïnitialiseerd voordat u verder gaat.';
+$lang['samba_master_node_needs_initialization'] = 'Het hoofdknooppunt moet worden geïnitialiseerd voordat u doorgaat.';
 $lang['samba_mode'] = 'mode';
-$lang['samba_samba_directory_installed_warning'] = 'De Samba Directory software is geïnstalleerd, zodat u niet langer deze app nodig.';
+$lang['samba_samba_directory_installed_warning'] = 'De Samba Directory-software is geïnstalleerd, dus je hebt deze app niet langer nodig.';
