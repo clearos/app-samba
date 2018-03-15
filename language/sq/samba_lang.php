095dc4366075580ad7e42211322b86ceb817203a
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:15:47 +0000

    Add new files and directories, commit changes, push changes

--- language/sq/samba_lang.php
+++ language/sq/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Llogaria Administrator';
-$lang['samba_app_description'] = 'Windows Networking (Samba) siguron zam nevojshme për të ndërveprojnë me sistemin Windows. Aplikacioni ofron shërbime të legalizuara, fotografi dhe të shtypura, së bashku me menaxhimin domain Windows-it.';
-$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_administrator_account'] = 'Llogaria e administratorit';
+$lang['samba_app_description'] = 'Windows Networking (Samba) siguron ngjitësin e nevojshëm për të ndërvepruar me sistemet e Windows. Aplikacioni ofron shërbime të legalizuara, fotografi dhe printime, së bashku me menaxhimin e domain të Windows.';
+$lang['samba_app_name'] = 'Rrjetëzimi i Windows (Samba)';
 $lang['samba_computers'] = 'Kompjuter';
-$lang['samba_driver_has_not_been_initialized'] = 'Shoferi nuk ka nisur.';
-$lang['samba_file_share_tooltip'] = 'Nëse ju do të donte për të krijuar rrjeti fotografi aksione, ju lutem, përdorni app Flexshare.';
-$lang['samba_initializing_core_system'] = 'Fillimin e sistemit bazë, ju lutem të jetë i durueshëm.';
-$lang['samba_initializing_warning'] = 'Initialization mund të marrë një minutë apo dy, ju lutemi të jetë i durueshëm.';
-$lang['samba_master_node_needs_initialization'] = 'nyje Master duhet të inicializohet para se të vazhdoni.';
+$lang['samba_driver_has_not_been_initialized'] = 'Shoferi nuk është inicializuar.';
+$lang['samba_file_share_tooltip'] = 'Nëse dëshironi të krijoni aksione të skedarëve të rrjetit, përdorni aplikacionin Flexshare.';
+$lang['samba_initializing_core_system'] = 'Fillimi i sistemit bazë, ju lutemi të jeni të duruar.';
+$lang['samba_initializing_warning'] = 'Fillimi mund të zgjasë një ose dy minuta, ju lutemi të jeni të duruar.';
+$lang['samba_master_node_needs_initialization'] = 'Nyja Master duhet të fillojë përpara se të vazhdohet.';
 $lang['samba_mode'] = 'mënyrë';
-$lang['samba_samba_directory_installed_warning'] = 'Software Samba Directory është i instaluar, kështu që ju nuk ka më nevojë këtë app.';
+$lang['samba_samba_directory_installed_warning'] = 'Instalohet softueri Samba Directory, kështu që nuk keni më nevojë për këtë aplikacion.';
