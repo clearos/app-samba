930734ce76fe33baa28504bc4ed13e63bf4f192b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:06:05 +0000

    Add new files and directories, commit changes, push changes

--- language/uk/samba_lang.php
+++ language/uk/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'Обліковий запис адміністратора';
-$lang['samba_app_description'] = 'Мереж Windows (Samba) забезпечує необхідний клей для взаємодії з системами Windows. Додаток надає послуги аутентифікації, файлів і друку, поряд з управлінням домену Windows.';
-$lang['samba_app_name'] = 'Мереж Windows (Samba)';
-$lang['samba_computers'] = 'комп&#39;ютери';
-$lang['samba_driver_has_not_been_initialized'] = 'Водій не був инициализирован.';
-$lang['samba_file_share_tooltip'] = 'Якщо ви хотіли б створити акції мережі файлів, необхідно використовувати додаток FlexShare.';
-$lang['samba_initializing_core_system'] = 'Ініціалізація ядра системи, будь ласка, будьте терплячі.';
-$lang['samba_initializing_warning'] = 'Ініціалізація може зайняти хвилину або дві, будь ласка, будьте терплячі.';
-$lang['samba_master_node_needs_initialization'] = 'Майстер вузол слід буде почати, перш ніж продовжити.';
-$lang['samba_mode'] = 'режим';
-$lang['samba_samba_directory_installed_warning'] = 'Програмне забезпечення Samba каталог встановлюється, так що вам більше не потрібно це додаток.';
+$lang['samba_app_description'] = 'Мережа Windows (Samba) забезпечує необхідний клей для взаємодії з системами Windows. Додаток забезпечує послуги аутентифікації, файли та друку, а також керування доменом Windows.';
+$lang['samba_app_name'] = 'Мережева мережа Windows (Samba)';
+$lang['samba_computers'] = 'Комп&#39;ютери';
+$lang['samba_driver_has_not_been_initialized'] = 'Водій не був ініціалізований.';
+$lang['samba_file_share_tooltip'] = 'Якщо ви хочете створити мережеві файлові ресурси, скористайтеся програмою Flexshare.';
+$lang['samba_initializing_core_system'] = 'Ініціалізація основної системи, будь ласка, будьте терплячі.';
+$lang['samba_initializing_warning'] = 'Ініціалізація може тривати хвилину або дві, будь ласка, будь терплячим.';
+$lang['samba_master_node_needs_initialization'] = 'Майстер вузла потрібно ініціалізувати, перш ніж продовжувати.';
+$lang['samba_mode'] = 'Режим';
+$lang['samba_samba_directory_installed_warning'] = 'Програма Samba Directory встановлена, тому вам більше не потрібна ця програма.';
