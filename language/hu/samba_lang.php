cb635d562c6a4012fab2b261f02a49edb25e371c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:33:00 +0000

    Add new files and directories, commit changes, push changes

--- language/hu/samba_lang.php
+++ language/hu/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'Rendszergazda fiók';
-$lang['samba_app_description'] = 'A Windows hálózati (Samba) biztosítja a szükséges ragasztót, hogy együttműködjön a Windows rendszerekben. Az alkalmazás hitelesítési szolgáltatásokat, fájl- és nyomtatási, valamint a Windows tartományi vezetés.';
-$lang['samba_app_name'] = 'A Windows hálózati (Samba)';
+$lang['samba_app_description'] = 'A Windows Hálózat (Samba) biztosítja a szükséges ragasztót a Windows rendszerekkel való együttműködéshez. Az alkalmazás hitelesítési szolgáltatásokat, fájlt és nyomtatást biztosít a Windows tartománykezeléssel együtt.';
+$lang['samba_app_name'] = 'Windows hálózat (Samba)';
 $lang['samba_computers'] = 'Számítógépek';
-$lang['samba_driver_has_not_been_initialized'] = 'Illesztőprogram nincs inicializálva.';
-$lang['samba_file_share_tooltip'] = 'Ha szeretne létrehozni hálózati fájlmegosztásokon, kérjük, használja a Flexshare alkalmazást.';
-$lang['samba_initializing_core_system'] = 'Inicializálása központi rendszer, kérjük, legyen türelemmel.';
-$lang['samba_initializing_warning'] = 'Inicializálás eltarthat egy-két percig, kérjük, legyen türelemmel.';
-$lang['samba_master_node_needs_initialization'] = 'Főcsomóponton inicializálni kell, mielőtt belevágna.';
+$lang['samba_driver_has_not_been_initialized'] = 'A sofőr nem lett inicializálva.';
+$lang['samba_file_share_tooltip'] = 'Ha hálózati fájlmegosztásokat szeretne létrehozni, kérjük, használja a Flexshare alkalmazást.';
+$lang['samba_initializing_core_system'] = 'A magrendszer inicializálása, légy türelemmel.';
+$lang['samba_initializing_warning'] = 'Az inicializálás akár egy-két percet is igénybe vehet, kérjük, légy türelmes.';
+$lang['samba_master_node_needs_initialization'] = 'A mester csomópontot meg kell kezdeni a folytatás előtt.';
 $lang['samba_mode'] = 'Mód';
-$lang['samba_samba_directory_installed_warning'] = 'A Samba Directory szoftvert telepítve, így nem kell ezt az alkalmazást.';
+$lang['samba_samba_directory_installed_warning'] = 'A Samba Directory szoftver telepítve van, így már nincs szüksége erre az alkalmazásra.';
