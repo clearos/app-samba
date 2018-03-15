5ee3091fecb85c42ee02dd71c5df5222fe819199
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:44:53 +0000

    Add new files and directories, commit changes, push changes

--- language/mk/samba_lang.php
+++ language/mk/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'администратор на корисничка сметка';
-$lang['samba_app_description'] = 'Windows вмрежување (Самба) обезбедува потребниот лепак за интероперабилност со Windows системите. Во стан обезбедува автентикација услуги, датотека и печати, заедно со управување на Windows домен.';
-$lang['samba_app_name'] = 'Windows вмрежување (Самба)';
-$lang['samba_computers'] = 'компјутери';
+$lang['samba_administrator_account'] = 'Администраторска сметка';
+$lang['samba_app_description'] = 'Windows Networking (Samba) го обезбедува потребното лепило за интероперабилност со Windows системите. Апликацијата обезбедува услуги за автентикација, датотека и печатење, заедно со управувањето со доменот на Windows.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_computers'] = 'Компјутери';
 $lang['samba_driver_has_not_been_initialized'] = 'Возачот не е иницијализиран.';
-$lang['samba_file_share_tooltip'] = 'Ако би сакале да се создаде мрежа датотека акции, Ве молиме да ја користите апликацијата за Flexshare.';
-$lang['samba_initializing_core_system'] = 'Иницијализирање јадрото на системот, Ве молиме да бидете трпеливи.';
-$lang['samba_initializing_warning'] = 'Иницијализација може да потрае една минута или две, ве молиме да бидете трпеливи.';
-$lang['samba_master_node_needs_initialization'] = 'Господар јазол мора да се иницијализира пред да продолжите.';
-$lang['samba_mode'] = 'режим';
-$lang['samba_samba_directory_installed_warning'] = 'софтвер Самба Директориум е инсталиран, така што веќе не треба оваа апликација.';
+$lang['samba_file_share_tooltip'] = 'Ако сакате да креирате мрежни датотеки, ве молиме користете ја апликацијата Flexshare.';
+$lang['samba_initializing_core_system'] = 'Иницијализирање на јадрото на системот, те молам бидете трпеливи.';
+$lang['samba_initializing_warning'] = 'Иницијализацијата може да потрае минута или две, те молам бидете трпеливи.';
+$lang['samba_master_node_needs_initialization'] = 'Главен јазол треба да се иницијализира пред да се продолжи.';
+$lang['samba_mode'] = 'Мод';
+$lang['samba_samba_directory_installed_warning'] = 'Софтверот Samba Directory е инсталиран, така што веќе не ви треба оваа апликација.';
