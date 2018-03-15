d8e18807d9f9cb1af757afa5a7ab53b6df7e5f71
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:58:43 +0000

    Add new files and directories, commit changes, push changes

--- language/es/samba_lang.php
+++ language/es/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'cuenta de administrador';
-$lang['samba_app_description'] = 'Red de Windows (Samba) proporciona el pegamento necesaria para interoperar con sistemas Windows. La aplicación proporciona servicios de autenticación, archivos e impresión, junto con la gestión de dominio de Windows.';
-$lang['samba_app_name'] = 'Red de Windows (Samba)';
+$lang['samba_administrator_account'] = 'Cuenta de administrador';
+$lang['samba_app_description'] = 'Windows Networking (Samba) proporciona el pegamento necesario para interoperar con los sistemas de Windows. La aplicación proporciona servicios de autenticación, archivo e impresión, junto con la administración de dominio de Windows.';
+$lang['samba_app_name'] = 'Redes de Windows (Samba)';
 $lang['samba_computers'] = 'Ordenadores';
 $lang['samba_driver_has_not_been_initialized'] = 'El controlador no se ha inicializado.';
-$lang['samba_file_share_tooltip'] = 'Si desea crear recursos compartidos de archivos de red, por favor utilice la aplicación FlexShare.';
-$lang['samba_initializing_core_system'] = 'La inicialización de sistema central, por favor sea paciente.';
-$lang['samba_initializing_warning'] = 'La inicialización puede tardar uno o dos minutos, por favor sea paciente.';
-$lang['samba_master_node_needs_initialization'] = 'nodo maestro tiene que ser inicializado antes de proceder.';
+$lang['samba_file_share_tooltip'] = 'Si desea crear archivos compartidos de red, utilice la aplicación Flexshare.';
+$lang['samba_initializing_core_system'] = 'Inicializando el sistema central, por favor sea paciente.';
+$lang['samba_initializing_warning'] = 'La inicialización puede tomar uno o dos minutos, por favor sea paciente.';
+$lang['samba_master_node_needs_initialization'] = 'El nodo maestro debe inicializarse antes de continuar.';
 $lang['samba_mode'] = 'Modo';
-$lang['samba_samba_directory_installed_warning'] = 'El software de Directory Samba está instalado, por lo que ya no necesita esta aplicación.';
+$lang['samba_samba_directory_installed_warning'] = 'El software Samba Directory está instalado, por lo que ya no necesita esta aplicación.';
