aeb1dc8fa24dcb3372c1f3f94696917b481caec8
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:26:05 +0000

    Add new files and directories, commit changes, push changes

--- language/eo/samba_lang.php
+++ language/eo/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'administranto Konto';
-$lang['samba_app_description'] = 'Fenestroj Interkonektanta (Sambo) provizas la necesan gluo por interoperar kun Windows sistemoj. La app provizas identigan servoj, arkivo kaj presaĵo, kune kun Vindozo havaĵo demarŝo.';
-$lang['samba_app_name'] = 'Fenestroj Interkonektanta (Samba)';
-$lang['samba_computers'] = 'komputiloj';
-$lang['samba_driver_has_not_been_initialized'] = 'Ŝoforo ne estis inicializado.';
-$lang['samba_file_share_tooltip'] = 'Se vi ŝatus krei reton dosiero akcioj, bonvolu uzi la Flexshare app.';
-$lang['samba_initializing_core_system'] = 'Inicializar kerna sistemo, bonvolu esti pacienca.';
-$lang['samba_initializing_warning'] = 'Inicialización povas preni momenton aŭ du, bonvolu esti pacienca.';
-$lang['samba_master_node_needs_initialization'] = 'Mastro nodo devas esti inicializado antaŭ ol vi antaŭeniru.';
-$lang['samba_mode'] = 'mode';
-$lang['samba_samba_directory_installed_warning'] = 'La Samba Gvidlibro programaro estas instalita, do vi ne bezonas tiun programon.';
+$lang['samba_administrator_account'] = 'Administra Konto';
+$lang['samba_app_description'] = 'Windows Networking (Samba) provizas la necesan gluon por interkonekti kun sistemoj de Windows. La programo provizas aŭtentikajn servojn, dosierojn kaj presitajn, kune kun administrado de regado de Windows.';
+$lang['samba_app_name'] = 'Reto de Windows (Samba)';
+$lang['samba_computers'] = 'Komputiloj';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver ne estis komence.';
+$lang['samba_file_share_tooltip'] = 'Se vi volas krei retajn dosierojn, bonvolu uzi la Flexshare-aplikaĵon.';
+$lang['samba_initializing_core_system'] = 'Komencante la kernan sistemon, bonvolu pacienci.';
+$lang['samba_initializing_warning'] = 'Komencaĵo povas preni minuton aŭ du, bonvolu pacienci.';
+$lang['samba_master_node_needs_initialization'] = 'Majstro-nodo devas esti komence antaŭ ol iri.';
+$lang['samba_mode'] = 'Modo';
+$lang['samba_samba_directory_installed_warning'] = 'La programaro de Samba Dosierujo estas instalita, do vi ne plu bezonas ĉi tiun programon.';
