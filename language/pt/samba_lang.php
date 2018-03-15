5080afb40c3cf270d162d9c10e1012cd16ddd4af
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:52:39 +0000

    Add new files and directories, commit changes, push changes

--- language/pt/samba_lang.php
+++ language/pt/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Conta de administrador';
-$lang['samba_app_description'] = 'Rede Windows (Samba) fornece a cola necessária para interoperar com sistemas Windows. O aplicativo fornece serviços de autenticação, arquivo e impressão, juntamente com a gestão de domínio do Windows.';
-$lang['samba_app_name'] = 'Rede Windows (Samba)';
+$lang['samba_administrator_account'] = 'Conta de Administrador';
+$lang['samba_app_description'] = 'O Windows Networking (Samba) fornece a cola necessária para interoperar com os sistemas Windows. O aplicativo fornece serviços de autenticação, arquivo e impressão, juntamente com o gerenciamento de domínio do Windows.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
 $lang['samba_computers'] = 'Computadores';
-$lang['samba_driver_has_not_been_initialized'] = 'Controlador não foi inicializado.';
-$lang['samba_file_share_tooltip'] = 'Se você gostaria de criar compartilhamentos de arquivos de rede, por favor use o aplicativo FlexShare.';
-$lang['samba_initializing_core_system'] = 'Inicializar núcleo do sistema, por favor, seja paciente.';
-$lang['samba_initializing_warning'] = 'Inicialização pode demorar um minuto ou dois, por favor, seja paciente.';
-$lang['samba_master_node_needs_initialization'] = 'nó mestre precisa ser inicializado antes de prosseguir.';
+$lang['samba_driver_has_not_been_initialized'] = 'O driver não foi inicializado.';
+$lang['samba_file_share_tooltip'] = 'Se você quiser criar compartilhamentos de arquivos de rede, use o aplicativo Flexshare.';
+$lang['samba_initializing_core_system'] = 'Inicializando o sistema central, seja paciente.';
+$lang['samba_initializing_warning'] = 'A inicialização pode levar um minuto ou dois, seja paciente.';
+$lang['samba_master_node_needs_initialization'] = 'O nó mestre precisa ser inicializado antes de prosseguir.';
 $lang['samba_mode'] = 'Modo';
-$lang['samba_samba_directory_installed_warning'] = 'O software Diretório Samba é instalado, assim você não precisa deste aplicativo.';
+$lang['samba_samba_directory_installed_warning'] = 'O software Samba Directory está instalado, então você não precisa mais desse aplicativo.';
