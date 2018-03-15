eb889bf4ac45a3b33274f0abaa78d59a3c7bdd47
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:00:02 +0000

    Add new files and directories, commit changes, push changes

--- language/sw/samba_lang.php
+++ language/sw/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Akaunti msimamizi';
-$lang['samba_app_description'] = 'Windows Networking (Samba) hutoa gundi muhimu interoperate na mifumo Windows. programu hutoa huduma ya kujitambulisha, faili na magazeti, pamoja na usimamizi wa Windows domain.';
+$lang['samba_administrator_account'] = 'Akaunti ya Msimamizi';
+$lang['samba_app_description'] = 'Windows Networking (Samba) hutoa gundi muhimu kuingiliana na mifumo ya Windows. Programu hutoa huduma za kuthibitisha, faili na kuchapisha, pamoja na usimamizi wa kikoa cha Windows.';
 $lang['samba_app_name'] = 'Windows Networking (Samba)';
 $lang['samba_computers'] = 'Kompyuta';
-$lang['samba_driver_has_not_been_initialized'] = 'Dereva haijawahi initialized.';
-$lang['samba_file_share_tooltip'] = 'Kama ungependa kujenga hisa mtandao file, tafadhali kutumia programu Flexshare.';
-$lang['samba_initializing_core_system'] = 'Initializing mfumo wa msingi, tafadhali kuwa na subira.';
-$lang['samba_initializing_warning'] = 'Initialization inaweza kuchukua dakika moja au mbili, tafadhali kuwa na subira.';
-$lang['samba_master_node_needs_initialization'] = 'Mwalimu node inahitaji kuanzishwa kabla ya kuendelea.';
-$lang['samba_mode'] = 'mode';
-$lang['samba_samba_directory_installed_warning'] = 'programu Samba Saraka imewekwa, hivyo hakuna tena haja ya programu hii.';
+$lang['samba_driver_has_not_been_initialized'] = 'Dereva haijaanzishwa.';
+$lang['samba_file_share_tooltip'] = 'Ikiwa ungependa kuunda hisa za faili za mtandao, tafadhali tumia programu ya Flexshare.';
+$lang['samba_initializing_core_system'] = 'Kuanzisha mfumo wa msingi, tafadhali subira.';
+$lang['samba_initializing_warning'] = 'Uzinduzi unaweza kuchukua dakika moja au mbili, tafadhali subira.';
+$lang['samba_master_node_needs_initialization'] = 'Node ya Mwalimu inapaswa kuanzishwa kabla ya kuendelea.';
+$lang['samba_mode'] = 'Njia';
+$lang['samba_samba_directory_installed_warning'] = 'Programu ya Directory ya Samba imewekwa, kwa hiyo huhitaji tena programu hii.';
