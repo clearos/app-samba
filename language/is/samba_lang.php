e32243454aed6e488e233d371142e0dda9af06a4
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:35:40 +0000

    Add new files and directories, commit changes, push changes

--- language/is/samba_lang.php
+++ language/is/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'stjórnandi reikningur';
-$lang['samba_app_description'] = 'Windows Networking (Samba) veitir nauðsynlegar límið um samrekstur við Windows kerfi. The app veitir staðfesting þjónustu, skrá og prenta, ásamt Windows ríki stjórnun.';
+$lang['samba_administrator_account'] = 'Stjórnandi reikningur';
+$lang['samba_app_description'] = 'Windows Networking (Samba) veitir nauðsynlega lím til að eiga samvinnu við Windows kerfi. The app veitir staðfesting þjónustu, skrá og prenta, ásamt Windows lén stjórnun.';
 $lang['samba_app_name'] = 'Windows Networking (Samba)';
 $lang['samba_computers'] = 'Tölvur';
-$lang['samba_driver_has_not_been_initialized'] = 'Ökumaður hefur ekki verið forsniðin.';
-$lang['samba_file_share_tooltip'] = 'Ef þú vildi eins og til að búa til net skrá hlutabréf, vinsamlegast notaðu Flexshare app.';
-$lang['samba_initializing_core_system'] = 'Frumstilla algerlega kerfi, vinsamlegast sýndu þolinmæði.';
-$lang['samba_initializing_warning'] = 'Frumstilling getur tekið eina mínútu eða tvær, vinsamlegast sýndu þolinmæði.';
-$lang['samba_master_node_needs_initialization'] = 'Master hnút þarf að frumstilla áður en farið er.';
-$lang['samba_mode'] = 'Mode';
-$lang['samba_samba_directory_installed_warning'] = 'Samba Directory hugbúnaður er uppsettur, þannig að þú þarft ekki lengur á þetta app.';
+$lang['samba_driver_has_not_been_initialized'] = 'Ökumaður hefur ekki verið settur í gang.';
+$lang['samba_file_share_tooltip'] = 'Ef þú vilt búa til netskráarhluta skaltu nota Flexshare app.';
+$lang['samba_initializing_core_system'] = 'Upphaf kjarnakerfis, vinsamlegast vertu þolinmóð.';
+$lang['samba_initializing_warning'] = 'Upphafsdagur getur tekið eina mínútu eða tvær, vinsamlegast vertu þolinmóður.';
+$lang['samba_master_node_needs_initialization'] = 'Stöðva þarf höfuðknúið áður en það fer fram.';
+$lang['samba_mode'] = 'Ham';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directory hugbúnaðurinn er uppsettur, svo þú þarft ekki lengur þessa app.';
