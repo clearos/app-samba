868a7d5362442411569a5e202d7f83f622615f34
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:14:55 +0000

    Add new files and directories, commit changes, push changes

--- language/af/samba_lang.php
+++ language/af/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'administrateur rekening';
-$lang['samba_app_description'] = 'Windows Netwerk (Samba) verskaf die nodige gom om versoenbaar met Windows-stelsels. Die app bied verifikasie dienste, lêer en druk, saam met Windows domein beheer.';
+$lang['samba_administrator_account'] = 'Administrateur rekening';
+$lang['samba_app_description'] = 'Windows Networking (Samba) verskaf die nodige gom om met Windows-stelsels saam te werk. Die program bied verifikasie dienste, lêer en druk, saam met Windows domein bestuur.';
 $lang['samba_app_name'] = 'Windows Netwerk (Samba)';
 $lang['samba_computers'] = 'rekenaars';
-$lang['samba_driver_has_not_been_initialized'] = 'Bestuurder is nie geïnisialiseer.';
-$lang['samba_file_share_tooltip'] = 'As jy wil graag &#39;n netwerk lêer aandele te skep, gebruik asseblief die Flexshare inligting.';
-$lang['samba_initializing_core_system'] = 'Inisialiseer kern stelsel, asseblief wees geduldig.';
-$lang['samba_initializing_warning'] = 'Inisialisering kan &#39;n minuut of twee neem, asseblief wees geduldig.';
-$lang['samba_master_node_needs_initialization'] = 'Meester knoop moet geïnisialiseer voordat jy voortgaan.';
+$lang['samba_driver_has_not_been_initialized'] = 'Bestuurder is nie geïnitialiseer nie.';
+$lang['samba_file_share_tooltip'] = 'As jy netwerk lêer aandele wil skep, gebruik asseblief die Flexshare-program.';
+$lang['samba_initializing_core_system'] = 'Aanvanklike kernstelsel, wees asseblief geduldig.';
+$lang['samba_initializing_warning'] = 'Initialisering kan &#39;n minuut of twee neem, wees asseblief geduldig.';
+$lang['samba_master_node_needs_initialization'] = 'Meesternodus moet geïnitialiseer word voordat u verder gaan.';
 $lang['samba_mode'] = 'Wyse';
-$lang['samba_samba_directory_installed_warning'] = 'Die Samba Gids sagteware geïnstalleer is, sodat jy nie meer hierdie inligting nodig het.';
+$lang['samba_samba_directory_installed_warning'] = 'Die Samba Directory sagteware is geïnstalleer, dus jy het nie meer hierdie program nodig nie.';
