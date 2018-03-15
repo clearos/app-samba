081c686ce3cd58d56eddc8c5ab4a52076a26fd51
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:25:14 +0000

    Add new files and directories, commit changes, push changes

--- language/da/samba_lang.php
+++ language/da/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'administrator konto';
-$lang['samba_app_description'] = 'Windows Networking (Samba) giver den nødvendige lim til at fungere sammen med Windows-systemer. Den app giver autentificeringstjenester, fil og print, sammen med Windows-domæne management.';
+$lang['samba_administrator_account'] = 'Administratorkonto';
+$lang['samba_app_description'] = 'Windows Networking (Samba) giver den nødvendige lim til at interoperere med Windows-systemer. App&#39;en leverer autentificeringstjenester, fil og print sammen med Windows domæneadministration.';
 $lang['samba_app_name'] = 'Windows Networking (Samba)';
 $lang['samba_computers'] = 'Computere';
-$lang['samba_driver_has_not_been_initialized'] = 'Driveren er ikke blevet initialiseret.';
-$lang['samba_file_share_tooltip'] = 'Hvis du vil oprette netværk filshares, så benyt Flexshare app.';
-$lang['samba_initializing_core_system'] = 'Initialiserer core system, vær tålmodig.';
-$lang['samba_initializing_warning'] = 'Initialisering kan tage et minut eller to, vær tålmodig.';
-$lang['samba_master_node_needs_initialization'] = 'Master node skal initialiseres før du fortsætter.';
+$lang['samba_driver_has_not_been_initialized'] = 'Driveren er ikke initialiseret.';
+$lang['samba_file_share_tooltip'] = 'Hvis du vil oprette netværksfilaktier, skal du bruge Flexshare-appen.';
+$lang['samba_initializing_core_system'] = 'Initialiserende kernesystem, vær så tålmodig.';
+$lang['samba_initializing_warning'] = 'Initialisering kan tage et øjeblik eller to, vær så tålmodig.';
+$lang['samba_master_node_needs_initialization'] = 'Master node skal initialiseres, inden du fortsætter.';
 $lang['samba_mode'] = 'Mode';
-$lang['samba_samba_directory_installed_warning'] = 'Samba Register softwaren er installeret, så du ikke længere har brug for denne app.';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directory-softwaren er installeret, så du behøver ikke længere denne app.';
