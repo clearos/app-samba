edac44e3998bd4b50d468fddd30889049738a0a5
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:53:31 +0000

    Add new files and directories, commit changes, push changes

--- language/ro/samba_lang.php
+++ language/ro/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Contul de administrator';
-$lang['samba_app_description'] = 'Ferestre din rețea (Samba) oferă adezivul necesar pentru interoperare cu sistemele de operare Windows. Aplicația furnizează servicii de autentificare, fișiere și imprimare, împreună cu managementul de domeniu Windows.';
-$lang['samba_app_name'] = 'Ferestre din rețea (Samba)';
-$lang['samba_computers'] = 'computere';
-$lang['samba_driver_has_not_been_initialized'] = 'Conducător auto nu a fost inițializat.';
-$lang['samba_file_share_tooltip'] = 'În cazul în care doriți să creați acțiuni de fișiere de rețea, vă rugăm să folosiți aplicația Flexshare.';
-$lang['samba_initializing_core_system'] = 'core system inițializarea, vă rugăm să aveți răbdare.';
+$lang['samba_administrator_account'] = 'Cont Administrator';
+$lang['samba_app_description'] = 'Windows Networking (Samba) furnizează adezivul necesar pentru a interopera cu sistemele Windows. Aplicația oferă servicii de autentificare, fișiere și tipărire, împreună cu gestionarea domeniului Windows.';
+$lang['samba_app_name'] = 'Rețeaua Windows (Samba)';
+$lang['samba_computers'] = 'calculatoare';
+$lang['samba_driver_has_not_been_initialized'] = 'Driverul nu a fost inițializat.';
+$lang['samba_file_share_tooltip'] = 'Dacă doriți să creați acțiuni de fișiere de rețea, utilizați aplicația Flexshare.';
+$lang['samba_initializing_core_system'] = 'Inițializarea sistemului de bază, vă rugăm să aveți răbdare.';
 $lang['samba_initializing_warning'] = 'Inițializarea poate dura un minut sau două, vă rugăm să aveți răbdare.';
-$lang['samba_master_node_needs_initialization'] = 'nod principal trebuie să fie inițializate înainte de a continua.';
+$lang['samba_master_node_needs_initialization'] = 'Primul nod trebuie inițializat înainte de a continua.';
 $lang['samba_mode'] = 'mod';
-$lang['samba_samba_directory_installed_warning'] = 'Software-ul Samba Director este instalat, astfel încât nu mai este nevoie de această aplicație.';
+$lang['samba_samba_directory_installed_warning'] = 'Software-ul Samba Directory este instalat, deci nu mai aveți nevoie de această aplicație.';
