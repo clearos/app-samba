7d7c3ac9de77df19431a437e143b5346212bfbcc
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:53:57 +0000

    Add new files and directories, commit changes, push changes

--- language/ru/samba_lang.php
+++ language/ru/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'Учетная запись администратора';
-$lang['samba_app_description'] = 'Сетей Windows (Samba) обеспечивает необходимый клей для взаимодействия с системами Windows. Приложение предоставляет услуги аутентификации, файлов и печати, наряду с управлением домена Windows.';
-$lang['samba_app_name'] = 'Сетей Windows (Samba)';
+$lang['samba_app_description'] = 'Windows Networking (Samba) обеспечивает необходимый клей для взаимодействия с системами Windows. Приложение предоставляет службы аутентификации, файл и печать вместе с управлением доменами Windows.';
+$lang['samba_app_name'] = 'Windows Networking (Samba)';
 $lang['samba_computers'] = 'компьютеры';
-$lang['samba_driver_has_not_been_initialized'] = 'Водитель не был инициализирован.';
-$lang['samba_file_share_tooltip'] = 'Если вы хотели бы создать акции сети файлов, необходимо использовать приложение FlexShare.';
-$lang['samba_initializing_core_system'] = 'Инициализация ядра системы, пожалуйста, будьте терпеливы.';
+$lang['samba_driver_has_not_been_initialized'] = 'Драйвер не был инициализирован.';
+$lang['samba_file_share_tooltip'] = 'Если вы хотите создать общие сетевые файлы, используйте приложение Flexshare.';
+$lang['samba_initializing_core_system'] = 'Инициализируя основную систему, будьте терпеливы.';
 $lang['samba_initializing_warning'] = 'Инициализация может занять минуту или две, пожалуйста, будьте терпеливы.';
-$lang['samba_master_node_needs_initialization'] = 'Мастер узел должен быть инициализирован, прежде чем продолжить.';
+$lang['samba_master_node_needs_initialization'] = 'Прежде чем продолжить, мастер-узел должен быть инициализирован.';
 $lang['samba_mode'] = 'Режим';
-$lang['samba_samba_directory_installed_warning'] = 'Программное обеспечение Samba каталог устанавливается, так что вам больше не нужно это приложение.';
+$lang['samba_samba_directory_installed_warning'] = 'Программное обеспечение Samba Directory установлено, поэтому вам больше не нужно это приложение.';
