9cc36b31b4943be2f856f1c5cd7929a91a1e23cf
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:27:50 +0000

    Add new files and directories, commit changes, push changes

--- language/fr/samba_lang.php
+++ language/fr/samba_lang.php
@@ -2,12 +2,12 @@
 
 $lang['samba_administrator_account'] = 'Compte administrateur';
 $lang['samba_app_description'] = 'Windows Networking (Samba) fournit la colle nécessaire pour interopérer avec les systèmes Windows. L&#39;application fournit des services d&#39;authentification, de fichiers et d&#39;impression, ainsi que la gestion de domaine Windows.';
-$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_app_name'] = 'Réseau Windows (Samba)';
 $lang['samba_computers'] = 'Des ordinateurs';
-$lang['samba_driver_has_not_been_initialized'] = 'Pilote n&#39;a pas été initialisé.';
-$lang['samba_file_share_tooltip'] = 'Si vous souhaitez créer des partages de fichiers réseau, s&#39;il vous plaît utiliser l&#39;application FlexShare.';
-$lang['samba_initializing_core_system'] = 'Initialisation système de base, s&#39;il vous plaît être patient.';
-$lang['samba_initializing_warning'] = 'Initialisation peut prendre une minute ou deux, s&#39;il vous plaît soyez patient.';
-$lang['samba_master_node_needs_initialization'] = 'nœud maître doit être initialisé avant de poursuivre.';
+$lang['samba_driver_has_not_been_initialized'] = 'Le pilote n&#39;a pas été initialisé.';
+$lang['samba_file_share_tooltip'] = 'Si vous souhaitez créer des partages de fichiers réseau, utilisez l&#39;application FlexShare.';
+$lang['samba_initializing_core_system'] = 'Initialisation du système de base, s&#39;il vous plaît soyez patient.';
+$lang['samba_initializing_warning'] = 'L&#39;initialisation peut prendre une minute ou deux, soyez patient.';
+$lang['samba_master_node_needs_initialization'] = 'Le noeud maître doit être initialisé avant de continuer.';
 $lang['samba_mode'] = 'Mode';
-$lang['samba_samba_directory_installed_warning'] = 'Le logiciel Directory Samba est installé, de sorte que vous ne devez plus cette application.';
+$lang['samba_samba_directory_installed_warning'] = 'Le logiciel Samba Directory est installé, donc vous n&#39;avez plus besoin de cette application.';
