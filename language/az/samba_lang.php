a67d5a72dce687c55eac8652e8be57735da87920
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:17:33 +0000

    Add new files and directories, commit changes, push changes

--- language/az/samba_lang.php
+++ language/az/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Administrator hesab';
-$lang['samba_app_description'] = 'Windows Networking (Samba) Windows sistemi ilə çalışacak üçün zəruri yapışqan edir. app Windows domain rəhbərliyi ilə yanaşı, identifikasiyası xidmətləri, fayl və çap edir.';
-$lang['samba_app_name'] = 'Windows Networking (Samba)';
-$lang['samba_computers'] = 'Computers';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver başlatılmadı deyil.';
-$lang['samba_file_share_tooltip'] = 'Siz şəbəkə fayl səhmlərin yaratmaq istəyirsinizsə, Flexshare app istifadə edin.';
-$lang['samba_initializing_core_system'] = 'core sistemi başlatılıyor, səbr edin.';
-$lang['samba_initializing_warning'] = 'Başlatma bir dəqiqə və ya iki bilər, səbr edin.';
-$lang['samba_master_node_needs_initialization'] = 'Master node davam əvvəl başlatılmış lazımdır.';
-$lang['samba_mode'] = 'Mode';
-$lang['samba_samba_directory_installed_warning'] = 'Siz artıq bu app lazımdır, belə ki, Samba Directory software, yüklü.';
+$lang['samba_administrator_account'] = 'Administrator hesabı';
+$lang['samba_app_description'] = 'Windows Networking (Samba), Windows sistemləri ilə qarşılıqlı əlaqəli yapışqan təmin edir. Bu proqramlar, Windows domeninin idarə olunması ilə yanaşı, identifikasiya xidmətləri, fayl və çap təmin edir.';
+$lang['samba_app_name'] = 'Windows şəbəkəsi (Samba)';
+$lang['samba_computers'] = 'Kompüterlər';
+$lang['samba_driver_has_not_been_initialized'] = 'Sürücü başlanmadı.';
+$lang['samba_file_share_tooltip'] = 'Şəbəkə faylının paylarını yaratmaq istəyirsinizsə, Flexshare tətbiqini istifadə edin.';
+$lang['samba_initializing_core_system'] = 'Core sistemin başlanğıcında, səbr edin.';
+$lang['samba_initializing_warning'] = 'Initializasiya bir-iki dəqiqə çəkə bilər, xahiş edirəm səbr edin.';
+$lang['samba_master_node_needs_initialization'] = 'Davam etmədən əvvəl master nodeun başlanılması lazımdır.';
+$lang['samba_mode'] = 'Modu';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directory proqramı yüklənir, buna görə artıq bu tətbiqə ehtiyacınız yoxdur.';
