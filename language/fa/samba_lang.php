8156369733d0e6b69f940b83266589fbb0e44b13
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:51:21 +0000

    Add new files and directories, commit changes, push changes

--- language/fa/samba_lang.php
+++ language/fa/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'حساب مدیر';
-$lang['samba_app_description'] = 'ویندوز شبکه (سامبا) چسب لازم برای همکاری با سیستم های ویندوز فراهم می کند. این نرم افزار فراهم خدمات احراز هویت، فایل و چاپ، همراه با مدیریت دامنه ویندوز.';
-$lang['samba_app_name'] = 'ویندوز شبکه (سامبا)';
+$lang['samba_administrator_account'] = 'حساب کاربری مدیر';
+$lang['samba_app_description'] = 'شبکه ویندوز (Samba) چسب لازم برای همکاری با سیستم های ویندوز فراهم می کند. این برنامه خدمات احراز هویت، فایل و چاپ را همراه با مدیریت دامنه ویندوز فراهم می کند.';
+$lang['samba_app_name'] = 'شبکه ویندوز (سامبا)';
 $lang['samba_computers'] = 'کامپیوترها';
-$lang['samba_driver_has_not_been_initialized'] = 'درایور مقداردهی اولیه نشده است.';
-$lang['samba_file_share_tooltip'] = 'اگر شما می خواهم برای ایجاد سهام فایل شبکه، لطفا با استفاده از برنامه Flexshare.';
-$lang['samba_initializing_core_system'] = 'مقدار دهی اولیه هسته سیستم، لطفا صبور باشید.';
-$lang['samba_initializing_warning'] = 'مقدار دهی اولیه ممکن است یک یا دو دقیقه طول بکشد، لطفا صبور باشید.';
-$lang['samba_master_node_needs_initialization'] = 'استاد گره نیاز به قبل از اقدام مقداردهی اولیه می شود.';
+$lang['samba_driver_has_not_been_initialized'] = 'درایور هنوز مقداردهی نشده است.';
+$lang['samba_file_share_tooltip'] = 'اگر می خواهید اشتراک فایل های شبکه ایجاد کنید، لطفا از برنامه Flexshare استفاده کنید.';
+$lang['samba_initializing_core_system'] = 'ابتدا سیستم هسته ای، لطفا صبور باشید';
+$lang['samba_initializing_warning'] = 'ابتدا ممکن است یک یا دو دقیقه طول بکشد، لطفا صبور باشید';
+$lang['samba_master_node_needs_initialization'] = 'گره متناظر باید قبل از ادامه مقداردهی شود.';
 $lang['samba_mode'] = 'حالت';
-$lang['samba_samba_directory_installed_warning'] = 'این نرم افزار سامبا راهنمای نصب شده است، بنابراین شما دیگر نیاز به این برنامه.';
+$lang['samba_samba_directory_installed_warning'] = 'نرم افزار دایرکتوری Samba نصب شده است، بنابراین شما دیگر نیازی به این برنامه ندارید.';
