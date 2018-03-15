d4868414487dcfff042ee54b9b2f94cba93dee92
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:31:14 +0000

    Add new files and directories, commit changes, push changes

--- language/ht/samba_lang.php
+++ language/ht/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'administratè Kont';
-$lang['samba_app_description'] = 'Windows Rezo (Samba) bay lakòl ki nesesè yo enterajis ki gen yon sistèm fenèt yo. App a bay sèvis otantifikasyon, dosye ak ekri an lèt detache, ansanm ak jesyon domèn fenèt yo.';
+$lang['samba_administrator_account'] = 'Kont Administratè';
+$lang['samba_app_description'] = 'Fenèt Networking (Samba) bay lakòl ki nesesè pou interoperate ak Windows sistèm yo. App a bay sèvis otantifikasyon, ranpli ak enprime, ansanm ak jesyon domèn Windows.';
 $lang['samba_app_name'] = 'Windows Rezo (Samba)';
 $lang['samba_computers'] = 'Odinatè';
-$lang['samba_driver_has_not_been_initialized'] = 'Chofè pa te inisyalizèd.';
+$lang['samba_driver_has_not_been_initialized'] = 'Chofè pa te inisyalize.';
 $lang['samba_file_share_tooltip'] = 'Si ou ta renmen yo kreye aksyon dosye rezo, tanpri itilize app a Flexshare.';
-$lang['samba_initializing_core_system'] = 'Inisyalizin sistèm nwayo, tanpri pran pasyans.';
-$lang['samba_initializing_warning'] = 'Inisyalizasyon ka pran yon minit oubyen de, tanpri pran pasyans.';
-$lang['samba_master_node_needs_initialization'] = 'Mèt ne bezwen yo dwe inisyalizèd anvan yo.';
+$lang['samba_initializing_core_system'] = 'Inisyalize sistèm nwayo, tanpri pran pasyans.';
+$lang['samba_initializing_warning'] = 'Inisyalizasyon ka pran yon minit oswa de, tanpri pran pasyans.';
+$lang['samba_master_node_needs_initialization'] = 'Mèt ne bezwen inisyalize anvan pwosedi.';
 $lang['samba_mode'] = 'Mode';
-$lang['samba_samba_directory_installed_warning'] = 'Se lojisyèl an Samba Anyè enstale, kidonk, ou pa bezwen app sa a.';
+$lang['samba_samba_directory_installed_warning'] = 'Se lojisyèl an Anyè Samba enstale, kidonk, ou pa bezwen sa a app.';
