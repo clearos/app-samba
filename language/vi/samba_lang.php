199f22c5f90942355f16e678beba249b8bde1044
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:08:18 +0000

    Add new files and directories, commit changes, push changes

--- language/vi/samba_lang.php
+++ language/vi/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Tài khoản quản trị';
-$lang['samba_app_description'] = 'Kết nối mạng Windows (Samba) cung cấp keo cần thiết để tương thích với các hệ thống Windows. Ứng dụng cung cấp dịch vụ chứng thực, tập tin và in, cùng với quản lý miền Windows.';
-$lang['samba_app_name'] = 'Kết nối mạng Windows (Samba)';
-$lang['samba_computers'] = 'Máy tính';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver đã không được khởi tạo.';
-$lang['samba_file_share_tooltip'] = 'Nếu bạn muốn tạo ra các file chia sẻ mạng, hãy sử dụng các ứng dụng Flexshare.';
+$lang['samba_administrator_account'] = 'Tài khoản Quản trị viên';
+$lang['samba_app_description'] = 'Windows Networking (Samba) cung cấp keo cần thiết để tương thích với các hệ thống Windows. Ứng dụng này cung cấp dịch vụ chứng thực, tệp và in, cùng với quản lý miền Windows.';
+$lang['samba_app_name'] = 'Mạng Windows (Samba)';
+$lang['samba_computers'] = 'Máy vi tính';
+$lang['samba_driver_has_not_been_initialized'] = 'Driver chưa được khởi tạo.';
+$lang['samba_file_share_tooltip'] = 'Nếu bạn muốn tạo chia sẻ tập tin mạng, hãy sử dụng ứng dụng Flexshare.';
 $lang['samba_initializing_core_system'] = 'Khởi tạo hệ thống cốt lõi, hãy kiên nhẫn.';
-$lang['samba_initializing_warning'] = 'Khởi tạo có thể mất một hoặc hai phút, hãy kiên nhẫn.';
-$lang['samba_master_node_needs_initialization'] = 'Thạc sĩ node cần phải được khởi tạo trước khi tiếp tục.';
+$lang['samba_initializing_warning'] = 'Việc khởi tạo có thể mất một vài phút, hãy kiên nhẫn.';
+$lang['samba_master_node_needs_initialization'] = 'Nút chính cần được khởi tạo trước khi tiếp tục.';
 $lang['samba_mode'] = 'Chế độ';
-$lang['samba_samba_directory_installed_warning'] = 'Các phần mềm Samba Directory được cài đặt, do đó bạn không còn cần ứng dụng này.';
+$lang['samba_samba_directory_installed_warning'] = 'Phần mềm Thư mục Samba được cài đặt, vì vậy bạn không cần ứng dụng này nữa.';
