2ea168f3f7d6e7a6ffce8b89315d4a71f961b2eb
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:47:30 +0000

    Add new files and directories, commit changes, push changes

--- language/ms/samba_lang.php
+++ language/ms/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Akaun pentadbir';
-$lang['samba_app_description'] = 'Windows Rangkaian (Samba) menyediakan gam yang diperlukan untuk saling beroperasi dengan sistem Windows. Aplikasi ini menyediakan perkhidmatan pengesahan, fail dan cetak, bersama-sama dengan Windows pengurusan domain.';
-$lang['samba_app_name'] = 'Windows Rangkaian (Samba)';
+$lang['samba_administrator_account'] = 'Akaun Pentadbir';
+$lang['samba_app_description'] = 'Windows Networking (Samba) menyediakan gam yang perlu untuk berinteraksi dengan sistem Windows. Aplikasi ini menyediakan perkhidmatan pengesahan, fail dan cetak, bersama-sama dengan pengurusan domain Windows.';
+$lang['samba_app_name'] = 'Rangkaian Windows (Samba)';
 $lang['samba_computers'] = 'Komputer';
 $lang['samba_driver_has_not_been_initialized'] = 'Pemandu belum dimulakan.';
-$lang['samba_file_share_tooltip'] = 'Jika anda ingin membuat perkongsian fail rangkaian, sila gunakan aplikasi Flexshare itu.';
+$lang['samba_file_share_tooltip'] = 'Jika anda ingin mencipta saham fail rangkaian, sila gunakan aplikasi Flexshare.';
 $lang['samba_initializing_core_system'] = 'Memulakan sistem teras, sila bersabar.';
-$lang['samba_initializing_warning'] = 'Pengawalan boleh mengambil satu atau dua minit, sila bersabar.';
-$lang['samba_master_node_needs_initialization'] = 'nod induk perlu dimulakan sebelum meneruskan.';
-$lang['samba_mode'] = 'mod';
-$lang['samba_samba_directory_installed_warning'] = 'Perisian Direktori Samba dipasang, supaya anda tidak lagi perlu app ini.';
+$lang['samba_initializing_warning'] = 'Inisialisasi mungkin mengambil masa satu atau dua minit, sila bersabar.';
+$lang['samba_master_node_needs_initialization'] = 'Nod induk perlu dimulakan sebelum meneruskan.';
+$lang['samba_mode'] = 'Mod';
+$lang['samba_samba_directory_installed_warning'] = 'Perisian Direktori Samba dipasang, jadi anda tidak lagi memerlukan aplikasi ini.';
