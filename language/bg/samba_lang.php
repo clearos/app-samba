377ccd62723be6414ab989bd6d08bba4d34b1f4e
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:20:55 +0000

    Add new files and directories, commit changes, push changes

--- language/bg/samba_lang.php
+++ language/bg/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'администраторския акаунт';
-$lang['samba_app_description'] = 'Windows Networking (Samba) осигурява необходимата лепилото да взаимодейства с Windows системи. Приложението осигурява удостоверяване услуги, файл и печат, заедно с управлението на Windows домейн.';
+$lang['samba_administrator_account'] = 'Администраторски профил';
+$lang['samba_app_description'] = 'Windows Networking (Samba) осигурява необходимото лепило за съвместна работа с системите на Windows. Приложението предоставя услуги за удостоверяване, файлове и печат, както и управлението на домейни в Windows.';
 $lang['samba_app_name'] = 'Windows Networking (Samba)';
 $lang['samba_computers'] = 'Компютри';
-$lang['samba_driver_has_not_been_initialized'] = 'Шофьор не е инициализиран.';
-$lang['samba_file_share_tooltip'] = 'Ако искате да създадете споделени файлове в мрежата, моля, използвайте приложението Flexshare.';
-$lang['samba_initializing_core_system'] = 'Инициализиране ядро ​​на системата, моля, бъдете търпеливи.';
-$lang['samba_initializing_warning'] = 'Инициализация може да отнеме минута или две, моля бъдете търпеливи.';
-$lang['samba_master_node_needs_initialization'] = 'Магистър възел трябва да се инициализира, преди да продължите.';
+$lang['samba_driver_has_not_been_initialized'] = 'Шофьорът не е инициализиран.';
+$lang['samba_file_share_tooltip'] = 'Ако искате да създадете мрежови споделени файлове, моля, използвайте приложението FlexShare.';
+$lang['samba_initializing_core_system'] = 'Инициализиране на основната система, моля, бъдете търпеливи.';
+$lang['samba_initializing_warning'] = 'Инициализацията може да отнеме минута или две, моля, бъдете търпеливи.';
+$lang['samba_master_node_needs_initialization'] = 'Главният възел трябва да бъде инициализиран преди да продължите.';
 $lang['samba_mode'] = 'вид';
-$lang['samba_samba_directory_installed_warning'] = 'Софтуерът на Samba Directory е инсталиран, така че вече няма нужда от това приложение.';
+$lang['samba_samba_directory_installed_warning'] = 'Софтуерът на Samba Directory е инсталиран, така че вече нямате нужда от това приложение.';
