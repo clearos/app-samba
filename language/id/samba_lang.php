7c5e50139452e5a21beacc8271439d16e3c24022
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:33:54 +0000

    Add new files and directories, commit changes, push changes

--- language/id/samba_lang.php
+++ language/id/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'administrator Akun';
-$lang['samba_app_description'] = 'Jendela Networking (Samba) menyediakan lem yang diperlukan untuk beroperasi dengan sistem Windows. Aplikasi ini menyediakan layanan otentikasi, file dan cetak, bersama dengan manajemen domain Windows.';
-$lang['samba_app_name'] = 'Jendela Networking (Samba)';
-$lang['samba_computers'] = 'komputer';
+$lang['samba_administrator_account'] = 'Akun Administrator';
+$lang['samba_app_description'] = 'Jaringan Windows (Samba) menyediakan lem yang diperlukan untuk beroperasi dengan sistem Windows. Aplikasi ini menyediakan layanan autentikasi, file dan cetak, bersamaan dengan pengelolaan domain Windows.';
+$lang['samba_app_name'] = 'Jaringan Windows (Samba)';
+$lang['samba_computers'] = 'Komputer';
 $lang['samba_driver_has_not_been_initialized'] = 'Driver belum diinisialisasi.';
-$lang['samba_file_share_tooltip'] = 'Jika Anda ingin membuat file saham jaringan, silakan gunakan aplikasi Flexshare.';
-$lang['samba_initializing_core_system'] = 'Inisialisasi sistem inti, harap bersabar.';
-$lang['samba_initializing_warning'] = 'Inisialisasi dapat mengambil satu atau dua menit, harap bersabar.';
-$lang['samba_master_node_needs_initialization'] = 'Node master perlu diinisialisasi sebelum melanjutkan.';
+$lang['samba_file_share_tooltip'] = 'Jika Anda ingin membuat saham file jaringan, gunakan aplikasi Flexshare.';
+$lang['samba_initializing_core_system'] = 'Inisialisasi sistem inti, mohon bersabar.';
+$lang['samba_initializing_warning'] = 'Inisialisasi mungkin memerlukan satu atau dua menit, mohon bersabar.';
+$lang['samba_master_node_needs_initialization'] = 'Master node perlu diinisialisasi sebelum melanjutkan.';
 $lang['samba_mode'] = 'Mode';
-$lang['samba_samba_directory_installed_warning'] = 'Perangkat lunak Samba Directory diinstal, sehingga Anda tidak perlu lagi aplikasi ini.';
+$lang['samba_samba_directory_installed_warning'] = 'Perangkat lunak Samba Directory terinstal, sehingga Anda tidak lagi membutuhkan aplikasi ini.';
