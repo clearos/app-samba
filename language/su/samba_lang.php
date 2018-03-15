9048becc465ed16cbf378a62b15f4562fa4e1050
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:59:09 +0000

    Add new files and directories, commit changes, push changes

--- language/su/samba_lang.php
+++ language/su/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'administrator Akun';
-$lang['samba_app_description'] = 'Windows jaringan (Samba) nyadiakeun lem perlu interoperate jeung sistem Windows. app nyadiakeun ladenan auténtikasi, file jeung print, babarengan jeung manajemén domain Windows.';
-$lang['samba_app_name'] = 'Windows jaringan (Samba)';
+$lang['samba_app_description'] = 'Windows jejaring (Samba) nyadiakeun lem perlu interoperate kalawan sistem Windows. aplikasi nu nyadiakeun ladenan auténtikasi, file na print marengan manajemén domain Windows.';
+$lang['samba_app_name'] = 'Windows jejaring (Samba)';
 $lang['samba_computers'] = 'komputer';
-$lang['samba_driver_has_not_been_initialized'] = 'Supir tacan initialized.';
-$lang['samba_file_share_tooltip'] = 'Lamun hoyong nyieun biasa file jaringan, mangga nganggo aplikasi Flexshare.';
-$lang['samba_initializing_core_system'] = 'Initializing sistim inti, mangga jadi sabar.';
-$lang['samba_initializing_warning'] = 'Initialization butuh hiji atawa dua menit, mangga jadi sabar.';
+$lang['samba_driver_has_not_been_initialized'] = 'Supir teu acan initialized.';
+$lang['samba_file_share_tooltip'] = 'Lamun hoyong jieun biasa file jaringan, mangga nganggo aplikasi Flexshare.';
+$lang['samba_initializing_core_system'] = 'Initializing Sistim core, punten janten sabar.';
+$lang['samba_initializing_warning'] = 'Initialization butuh atawa dua menit, punten janten sabar.';
 $lang['samba_master_node_needs_initialization'] = 'Master titik perlu initialized saméméh lajengkeun.';
 $lang['samba_mode'] = 'mode';
-$lang['samba_samba_directory_installed_warning'] = 'Software Samba Diréktori geus dipasang, ku kituna anjeun teu deui kudu app ieu.';
+$lang['samba_samba_directory_installed_warning'] = 'Sofwer Samba Diréktori geus dipasang, jadi Anjeun euweuh perlu aplikasi ieu.';
