5bb3b2f2f3e26fa34c802eed1d0203dae8f3b16c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:38:20 +0000

    Add new files and directories, commit changes, push changes

--- language/kk/samba_lang.php
+++ language/kk/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Әкімші шот';
-$lang['samba_app_description'] = 'Windows Networking (Samba) Windows жүйелерімен өзара қажетті желім ұсынады. Қолданба Windows домен басқару бірге, аутентификация қызметтерді, файлдар мен басып шығарғышты қамтамасыз етеді.';
-$lang['samba_app_name'] = 'Windows Networking (Samba)';
+$lang['samba_administrator_account'] = 'Әкімші тіркелгісі';
+$lang['samba_app_description'] = 'Windows Networking (Samba) Windows жүйелерімен өзара әрекеттесу үшін қажетті желімді қамтамасыз етеді. Қолданба Windows доменін басқаруымен қатар аутентификация қызметтерін, файлды және басып шығаруды қамтамасыз етеді.';
+$lang['samba_app_name'] = 'Windows желілері (Samba)';
 $lang['samba_computers'] = 'Компьютерлер';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver бапталғаннан жоқ.';
-$lang['samba_file_share_tooltip'] = 'Егер сіз желілік файл акцияларын жасау келсе, Flexshare қолданбасын пайдаланыңыз.';
-$lang['samba_initializing_core_system'] = 'негізгі жүйесін баптандыру, өтінеміз шыдамды.';
-$lang['samba_initializing_warning'] = 'Инициализации бір минут немесе екі кетуі мүмкін, өтінеміз шыдамды.';
-$lang['samba_master_node_needs_initialization'] = 'Мастер торап жалғастыру алдында баптандырылады қажет.';
-$lang['samba_mode'] = 'Mode';
-$lang['samba_samba_directory_installed_warning'] = 'Samba Directory бағдарламалық қамтамасыз ету орнатылған, сондықтан сіз енді бұл қолданбаны қажет.';
+$lang['samba_driver_has_not_been_initialized'] = 'Драйвер инициализацияланған жоқ.';
+$lang['samba_file_share_tooltip'] = 'Желілік файлдарды жасауды қаласаңыз, Flexshare бағдарламасын қолданыңыз.';
+$lang['samba_initializing_core_system'] = 'Негізгі жүйені инициализациялау үшін шыдамды болыңыз.';
+$lang['samba_initializing_warning'] = 'Инициализациялау бір-екі минутқа созылуы мүмкін, шыдамды болыңыз.';
+$lang['samba_master_node_needs_initialization'] = 'Жалғастырмас бұрын Master түйінін баптандыру керек.';
+$lang['samba_mode'] = 'Режим';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directory бағдарламалық жасақтамасы орнатылған, сондықтан сіз бұл қолданбаға қажет емессіз.';
