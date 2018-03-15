3f1d14aad3293e56146ea198769d1c408669dd34
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:37:00 +0000

    Add new files and directories, commit changes, push changes

--- language/ja/samba_lang.php
+++ language/ja/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = '管理者アカウント';
-$lang['samba_app_description'] = 'Windowsネットワーク（サンバ）は、Windowsシステムと相互運用するために必要な接着剤を提供します。 アプリは、Windowsドメインの管理とともに、認証サービス、ファイルやプリントを提供します。';
-$lang['samba_app_name'] = 'Windowsネットワーク（サンバ）';
-$lang['samba_computers'] = 'コンピューター';
+$lang['samba_app_description'] = 'Windowsネットワーク（Samba）は、Windowsシステムとの相互運用に必要な接着剤を提供します。 このアプリケーションは、Windowsドメイン管理と共に、認証サービス、ファイルおよびプリントを提供します。';
+$lang['samba_app_name'] = 'Windowsネットワーク（Samba）';
+$lang['samba_computers'] = 'コンピュータ';
 $lang['samba_driver_has_not_been_initialized'] = 'ドライバが初期化されていません。';
-$lang['samba_file_share_tooltip'] = 'あなたがネットワークファイル共有を作成したい場合は、FlexShareのアプリを使用してください。';
-$lang['samba_initializing_core_system'] = 'コアシステムの初期化、しばらくお待ちください。';
-$lang['samba_initializing_warning'] = '初期設定は1〜2分かかる場合があります、しばらくお待ちください。';
-$lang['samba_master_node_needs_initialization'] = 'マスターノードは、先に進む前に初期化する必要があります。';
+$lang['samba_file_share_tooltip'] = 'ネットワークファイル共有を作成する場合は、Flexshareアプリを使用してください。';
+$lang['samba_initializing_core_system'] = 'コアシステムを初期化してください。';
+$lang['samba_initializing_warning'] = '初期化には1〜2分かかる場合がありますので、しばらくお待ちください。';
+$lang['samba_master_node_needs_initialization'] = '先に進む前にマスターノードを初期化する必要があります。';
 $lang['samba_mode'] = 'モード';
-$lang['samba_samba_directory_installed_warning'] = 'Sambaのディレクトリソフトウェアがインストールされていないので、あなたは、もはやこのアプリを必要とされています。';
+$lang['samba_samba_directory_installed_warning'] = 'Samba Directoryソフトウェアがインストールされているため、このアプリは必要ありません。';
