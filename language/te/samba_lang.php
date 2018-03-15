6293a97790b2df5fae4d6641ae8d559586b84824
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:02:51 +0000

    Add new files and directories, commit changes, push changes

--- language/te/samba_lang.php
+++ language/te/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'అడ్మినిస్ట్రేటర్ ఖాతా';
-$lang['samba_app_description'] = 'విండోస్ నెట్వర్కింగ్ (సాంబా) Windows వ్యవస్థలు యింటరాపరేట్ అవసరం గ్లూ అందిస్తుంది. అనువర్తనం విండోస్ డొమైన్ నిర్వహణ పాటు, ధృవీకరణ సేవలను, ఫైలు మరియు ముద్రణ అందిస్తుంది.';
+$lang['samba_app_description'] = 'Windows నెట్వర్కింగ్ (Samba) విండోస్ సిస్టమ్స్తో ఇంటార్పరేట్ చేయడానికి అవసరమైన గ్లూ అందిస్తుంది. ఈ అనువర్తనం విండోస్ డొమైన్ మేనేజ్మెంట్తోపాటు, ధృవీకరణ సేవలు, ఫైలు మరియు ప్రింట్ను అందిస్తుంది.';
 $lang['samba_app_name'] = 'విండోస్ నెట్వర్కింగ్ (సాంబా)';
 $lang['samba_computers'] = 'కంప్యూటర్లు';
-$lang['samba_driver_has_not_been_initialized'] = 'డ్రైవర్ initialized లేదు.';
-$lang['samba_file_share_tooltip'] = 'మీరు నెట్వర్క్ ఫైల్ షేర్ల చెయ్యాలనుకుంటే, దయచేసి Flexshare అనువర్తనం ఉపయోగించడానికి.';
-$lang['samba_initializing_core_system'] = 'కోర్ వ్యవస్థ ప్రారంభించడం, దయచేసి వేచి ఉండండి.';
-$lang['samba_initializing_warning'] = 'మొదలుపెట్టటం ఒక నిమిషం లేదా రెండు పట్టవచ్చు, దయచేసి వేచి ఉండండి.';
-$lang['samba_master_node_needs_initialization'] = 'మాస్టర్ నోడ్ కొనసాగే ముందు initialized అవసరం.';
+$lang['samba_driver_has_not_been_initialized'] = 'డ్రైవర్ ప్రారంభించబడలేదు.';
+$lang['samba_file_share_tooltip'] = 'మీరు నెట్వర్క్ ఫైల్ షేర్లను సృష్టించాలనుకుంటే, దయచేసి Flexshare అనువర్తనాన్ని ఉపయోగించండి.';
+$lang['samba_initializing_core_system'] = 'ప్రధాన వ్యవస్థను ప్రారంభించడం, దయచేసి ఓపికగా ఉండండి.';
+$lang['samba_initializing_warning'] = 'ప్రారంభించడం ఒక నిమిషం లేదా రెండు సమయం పట్టవచ్చు, దయచేసి వేచి ఉండండి.';
+$lang['samba_master_node_needs_initialization'] = 'మాస్టర్ నోడ్ కొనసాగే ముందు ప్రారంభించాలి.';
 $lang['samba_mode'] = 'మోడ్';
-$lang['samba_samba_directory_installed_warning'] = 'కాబట్టి మీరు ఇకపై ఈ అనువర్తనం అవసరం, సాంబా డైరెక్టరీ సాఫ్ట్వేర్ ఇన్స్టాల్.';
+$lang['samba_samba_directory_installed_warning'] = 'Samba డైరెక్టరీ సాఫ్ట్వేర్ వ్యవస్థాపించబడింది, కాబట్టి మీరు ఇక ఈ అనువర్తనం అవసరం లేదు.';
