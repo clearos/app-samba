892005eb73def4a69386fcdc5361456301a7760a
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:30:23 +0000

    Add new files and directories, commit changes, push changes

--- language/gu/samba_lang.php
+++ language/gu/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'સંચાલક ખાતું';
-$lang['samba_app_description'] = 'Windows નેટવર્કીંગ (સામ્બા) Windows સિસ્ટમો સાથે આંતરપ્રક્રિયા જરૂરી ગુંદર પૂરી પાડે છે. એપ્લિકેશન વિન્ડોઝ ડોમેન મેનેજમેન્ટ સાથે સત્તાધિકરણ સેવાઓ, ફાઈલ અને પ્રિન્ટ પૂરી પાડે છે.';
-$lang['samba_app_name'] = 'Windows નેટવર્કીંગ (સામ્બા)';
+$lang['samba_administrator_account'] = 'સંચાલક એકાઉન્ટ';
+$lang['samba_app_description'] = 'વિન્ડોઝ નેટવર્કિંગ (સામ્બા) વિન્ડોઝ સિસ્ટમ્સ સાથે ઇન્ટરઓપરેટ કરવા માટે જરૂરી ગુંદર પૂરું પાડે છે. એપ્લિકેશન ડોમેન મેનેજમેન્ટ સાથે અધિકૃતતા સેવાઓ, ફાઇલ અને પ્રિન્ટ પ્રદાન કરે છે.';
+$lang['samba_app_name'] = 'વિન્ડોઝ નેટવર્કિંગ (સામ્બા)';
 $lang['samba_computers'] = 'એન્જીનિયરિંગ';
-$lang['samba_driver_has_not_been_initialized'] = 'ડ્રાઈવર આરંભ કરવામાં આવ્યો છે.';
-$lang['samba_file_share_tooltip'] = 'તમે નેટવર્ક ફાઈલ મિત્રો બનાવવા માંગો છો, તો Flexshare એપ્લિકેશન ઉપયોગ કરો.';
-$lang['samba_initializing_core_system'] = 'કોર સિસ્ટમ પ્રારંભ, કૃપા કરીને ધીરજ ધરો.';
-$lang['samba_initializing_warning'] = 'આરંભ એક અથવા બે મિનિટ લાગી શકે છે, કૃપા કરીને ધીરજ.';
-$lang['samba_master_node_needs_initialization'] = 'માસ્ટર નોડ પ્રક્રિયા કરવા પહેલાં આરંભ કરવાની જરૂર છે.';
-$lang['samba_mode'] = 'સ્થિતિ';
-$lang['samba_samba_directory_installed_warning'] = 'સામ્બા ડિરેક્ટરી સોફ્ટવેર સ્થાપિત થયેલ છે, જેથી તમે લાંબા સમય સુધી આ એપ્લિકેશન જરૂર છે.';
+$lang['samba_driver_has_not_been_initialized'] = 'ડ્રાઈવર પ્રારંભ કરવામાં આવ્યો નથી.';
+$lang['samba_file_share_tooltip'] = 'જો તમે નેટવર્ક ફાઇલ શેર્સ બનાવવા માંગો છો, તો Flexshare એપ્લિકેશનનો ઉપયોગ કરો.';
+$lang['samba_initializing_core_system'] = 'કોર સિસ્ટમ શરૂ કરી રહ્યા છીએ, કૃપા કરીને ધીરજ રાખો.';
+$lang['samba_initializing_warning'] = 'શરૂઆતમાં એક કે બે મિનિટ લાગી શકે છે, કૃપા કરીને ધીરજ રાખો.';
+$lang['samba_master_node_needs_initialization'] = 'આગળ વધતા પહેલા માસ્ટર નોડને પ્રારંભ કરવાની જરૂર છે.';
+$lang['samba_mode'] = 'મોડ';
+$lang['samba_samba_directory_installed_warning'] = 'સામ્બા નિર્દેશિકા સૉફ્ટવેર ઇન્સ્ટોલ કરેલું છે, તેથી તમને હવે આ એપ્લિકેશનની જરૂર નથી';
