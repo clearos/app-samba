da30f2098b5c580fd8b86a5d8220cb39f1fba349
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:03:19 +0000

    Add new files and directories, commit changes, push changes

--- language/tg/samba_lang.php
+++ language/tg/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'Ҳисоби Administrator';
-$lang['samba_app_description'] = 'Windows Networking (Samba) ба ширеше зарур interoperate бо системаҳои Windows таъмин менамояд. Дар барнома аз хизматрасониҳои аутентификатсия, файл ва чоп, дар якҷоягӣ бо идоракунии домени Windows.';
+$lang['samba_administrator_account'] = 'Ҳисоботи мудир';
+$lang['samba_app_description'] = 'Windows Networking (Samba) барои пайвастшавӣ бо системаҳои Windows-ро бо шираи зарурӣ таъмин мекунад. Барнома хидматҳои аслӣ, файл ва чопро дар якҷоягӣ бо идоракунии домени Windows таъмин мекунад.';
 $lang['samba_app_name'] = 'Windows Networking (Samba)';
 $lang['samba_computers'] = 'Компютерҳо';
-$lang['samba_driver_has_not_been_initialized'] = 'Ронандаи кардааст initialized нашудааст.';
-$lang['samba_file_share_tooltip'] = 'Агар шумо мехоҳед ба эҷоди файли саҳмияҳои шабака, лутфан истифода баред барнома Flexshare.';
-$lang['samba_initializing_core_system'] = 'Initializing системаи аслӣ, лутфан сабр кун.';
-$lang['samba_initializing_warning'] = 'Initialization метавонад аз як ё ду дақиқа мегирад, лутфан сабр кун.';
-$lang['samba_master_node_needs_initialization'] = 'Master гиреҳ бояд пеш аз кӯчонидани initialized шавад.';
-$lang['samba_mode'] = 'ҳолати';
-$lang['samba_samba_directory_installed_warning'] = 'Нармафзор Samba Directory гузошта шуда бошад, пас шумо дигар лозим аст, ин барнома.';
+$lang['samba_driver_has_not_been_initialized'] = 'Автомобил намоиш дода нашудааст.';
+$lang['samba_file_share_tooltip'] = 'Агар шумо хоҳед, ки саҳмҳои файлии шабакавиро эҷод кунед, лутфан барномаи Flexshare -ро истифода баред.';
+$lang['samba_initializing_core_system'] = 'Системаи асосии системаро оғоз кунед, лутфан пурсед.';
+$lang['samba_initializing_warning'] = 'Аввалинкунӣ метавонад як ё ду дақиқа гирад, лутфан сабр кунед.';
+$lang['samba_master_node_needs_initialization'] = 'Нутқи магистр бояд пеш аз давомнокии ибтидоӣ оғоз шавад.';
+$lang['samba_mode'] = 'Мода';
+$lang['samba_samba_directory_installed_warning'] = 'Нармафзори Samba Directory насб карда мешавад, бинобар ин ба ин барнома ниёз надоред.';
