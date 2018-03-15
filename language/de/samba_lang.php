f5a369774abffbc0a2eac324ee760891d384aa76
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 13:29:07 +0000

    Add new files and directories, commit changes, push changes

--- language/de/samba_lang.php
+++ language/de/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
 $lang['samba_administrator_account'] = 'Administratorkonto';
-$lang['samba_app_description'] = 'Windows Networking (Samba) sorgt für die notwendige Kleber mit Windows-Systemen zusammenarbeiten. Die App bietet Authentifizierungsdienste, Datei- und Druck, zusammen mit Windows-Domain-Verwaltung.';
-$lang['samba_app_name'] = 'Windows Networking (Samba)';
-$lang['samba_computers'] = 'Computer';
-$lang['samba_driver_has_not_been_initialized'] = 'Treiber nicht initialisiert wurde.';
-$lang['samba_file_share_tooltip'] = 'Wenn Sie möchten, Netzwerk-Dateifreigaben zu erstellen, verwenden Sie bitte die Flexshare App.';
-$lang['samba_initializing_core_system'] = 'Initialisierung Core-System, bitte etwas Geduld.';
-$lang['samba_initializing_warning'] = 'Die Initialisierung kann eine Minute dauern oder zwei, sind bitte geduldig.';
-$lang['samba_master_node_needs_initialization'] = 'Master-Knoten benötigt, bevor fortfahren initialisiert werden.';
+$lang['samba_app_description'] = 'Windows-Netzwerk (Samba) bietet die notwendige Unterstützung für die Zusammenarbeit mit Windows-Systemen. Die App bietet Authentifizierungsdienste, Datei- und Druckfunktionen sowie die Windows-Domänenverwaltung.';
+$lang['samba_app_name'] = 'Windows-Netzwerk (Samba)';
+$lang['samba_computers'] = 'Computers';
+$lang['samba_driver_has_not_been_initialized'] = 'Der Treiber wurde nicht initialisiert.';
+$lang['samba_file_share_tooltip'] = 'Wenn Sie Netzwerkfreigaben erstellen möchten, verwenden Sie bitte die FlexShare App.';
+$lang['samba_initializing_core_system'] = 'Initialisierung des Kernsystems, bitte haben Sie etwas Geduld.';
+$lang['samba_initializing_warning'] = 'Die Initialisierung kann ein oder zwei Minuten dauern. Bitte haben Sie etwas Geduld.';
+$lang['samba_master_node_needs_initialization'] = 'Der Master-Knoten muss vor dem Fortfahren initialisiert werden.';
 $lang['samba_mode'] = 'Modus';
-$lang['samba_samba_directory_installed_warning'] = 'Die Samba-Directory-Software installiert ist, so dass Sie nicht mehr diese App benötigen.';
+$lang['samba_samba_directory_installed_warning'] = 'Die Samba Directory-Software ist installiert, sodass Sie diese App nicht mehr benötigen.';
