dfc4d4c9c8f48d83196ae77223764bc48e28261f
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:10:59 +0000

    Add new files and directories, commit changes, push changes

--- language/zu/samba_lang.php
+++ language/zu/samba_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['samba_administrator_account'] = 'ngasebenzi';
-$lang['samba_app_description'] = 'Windows Networking (Samba) inikeza glue ezidingekayo yokusebenza Windows izinhlelo. Uhlelo lokusebenza inikeza ngamasevisi ubuqiniso, ifayela futhi uprinte, kanye Windows ukuphathwa kwesizinda.';
-$lang['samba_app_name'] = 'Windows Networking (Samba)';
-$lang['samba_computers'] = 'Ezamakhompiyutha';
-$lang['samba_driver_has_not_been_initialized'] = 'Driver engakaze ukuqaliswa.';
-$lang['samba_file_share_tooltip'] = 'Uma ungathanda ukudala amasheya ifayela inethiwekhi, sicela usebenzise uhlelo lokusebenza Flexshare.';
-$lang['samba_initializing_core_system'] = 'Iqalisa core uhlelo, sicela ubekezele.';
-$lang['samba_initializing_warning'] = 'Ukuqalisa kungathatha umzuzu noma emibili, sicela ubekezele.';
-$lang['samba_master_node_needs_initialization'] = 'Master Node idinga ukuba ukuqaliswa ngaphambi kokuqhubeka.';
-$lang['samba_mode'] = 'Mode';
-$lang['samba_samba_directory_installed_warning'] = 'Isofthiwe Samba Directory efakwe, ngakho wena akusadingeki le app.';
+$lang['samba_administrator_account'] = 'I-akhawunti yomphathi';
+$lang['samba_app_description'] = 'I-Windows Networking (i-Samba) inikeza i-glue edingekayo ukuthi ihambisane nezinhlelo ze-Windows. Uhlelo lokusebenza lunikeza izinsizakalo zokuqinisekisa, ifayela kanye nokuphrinta, kanye nokuphathwa kwesizinda se-Windows.';
+$lang['samba_app_name'] = 'I-Windows Networking (i-Samba)';
+$lang['samba_computers'] = 'Amakhompiyutha';
+$lang['samba_driver_has_not_been_initialized'] = 'Umshayeli akaqalisiwe.';
+$lang['samba_file_share_tooltip'] = 'Uma ungathanda ukwakha ukwabelana kwefayela yenethiwekhi, sicela usebenzise uhlelo lokusebenza lwe-Flexshare.';
+$lang['samba_initializing_core_system'] = 'Ukuqala uhlelo lwesisekelo, sicela ube nesineke.';
+$lang['samba_initializing_warning'] = 'Ukuqalisa kungathatha iminithi noma amabili, sicela ubekezele.';
+$lang['samba_master_node_needs_initialization'] = 'I-master node idinga ukuqaliswa ngaphambi kokuqhubeka.';
+$lang['samba_mode'] = 'Imodi';
+$lang['samba_samba_directory_installed_warning'] = 'Isofthiwe yeS directory Directory ifakiwe, ngakho-ke awusayidingi lolu hlelo lokusebenza.';
